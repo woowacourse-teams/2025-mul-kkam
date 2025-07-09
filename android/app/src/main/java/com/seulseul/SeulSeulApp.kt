c11c52a32fdfb977fdc8312da3db5d856117d0bc
hwannow <vnfdlvgid061@naver.com>
2025-07-09 16:47:36 +0900

    feat: 다크모드 비활성화
    - Application 클래스에서 AppCompatDelegate.setDefaultNightMode를 사용하여 다크모드 비활성화
    - 불필요한 values-night/themes.xml 파일 삭제

---
+++ android/app/src/main/java/com/seulseul/SeulSeulApp.kt
@@ -0,0 +1,11 @@
+package com.seulseul
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+class SeulSeulApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
