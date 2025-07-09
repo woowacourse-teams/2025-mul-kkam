1c166d57c48fb6e34c459e977fc965ed232b16f8
hwannow <vnfdlvgid061@naver.com>
2025-07-09 16:57:59 +0900

    refactor: Android 프로젝트 패키지 구조 변경

--- android/app/src/main/java/com/seulseul/ui/main/MainActivity.kt
+++ android/app/src/main/java/com/seulseul/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.seulseul
+package com.seulseul.ui.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.seulseul.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
