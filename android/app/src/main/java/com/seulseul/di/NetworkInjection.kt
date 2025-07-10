c11c52b8f2dc57987d108a22cf51cd86b16b5431
hwannow <vnfdlvgid061@naver.com>
2025-07-10 20:45:23 +0900

    feat: Retrofit 설정 및 Service 객체 생성
    - Retrofit 인스턴스 생성
    - Kotlinx Serialization Converter 추가
    - RouteService 인터페이스를 구현하는 Service 객체 생성

--- android/app/src/main/java/com/seulseul/di/NetworkInjection.kt
+++ android/app/src/main/java/com/seulseul/di/NetworkInjection.kt
@@ -1,11 +1,32 @@
 package com.seulseul.di
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.seulseul.data.remote.service.RouteService
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import retrofit2.Retrofit
 
 object NetworkInjection {
+    private val contentType = "application/json".toMediaType()
+
     private val okHttpClient: OkHttpClient by lazy {
         OkHttpClient
             .Builder()
             .build()
     }
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit
+            .Builder()
+            .baseUrl("baseUrl")
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory(contentType))
+            .build()
+    }
+
+    val routeService: RouteService by lazy {
+        retrofit.create(RouteService::class.java)
+    }
 }
+
