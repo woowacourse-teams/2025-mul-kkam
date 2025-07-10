2b9b991a67031c285ace7cb60e2ada3c9af6a7c6
CheChe903 <cheche903@naver.com>
2025-07-10 13:44:59 +0900

    [BE][feat]: 프로젝트 사전 세팅

---
+++ backend/src/main/java/backend/seul_seul/SeulSeulApplication.java
@@ -0,0 +1,13 @@
+package backend.seul_seul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeulSeulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeulSeulApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/backend/seul_seul/SeulSeulApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.seul_seul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeulSeulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
