25d42bbdc07e8ef76ac8d711e8e9e10f00884fd7
CheChe903 <cheche903@naver.com>
2025-07-19 20:08:45 +0900

    feat: Cup 추가 컨트롤러 구현

--- backend/src/main/java/backend/mulkkam/cup/controller/CupController.java
+++ backend/src/main/java/backend/mulkkam/cup/controller/CupController.java
@@ -1,11 +1,23 @@
 package backend.mulkkam.cup.controller;
 
+import backend.mulkkam.cup.dto.request.CupRegisterRequest;
+import backend.mulkkam.cup.dto.response.CupResponse;
+import backend.mulkkam.cup.service.CupService;
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class CupController {
+
+    private final CupService cupService;
+
+    public CupController(CupService cupService) {
+        this.cupService = cupService;
+    }
 
     @GetMapping("/cups")
     public void read() {
@@ -13,7 +25,13 @@ public class CupController {
     }
 
     @PostMapping("/cup")
-    public void create() {
-
+    public ResponseEntity<CupResponse> create(CupRegisterRequest cupRegisterRequest) {
+        CupResponse cupResponse = cupService.create(cupRegisterRequest);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(cupResponse.id())
+                .toUri();
+        return ResponseEntity.created(location).body(cupResponse);
     }
 }
