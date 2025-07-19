c2ea87da456b9694008a86ace616cdb2e87524f4
CheChe903 <cheche903@naver.com>
2025-07-19 20:08:34 +0900

    feat: Cup 추가 서비스 로직 구현

--- backend/src/main/java/backend/mulkkam/cup/service/CupService.java
+++ backend/src/main/java/backend/mulkkam/cup/service/CupService.java
@@ -1,14 +1,33 @@
 package backend.mulkkam.cup.service;
 
+import backend.mulkkam.cup.domain.Cup;
+import backend.mulkkam.cup.domain.vo.CupNickname;
+import backend.mulkkam.cup.dto.request.CupRegisterRequest;
+import backend.mulkkam.cup.dto.response.CupResponse;
 import backend.mulkkam.cup.repository.CupRepository;
+import backend.mulkkam.intake.domain.vo.Amount;
+import backend.mulkkam.member.domain.Member;
+import backend.mulkkam.member.domain.vo.Gender;
+import backend.mulkkam.member.domain.vo.MemberNickname;
+import backend.mulkkam.member.service.MemberService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CupService {
 
     private final CupRepository cupRepository;
+    private final MemberService memberService;
 
-    public CupService(CupRepository cupRepository) {
+    public CupService(CupRepository cupRepository, MemberService memberService) {
         this.cupRepository = cupRepository;
+        this.memberService = memberService;
+    }
+
+    public CupResponse create(CupRegisterRequest cupRegisterRequest) {
+        Member member = new Member(1L, new MemberNickname("Member"), Gender.MALE, 50, new Amount(1200));
+        Cup cup = new Cup(null, member, new CupNickname(cupRegisterRequest.nickname()), cupRegisterRequest.amount());
+
+        Cup createdCup = cupRepository.save(cup);
+        return new CupResponse(createdCup.getId(), createdCup.getNickname().value(), createdCup.getAmount());
     }
 }
