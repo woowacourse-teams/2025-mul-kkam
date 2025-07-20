cd001cb86894d775505fbf135e56bd18b47ab190
CheChe903 <cheche903@naver.com>
2025-07-20 18:26:46 +0900

    chore: CI/CD 트리거

--- backend/src/main/java/backend/seulseul/SeulSeulApplication.java
+++ backend/src/main/java/backend/seulseul/SeulSeulApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SeulSeulApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeulSeulApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SeulSeulApplication.class, args);
+    }
 }
