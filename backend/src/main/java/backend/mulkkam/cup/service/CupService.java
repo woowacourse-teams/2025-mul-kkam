524abf8eacbb59a2cdd5adf140232ac5e2236380
2jin2.1031 <2jin2.1031@gmail.com>
2025-07-21 15:31:36 +0900

    refactor: 생성자 주입을 @RequiredArgsConstructor로 대체

--- backend/src/main/java/backend/mulkkam/cup/service/CupService.java
+++ backend/src/main/java/backend/mulkkam/cup/service/CupService.java
@@ -1,14 +1,12 @@
 package backend.mulkkam.cup.service;
 
 import backend.mulkkam.cup.repository.CupRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class CupService {
 
     private final CupRepository cupRepository;
-
-    public CupService(CupRepository cupRepository) {
-        this.cupRepository = cupRepository;
-    }
 }
--- backend/src/main/java/backend/mulkkam/intake/service/IntakeHistoryService.java
+++ backend/src/main/java/backend/mulkkam/intake/service/IntakeHistoryService.java
@@ -1,14 +1,13 @@
 package backend.mulkkam.intake.service;
 
 import backend.mulkkam.intake.repository.IntakeHistoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequiredArgsConstructor
 @Service
 public class IntakeHistoryService {
 
     private final IntakeHistoryRepository intakeHistoryRepository;
-
-    public IntakeHistoryService(IntakeHistoryRepository intakeHistoryRepository) {
-        this.intakeHistoryRepository = intakeHistoryRepository;
-    }
 }
