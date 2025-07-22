c6a2ca6484bb89cfcbac61ef6f206a738d330e92
CheChe903 <cheche903@naver.com>
2025-07-22 20:35:32 +0900

    feat: Cup 생성 로직 테스트 코드 구현

---
+++ backend/src/test/java/backend/mulkkam/cup/service/CupServiceIntegrationTest.java
@@ -0,0 +1,99 @@
+package backend.mulkkam.cup.service;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import backend.mulkkam.cup.dto.request.CupRegisterRequest;
+import backend.mulkkam.cup.dto.response.CupResponse;
+import backend.mulkkam.cup.repository.CupRepository;
+import backend.mulkkam.member.domain.Member;
+import backend.mulkkam.member.repository.MemberRepository;
+import backend.mulkkam.support.MemberFixture;
+import backend.mulkkam.support.ServiceIntegrationTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class CupServiceIntegrationTest extends ServiceIntegrationTest {
+
+    @Autowired
+    private CupService cupService;
+
+    @Autowired
+    private CupRepository cupRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @DisplayName("컵을 생성할 때에")
+    @Nested
+    class Create {
+
+        @DisplayName("정상적으로 저장한다")
+        @Test
+        void success() {
+            // given
+            CupRegisterRequest cupRegisterRequest = new CupRegisterRequest("스타벅스", 500);
+            Member member = new MemberFixture().build();
+            memberRepository.save(member);
+
+            // when
+            CupResponse cupResponse = cupService.create(cupRegisterRequest, member.getId());
+
+            // then
+            assertSoftly(softly -> {
+                softly.assertThat(cupResponse.nickname()).isEqualTo("스타벅스");
+                softly.assertThat(cupResponse.amount()).isEqualTo(500);
+                softly.assertThat(cupRepository.findById(cupResponse.id())).isPresent();
+            });
+        }
+
+        @DisplayName("용량이 0이면 예외가 발생한다")
+        @Test
+        void error_amountIsEqual0() {
+            // given
+            CupRegisterRequest cupRegisterRequest = new CupRegisterRequest("스타벅스", 0);
+            Member member = new MemberFixture().build();
+            memberRepository.save(member);
+
+            // when & then
+            assertThatThrownBy(() -> cupService.create(cupRegisterRequest, member.getId()))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("용량이 음수면 예외가 발생한다")
+        @Test
+        void error_amountLessThen0() {
+            // given
+            CupRegisterRequest cupRegisterRequest = new CupRegisterRequest("스타벅스", -100);
+            Member member = new MemberFixture().build();
+            memberRepository.save(member);
+
+            // when & then
+            assertThatThrownBy(() -> cupService.create(cupRegisterRequest, member.getId()))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("컵이 3개 저장되어 있을 때 예외가 발생한다")
+        @Test
+        void error_memberAlreadyHasThreeCups() {
+            // given
+            CupRegisterRequest cupRegisterRequest = new CupRegisterRequest("스타벅스", 500);
+            Member member = new MemberFixture().build();
+            memberRepository.save(member);
+            CupRegisterRequest cupRegisterRequest1 = new CupRegisterRequest("스타벅스", 500);
+            CupRegisterRequest cupRegisterRequest2 = new CupRegisterRequest("스타벅스", 500);
+            CupRegisterRequest cupRegisterRequest3 = new CupRegisterRequest("스타벅스", 500);
+
+            // when
+            cupService.create(cupRegisterRequest1, member.getId());
+            cupService.create(cupRegisterRequest2, member.getId());
+            cupService.create(cupRegisterRequest3, member.getId());
+
+            // then
+            assertThatThrownBy(() -> cupService.create(cupRegisterRequest, member.getId()))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+}
---
+++ backend/src/test/java/backend/mulkkam/cup/service/CupServiceUnitTest.java
@@ -0,0 +1,123 @@
+package backend.mulkkam.cup.service;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+import backend.mulkkam.cup.domain.Cup;
+import backend.mulkkam.cup.domain.vo.CupNickname;
+import backend.mulkkam.cup.dto.request.CupRegisterRequest;
+import backend.mulkkam.cup.dto.response.CupResponse;
+import backend.mulkkam.cup.repository.CupRepository;
+import backend.mulkkam.member.domain.Member;
+import backend.mulkkam.member.repository.MemberRepository;
+import backend.mulkkam.support.MemberFixture;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CupServiceUnitTest {
+
+    @Mock
+    private CupRepository cupRepository;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @InjectMocks
+    private CupService cupService;
+
+
+    @DisplayName("컵을 생성할 때")
+    @Nested
+    class Create {
+
+        @Test
+        void success() {
+            // given
+            CupRegisterRequest cupRegisterRequest = new CupRegisterRequest(
+                    "스타벅스",
+                    500);
+            Member member = new MemberFixture().build();
+            given(memberRepository.findById(member.getId()))
+                    .willReturn(Optional.of(member));
+
+            Cup savedCup = new Cup(
+                    1L,
+                    member,
+                    new CupNickname("스타벅스"),
+                    500,
+                    1
+            );
+            when(cupRepository.save(any(Cup.class))).thenReturn(savedCup);
+
+            // when
+            CupResponse cupResponse = cupService.create(cupRegisterRequest, member.getId());
+
+            // then
+            assertSoftly(softly -> {
+                softly.assertThat(cupResponse.nickname()).isEqualTo("스타벅스");
+                softly.assertThat(cupResponse.amount()).isEqualTo(500);
+            });
+        }
+
+        @DisplayName("용량이 0이면 예외가 발생한다")
+        @Test
+        void error_amountIsEqual0() {
+            // given
+            CupRegisterRequest cupRegisterRequest = new CupRegisterRequest("스타벅스", 0);
+            Member member = new MemberFixture().build();
+            given(memberRepository.findById(member.getId()))
+                    .willReturn(Optional.of(member));
+
+            // when & then
+            assertThatThrownBy(() -> cupService.create(cupRegisterRequest, member.getId()))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("용량이 음수면 예외가 발생한다")
+        @Test
+        void error_amountLessThen0() {
+            // given
+            CupRegisterRequest cupRegisterRequest = new CupRegisterRequest("스타벅스", -100);
+            Member member = new MemberFixture().build();
+            given(memberRepository.findById(member.getId()))
+                    .willReturn(Optional.of(member));
+
+            // when & then
+            assertThatThrownBy(() -> cupService.create(cupRegisterRequest, member.getId()))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("컵이 3개 저장되어 있을 때 예외가 발생한다")
+        @Test
+        void error_memberAlreadyHasThreeCups() {
+            // given
+            CupRegisterRequest cupRegisterRequest = new CupRegisterRequest("스타벅스", 500);
+            Member member = new MemberFixture().build();
+            given(memberRepository.findById(member.getId()))
+                    .willReturn(Optional.of(member));
+
+            List<Cup> cups = List.of(
+                    new Cup(member, new CupNickname("스타벅스"), 500, 1),
+                    new Cup(member, new CupNickname("스타벅스"), 500, 1),
+                    new Cup(member, new CupNickname("스타벅스"), 500, 1));
+
+            // when
+            when(cupRepository.findAllByMemberId(member.getId())).thenReturn(cups);
+
+            // then
+            assertThatThrownBy(() -> cupService.create(cupRegisterRequest, member.getId()))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+} 
