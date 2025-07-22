0420aaf473382b0c86a6167bb8b855d668b984a2
2jin2.1031 <2jin2.1031@gmail.com>
2025-07-22 21:46:33 +0900

    test: MemberService의 단위테스트 추가

---
+++ backend/src/test/java/backend/mulkkam/member/service/MemberServiceUnitTest.java
@@ -0,0 +1,95 @@
+package backend.mulkkam.member.service;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.mockito.Mockito.when;
+
+import backend.mulkkam.member.domain.Member;
+import backend.mulkkam.member.domain.vo.Gender;
+import backend.mulkkam.member.dto.MemberPhysicalAttributesModifyRequest;
+import backend.mulkkam.member.repository.MemberRepository;
+import backend.mulkkam.support.MemberFixture;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class MemberServiceUnitTest {
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @InjectMocks
+    private MemberService memberService;
+
+    @DisplayName("멤버의 신체적인 속성 값을 수정할 때")
+    @Nested
+    class modifyPhysicalAttributes {
+
+        @DisplayName("올바른 데이터로 모든 필드를 수정할 시 값이 반영된다")
+        @Test
+        void success_validDataAllArgs() {
+            // given
+            Member member = new MemberFixture()
+                    .weight(null)
+                    .gender(null)
+                    .build();
+            Long memberId = 1L;
+            when(memberRepository.findById(memberId)).thenReturn(Optional.of(member));
+
+            Gender gender = Gender.FEMALE;
+            Integer weight = 50;
+            MemberPhysicalAttributesModifyRequest memberPhysicalAttributesModifyRequest = new MemberPhysicalAttributesModifyRequest(
+                    gender,
+                    weight
+            );
+
+            // when
+            memberService.modifyPhysicalAttributes(
+                    memberPhysicalAttributesModifyRequest,
+                    memberId
+            );
+
+            // then
+            Member result = memberRepository.findById(memberId).get();
+
+            assertSoftly(softly -> {
+                softly.assertThat(result.getGender()).isEqualTo(gender);
+                softly.assertThat(result.getWeight()).isEqualTo(weight);
+            });
+        }
+
+        @DisplayName("수정 요청한 필드들의 값들만 반영된다")
+        @Test
+        void success_validDataSomeArgs() {
+            // given
+            Member member = new MemberFixture().build();
+            Long memberId = 1L;
+            when(memberRepository.findById(memberId)).thenReturn(Optional.of(member));
+
+            Integer weight = 50;
+            MemberPhysicalAttributesModifyRequest memberPhysicalAttributesModifyRequest = new MemberPhysicalAttributesModifyRequest(
+                    null,
+                    weight
+            );
+
+            // when
+            memberService.modifyPhysicalAttributes(
+                    memberPhysicalAttributesModifyRequest,
+                    memberId
+            );
+
+            // then
+            Member result = memberRepository.findById(memberId).get();
+
+            assertSoftly(softly -> {
+                softly.assertThat(result.getGender()).isEqualTo(member.getGender());
+                softly.assertThat(result.getWeight()).isEqualTo(weight);
+            });
+        }
+    }
+}
