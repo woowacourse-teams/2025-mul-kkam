78f66507cf87d19b27987650256f5f3168200aa8
2jin2.1031 <2jin2.1031@gmail.com>
2025-07-23 14:16:17 +0900

    test: 불필요한 @SpringBootTest 어노테이션 제거 - 상속으로 인해 이미 적용되어 있음

--- backend/src/test/java/backend/mulkkam/member/service/MemberServiceIntegrationTest.java
+++ backend/src/test/java/backend/mulkkam/member/service/MemberServiceIntegrationTest.java
@@ -12,10 +12,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest(webEnvironment = WebEnvironment.NONE)
 class MemberServiceIntegrationTest extends ServiceIntegrationTest {
 
     @Autowired
