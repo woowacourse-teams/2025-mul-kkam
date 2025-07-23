cca5ff3d72795893cf3339fed09d2c3a7ac65a13
junseo511 <prime54@g.skku.edu>
2025-07-23 17:37:57 +0900

    feat: 프로필/목표 설정 화면 뒤로가기 버튼 기능 추가

--- android/app/src/main/java/com/mulkkam/ui/SettingGoalActivity.kt
+++ android/app/src/main/java/com/mulkkam/ui/SettingGoalActivity.kt
@@ -2,10 +2,18 @@ package com.mulkkam.ui
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import com.mulkkam.databinding.ActivitySettingGoalBinding
 import com.mulkkam.ui.binding.BindingActivity
 
 class SettingGoalActivity : BindingActivity<ActivitySettingGoalBinding>(ActivitySettingGoalBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.ivBack.setOnClickListener {
+            finish()
+        }
+    }
+
     companion object {
         fun newIntent(context: Context): Intent = Intent(context, SettingGoalActivity::class.java)
     }
