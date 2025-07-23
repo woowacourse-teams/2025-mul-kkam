50a4549abae21659e8ad050ef87a0ac4c032702d
CheChe903 <cheche903@naver.com>
2025-07-23 15:56:27 +0900

    feat: 컵 이름 정책 변경

--- backend/src/main/java/backend/mulkkam/cup/domain/vo/CupNickname.java
+++ backend/src/main/java/backend/mulkkam/cup/domain/vo/CupNickname.java
@@ -7,10 +7,11 @@ public record CupNickname(
         String value
 ) {
 
-    public static final int MAX_LENGTH = 5;
+    public static final int MAX_LENGTH = 10;
+    public static final int MIN_LENGTH = 2;
 
     public CupNickname {
-        if (value.isEmpty() || value.length() > MAX_LENGTH) {
+        if (value.length() < MIN_LENGTH || value.length() > MAX_LENGTH) {
             throw new IllegalArgumentException("컵 이름 형식이 올바르지 않습니다.");
         }
     }
