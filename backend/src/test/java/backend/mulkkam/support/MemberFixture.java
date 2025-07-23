ed8230420339e6c84383d5d3f62762025357a1de
2jin2.1031 <2jin2.1031@gmail.com>
2025-07-23 16:08:48 +0900

    test: 몸무게 자료형 수정(Integer -> Double)

--- backend/src/test/java/backend/mulkkam/support/MemberFixture.java
+++ backend/src/test/java/backend/mulkkam/support/MemberFixture.java
@@ -9,7 +9,7 @@ public class MemberFixture {
 
     private MemberNickname memberNickname = new MemberNickname("히로");
     private Gender gender = Gender.FEMALE;
-    private Integer weight = 50;
+    private Double weight = 50.2;
     private Amount targetAmount = new Amount(1000);
 
     public MemberFixture memberNickname(MemberNickname memberNickname) {
@@ -22,7 +22,7 @@ public class MemberFixture {
         return this;
     }
 
-    public MemberFixture weight(Integer weight) {
+    public MemberFixture weight(Double weight) {
         this.weight = weight;
         return this;
     }
