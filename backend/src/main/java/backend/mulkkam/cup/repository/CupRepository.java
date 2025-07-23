aceae5eed35f996cf765ac0a3836eecc154b3271
CheChe903 <cheche903@naver.com>
2025-07-23 15:47:33 +0900

    feat: findMaxRank 메서드 삭제

--- backend/src/main/java/backend/mulkkam/cup/repository/CupRepository.java
+++ backend/src/main/java/backend/mulkkam/cup/repository/CupRepository.java
@@ -2,13 +2,8 @@ package backend.mulkkam.cup.repository;
 
 import backend.mulkkam.cup.domain.Cup;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface CupRepository extends JpaRepository<Cup, Long> {
     List<Cup> findAllByMemberId(Long memberId);
-
-    @Query("SELECT MAX(c.cupRank) FROM Cup c WHERE c.member.id = :memberId")
-    Optional<Integer> findMaxRankByMemberId(Long memberId);
 }
