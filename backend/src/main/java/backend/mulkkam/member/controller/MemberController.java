295e0c5440a99b2389b8c41102aa0c5ada03e9a3
2jin2.1031 <2jin2.1031@gmail.com>
2025-07-23 15:13:29 +0900

    refactor: 반환 상태 코드 수정 - 이유: 팀 컨벤션

--- backend/src/main/java/backend/mulkkam/member/controller/MemberController.java
+++ backend/src/main/java/backend/mulkkam/member/controller/MemberController.java
@@ -22,6 +22,6 @@ public class MemberController {
                 physicalAttributesModifyRequest,
                 1L
         );
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok().build();
     }
 }
