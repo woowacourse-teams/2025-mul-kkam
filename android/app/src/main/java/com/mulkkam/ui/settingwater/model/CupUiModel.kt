916a05e6e761b4d0c6a698d9f157145111baffe6
junseo511 <prime54@g.skku.edu>
2025-07-24 16:30:05 +0900

    feat: CupUiModel에 Parcelable 적용

--- android/app/src/main/java/com/mulkkam/ui/settingwater/model/CupUiModel.kt
+++ android/app/src/main/java/com/mulkkam/ui/settingwater/model/CupUiModel.kt
@@ -1,9 +1,13 @@
 package com.mulkkam.ui.settingwater.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class CupUiModel(
     val id: Int,
     val nickname: String,
     val cupAmount: Int,
     val cupRank: Int,
     val isRepresentative: Boolean = false,
-)
+) : Parcelable
