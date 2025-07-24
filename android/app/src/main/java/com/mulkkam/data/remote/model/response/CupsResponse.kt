6f29b413063faf25ae719ca652a919c36931703e
devfeijoa <devfeijoa@gmail.com>
2025-07-24 20:09:53 +0900

    feat: 컵 관련 Response 데이터 클래스 및 도메인 모델로의 변환 함수 정의
    - `CupResponse.kt`: 개별 컵 정보를 나타내는 데이터 클래스 및 도메인 모델로의 변환 함수 정의
    - `CupsResponse.kt`: 컵 목록 정보를 나타내는 데이터 클래스 및 도메인 모델로의 변환 함수 정의

---
+++ android/app/src/main/java/com/mulkkam/data/remote/model/response/CupsResponse.kt
@@ -0,0 +1,19 @@
+package com.mulkkam.data.remote.model.response
+
+import com.mulkkam.domain.Cups
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CupsResponse(
+    @SerialName("size")
+    val size: Int,
+    @SerialName("cups")
+    val cups: List<CupResponse>,
+)
+
+fun CupsResponse.toDomain() =
+    Cups(
+        size = size,
+        cups = cups.map { it.toDomain() },
+    )
