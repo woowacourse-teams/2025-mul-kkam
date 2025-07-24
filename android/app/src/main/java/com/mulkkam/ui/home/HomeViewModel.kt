00882d1ac90f9d03c9811431b4cf5745a4cc5489
devfeijoa <devfeijoa@gmail.com>
2025-07-24 20:17:01 +0900

    refactor: viewModel 초기화 시 컵 정보 로드

--- android/app/src/main/java/com/mulkkam/ui/home/HomeViewModel.kt
+++ android/app/src/main/java/com/mulkkam/ui/home/HomeViewModel.kt
@@ -3,13 +3,25 @@ package com.mulkkam.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mulkkam.di.RepositoryInjection
+import com.mulkkam.domain.Cups
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
     private val _currentWaterIntake = MutableLiveData<Int>()
     val currentWaterIntake: LiveData<Int> get() = _currentWaterIntake
+    var cups: Cups? = null
+
+    init {
 
     fun addWaterIntake(newWaterIntake: Int) {
         val addedResult = (currentWaterIntake.value ?: 0) + newWaterIntake
         _currentWaterIntake.value = newWaterIntake
+        viewModelScope.launch {
+            cups = RepositoryInjection.cupsRepository.getCups()
+        }
+    }
+
     }
 }
