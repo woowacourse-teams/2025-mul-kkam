373906b4bc2528747d59e289cd00c26397efaea3
Jin409 <sualng123@gmail.com>
2025-07-24 20:15:09 +0900

    test: 100 이 넘는 경우 100을 반환하는지 검증

--- backend/src/test/java/backend/mulkkam/intake/domain/vo/AchievementRateTest.java
+++ backend/src/test/java/backend/mulkkam/intake/domain/vo/AchievementRateTest.java
@@ -30,5 +30,23 @@ class AchievementRateTest {
             assertThat(achievementRate.value())
                     .isCloseTo(10.0, within(0.1));
         }
+
+        @DisplayName("100이 넘는 경우 100을 반환한다")
+        @Test
+        void success_withValueOver100() {
+            // given
+            Amount targetIntakeAmount = new Amount(1_000);
+            Amount totalIntakeAmount = new Amount(1_000_000);
+
+            // when
+            AchievementRate achievementRate = new AchievementRate(
+                    totalIntakeAmount,
+                    targetIntakeAmount
+            );
+
+            // then
+            assertThat(achievementRate.value())
+                    .isEqualTo(100);
+        }
     }
 }
