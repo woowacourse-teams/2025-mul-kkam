cda30648d4cb4db70e6c2d76001c3797cbb55817
2jin2.1031 <2jin2.1031@gmail.com>
2025-07-27 01:05:50 +0900

    test: 빌드 실패 & 테스트 성공

--- backend/src/main/java/backend/mulkkam/MulKkamApplication.java
+++ backend/src/main/java/backend/mulkkam/MulKkamApplication.java
@@ -1,13 +1,12 @@
 package backend.mulkkam;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MulKkamApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MulKkamApplication.class, args);
+        //SpringApplication.run(MulKkamApplication.class, args);
         
     }
 
