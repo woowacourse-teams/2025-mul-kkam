b0092ec8bf9dfc358bd60c145cf6ac647da2b813
minSsan <msp1125@hanyang.ac.kr>
2025-07-28 02:18:23 +0900

    feat: 컵 삭제 API 추가 - 컨트롤러 메서드 추가

--- backend/src/main/java/backend/mulkkam/cup/controller/CupController.java
+++ backend/src/main/java/backend/mulkkam/cup/controller/CupController.java
@@ -5,7 +5,9 @@ import backend.mulkkam.cup.dto.response.CupsResponse;
 import backend.mulkkam.cup.service.CupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +28,12 @@ public class CupController {
     @PostMapping
     public ResponseEntity<Void> create(@RequestBody CupRegisterRequest cupRegisterRequest) {
         cupService.create(cupRegisterRequest, 1L);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+        cupService.delete(id, 1L);
         return ResponseEntity.ok().build();
     }
 }
