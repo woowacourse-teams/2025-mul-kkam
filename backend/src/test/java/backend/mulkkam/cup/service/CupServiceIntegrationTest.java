4deac730ed4639833f9f76e5565bd6962a4336a5
minSsan <msp1125@hanyang.ac.kr>
2025-07-28 22:30:03 +0900

    test(CupServiceIntegrationTest): 컵 삭제 통합 테스트 추가

--- backend/src/test/java/backend/mulkkam/cup/service/CupServiceIntegrationTest.java
+++ backend/src/test/java/backend/mulkkam/cup/service/CupServiceIntegrationTest.java
@@ -1,13 +1,9 @@
 package backend.mulkkam.cup.service;
 
-import static backend.mulkkam.common.exception.errorCode.BadRequestErrorCode.INVALID_CUP_AMOUNT;
-import static backend.mulkkam.common.exception.errorCode.BadRequestErrorCode.INVALID_CUP_COUNT;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import backend.mulkkam.common.exception.CommonException;
 import backend.mulkkam.cup.domain.Cup;
+import backend.mulkkam.cup.domain.vo.CupAmount;
+import backend.mulkkam.cup.domain.vo.CupNickname;
 import backend.mulkkam.cup.domain.vo.CupRank;
 import backend.mulkkam.cup.dto.request.CupRegisterRequest;
 import backend.mulkkam.cup.dto.response.CupResponse;
@@ -76,6 +72,33 @@ class CupServiceIntegrationTest extends ServiceIntegrationTest {
                 softly.assertThat(cupResponse.cupNickname()).isEqualTo(cupNickname);
                 softly.assertThat(cupResponse.cupAmount()).isEqualTo(cupAmount);
                 softly.assertThat(cupRepository.findById(cupResponse.id())).isPresent();
+            });
+        }
+
+        @DisplayName("세 개의 컵이 삭제되었다가 추가되는 경우, 우선순위가 중복되지 않는다.")
+        @Test
+        void success_createAfterDeleted() {
+            // given
+            Cup firstCup = new Cup(member, new CupNickname("first"), new CupAmount(100), new CupRank(1));
+            Cup secondCup = new Cup(member, new CupNickname("second"), new CupAmount(100), new CupRank(2));
+            Cup thirdCup = new Cup(member, new CupNickname("third"), new CupAmount(100), new CupRank(3));
+
+            cupRepository.save(firstCup);
+            cupRepository.save(secondCup);
+            cupRepository.save(thirdCup);
+
+            cupService.delete(thirdCup.getId(), member.getId());
+
+            CupRegisterRequest request = new CupRegisterRequest("new", 100);
+
+            // when
+            cupService.create(request, member.getId());
+
+            // then
+            assertSoftly(softly -> {
+                List<Cup> cups = cupRepository.findAllByMemberId(member.getId());
+                softly.assertThat(cups).hasSize(3);
+                softly.assertThat(cups.stream().map(Cup::getCupRank)).doesNotHaveDuplicates();
             });
         }
 
@@ -192,6 +215,79 @@ class CupServiceIntegrationTest extends ServiceIntegrationTest {
                         .map(CupResponse::cupRank)
                         .toList();
                 softly.assertThat(ranks).isSorted();
+            });
+        }
+    }
+
+    @DisplayName("컵을 삭제할 때")
+    @Nested
+    class Delete {
+
+        private final Member member = MemberFixtureBuilder.builder().build();
+        private final Cup firstCup = CupFixtureBuilder
+                .withMember(member)
+                .cupRank(new CupRank(1))
+                .build();
+        private final Cup secondCup = CupFixtureBuilder
+                .withMember(member)
+                .cupRank(new CupRank(2))
+                .build();
+        private final Cup thirdCup = CupFixtureBuilder
+                .withMember(member)
+                .cupRank(new CupRank(3))
+                .build();
+
+        @BeforeEach
+        void setup() {
+            memberRepository.save(member);
+            cupRepository.save(firstCup);
+            cupRepository.save(secondCup);
+            cupRepository.save(thirdCup);
+        }
+
+        @DisplayName("우선순위가 더 낮은 컵들의 우선순위가 한 단계씩 승격된다.")
+        @Test
+        void success_withLowerPriorityCups() {
+            // when
+            cupService.delete(firstCup.getId(), member.getId());
+
+            // then
+            assertSoftly(softly -> {
+                softly.assertThat(cupRepository.existsById(firstCup.getId())).isFalse();
+                softly.assertThat(cupRepository.findAllByMemberId(member.getId())).hasSize(2);
+                softly.assertThat(cupRepository.findById(secondCup.getId()))
+                        .isPresent()
+                        .get()
+                        .extracting(Cup::getCupRank)
+                        .isEqualTo(new CupRank(1));
+                softly.assertThat(cupRepository.findById(thirdCup.getId()))
+                        .isPresent()
+                        .get()
+                        .extracting(Cup::getCupRank)
+                        .isEqualTo(new CupRank(2));
+            });
+        }
+
+        @DisplayName("우선순위가 더 낮은 컵이 없는 경우, 그 어떤 컵의 우선순위도 승격되지 않는다.")
+        @Test
+        void success_withoutLowerPriorityCups() {
+            // when
+            cupService.delete(thirdCup.getId(), member.getId());
+
+            // then
+            assertSoftly(softly -> {
+                softly.assertThat(cupRepository.existsById(thirdCup.getId())).isFalse();
+                softly.assertThat(cupRepository.findAllByMemberId(member.getId())).hasSize(2);
+                softly.assertThat(cupRepository.findById(firstCup.getId()))
+                        .isPresent()
+                        .get()
+                        .extracting(Cup::getCupRank)
+                        .isEqualTo(new CupRank(1));
+                softly.assertThat(cupRepository.findById(secondCup.getId()))
+                        .isPresent()
+                        .get()
+                        .extracting(Cup::getCupRank)
+                        .isEqualTo(new CupRank(2));
             });
         }
     }
