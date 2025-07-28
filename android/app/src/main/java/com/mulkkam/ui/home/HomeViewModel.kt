1cd558d25951ba9c508befe8d9ca22a502ddf799
hwannow <vnfdlvgid061@naver.com>
2025-07-28 17:19:05 +0900

    feat: 홈화면 오늘 기록 조회 기능 추가

--- android/app/src/main/java/com/mulkkam/ui/home/HomeViewModel.kt
+++ android/app/src/main/java/com/mulkkam/ui/home/HomeViewModel.kt
@@ -6,15 +6,25 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mulkkam.di.RepositoryInjection
 import com.mulkkam.domain.Cups
+import com.mulkkam.domain.IntakeHistorySummary
 import kotlinx.coroutines.launch
+import java.time.LocalDate
 
 class HomeViewModel : ViewModel() {
-    private val _currentWaterIntake = MutableLiveData<Int>()
-    val currentWaterIntake: LiveData<Int> get() = _currentWaterIntake
+    private val _todayIntakeHistorySummary = MutableLiveData<IntakeHistorySummary>()
+    val todayIntakeHistorySummary: LiveData<IntakeHistorySummary> get() = _todayIntakeHistorySummary
+
     var cups: Cups? = null
 
     init {
         viewModelScope.launch {
+            val today = LocalDate.now()
+            val summary =
+                RepositoryInjection.intakeRepository.getIntakeHistory(today, today).firstOrNull()
+
+            _todayIntakeHistorySummary.value =
+                summary ?: IntakeHistorySummary.EMPTY_DAILY_WATER_INTAKE
+
             cups = RepositoryInjection.cupsRepository.getCups()
         }
     }
@@ -23,7 +33,18 @@ class HomeViewModel : ViewModel() {
         // TODO: 현재 cupRank가 2부터 들어가있음
         val cup = cups?.cups?.find { it.cupRank == 2 }
         val cupAmount = cup?.cupAmount
-        _currentWaterIntake.value =
-            (currentWaterIntake.value ?: 0) + (cupAmount ?: 0)
+
+        _todayIntakeHistorySummary.value =
+            _todayIntakeHistorySummary.value?.copy(
+                totalIntakeAmount =
+                    (
+                        _todayIntakeHistorySummary.value?.totalIntakeAmount
+                            ?: DEFAULT_INTAKE_AMOUNT
+                    ) + (cupAmount ?: DEFAULT_INTAKE_AMOUNT),
+            )
+    }
+
+    companion object {
+        private const val DEFAULT_INTAKE_AMOUNT = 0
     }
 }
