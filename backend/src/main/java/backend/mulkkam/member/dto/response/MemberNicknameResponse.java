80cf4305bc2706da4ed8752cd7cadb18fd314ce2
2jin2.1031 <2jin2.1031@gmail.com>
2025-07-28 23:30:21 +0900

    refactor: 불필요한 주석 제거

--- backend/src/main/java/backend/mulkkam/member/dto/response/MemberNicknameResponse.java
+++ backend/src/main/java/backend/mulkkam/member/dto/response/MemberNicknameResponse.java
@@ -2,7 +2,6 @@ package backend.mulkkam.member.dto.response;
 
 import backend.mulkkam.member.domain.Member;
 
-// TODO 2025. 7. 28. 22:40: 디렉토리 분리 response, request
 public record MemberNicknameResponse(String memberNickname) {
 
     public MemberNicknameResponse(Member member) {
