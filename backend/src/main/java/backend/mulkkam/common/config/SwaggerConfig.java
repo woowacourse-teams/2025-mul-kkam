1c095f95d4c9ad98c432c94897c415011c805463
2jin2.1031 <2jin2.1031@gmail.com>
2025-07-30 00:26:17 +0900

    feat: swagger 연동 config 추가

--- backend/src/main/java/backend/mulkkam/common/config/SwaggerConfig.java
+++ backend/src/main/java/backend/mulkkam/common/config/SwaggerConfig.java
@@ -8,9 +8,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI roomEscapeOpenAPI() {
-        String title = "RoomEscape Application Swagger";
-        String description = "방탈출 서비스의 API 문서입니다.";
+    public OpenAPI mulkkamOpenAPI() {
+        String title = "Mul-Kkam Application Swagger";
+        String description = "물깜 서비스의 API 문서입니다.";
 
         Info info = new Info().title(title).description(description).version("1.0.0");
 
