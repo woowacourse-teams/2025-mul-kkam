d092e73d051fd38d3bc7adb8bcc90adafe70cd8b
minSsan <msp1125@hanyang.ac.kr>
2025-07-30 01:50:13 +0900

    feat(CupController): 컵 순위 수정 API 추가 (`/cups/ranks`)

--- backend/src/main/java/backend/mulkkam/cup/controller/CupController.java
+++ backend/src/main/java/backend/mulkkam/cup/controller/CupController.java
@@ -1,12 +1,15 @@
 package backend.mulkkam.cup.controller;
 
 import backend.mulkkam.cup.dto.request.CupRegisterRequest;
+import backend.mulkkam.cup.dto.request.UpdateCupRanksRequest;
+import backend.mulkkam.cup.dto.response.CupsRanksResponse;
 import backend.mulkkam.cup.dto.response.CupsResponse;
 import backend.mulkkam.cup.service.CupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +30,11 @@ public class CupController {
     public ResponseEntity<Void> create(@RequestBody CupRegisterRequest cupRegisterRequest) {
         cupService.create(cupRegisterRequest, 1L);
         return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/ranks")
+    public ResponseEntity<CupsRanksResponse> updateRanks(@RequestBody UpdateCupRanksRequest request) {
+        CupsRanksResponse response = cupService.updateRanks(request, 1L);
+        return ResponseEntity.ok(response);
     }
 }
