0141163676df40a992a1680eb83c5ab209c6321b
minSsan <msp1125@hanyang.ac.kr>
2025-07-30 20:17:13 +0900

    refactor(CupRankTest): 테스트 클래스 및 메서드 명 수정

--- backend/src/test/java/backend/mulkkam/cup/domain/vo/CupRankTest.java
+++ backend/src/test/java/backend/mulkkam/cup/domain/vo/CupRankTest.java
@@ -17,12 +17,12 @@ class CupRankTest {
 
     @DisplayName("생성자 검증 시에")
     @Nested
-    class NewCupNickname {
+    class NewCupRank {
 
         @DisplayName("컵의 우선순위는 1 이상 3 이하만 가능하다")
         @ParameterizedTest
         @ValueSource(ints = {1, 2, 3})
-        void success_nameLengthBetween2And10(Integer input) {
+        void success_valueBetween2And10(Integer input) {
             // when & then
             assertThatCode(() -> {
                 new CupRank(input);
@@ -32,7 +32,7 @@ class CupRankTest {
         @DisplayName("범위를 벗어난 갯수는 설정할 수 없다")
         @ParameterizedTest
         @ValueSource(ints = {0, 4, -1, 5})
-        void error_nameLengthOutOfRange(Integer input) {
+        void error_valueOutOfRange(Integer input) {
             // when & then
             CommonException ex = assertThrows(CommonException.class,
                     () -> new CupRank(input));
