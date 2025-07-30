b830ee9c6673b0e55cf890f93197fb72f83408d6
minSsan <msp1125@hanyang.ac.kr>
2025-07-30 21:34:50 +0900

    test(CupServiceUnitTest): 컵 삭제 서비스 로직 단위 테스트 작성

--- backend/src/test/java/backend/mulkkam/cup/service/CupServiceUnitTest.java
+++ backend/src/test/java/backend/mulkkam/cup/service/CupServiceUnitTest.java
@@ -1,14 +1,5 @@
 package backend.mulkkam.cup.service;
 
-import static backend.mulkkam.common.exception.errorCode.BadRequestErrorCode.INVALID_CUP_AMOUNT;
-import static backend.mulkkam.common.exception.errorCode.BadRequestErrorCode.INVALID_CUP_COUNT;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
-
 import backend.mulkkam.common.exception.CommonException;
 import backend.mulkkam.cup.domain.Cup;
 import backend.mulkkam.cup.domain.vo.CupAmount;
@@ -21,8 +12,6 @@ import backend.mulkkam.member.domain.Member;
 import backend.mulkkam.member.repository.MemberRepository;
 import backend.mulkkam.support.CupFixtureBuilder;
 import backend.mulkkam.support.MemberFixtureBuilder;
-import java.util.List;
-import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -30,6 +19,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static backend.mulkkam.common.exception.errorCode.BadRequestErrorCode.INVALID_CUP_AMOUNT;
+import static backend.mulkkam.common.exception.errorCode.BadRequestErrorCode.INVALID_CUP_COUNT;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CupServiceUnitTest {
@@ -201,6 +202,59 @@ class CupServiceUnitTest {
                         .map(CupResponse::cupRank)
                         .toList();
                 softly.assertThat(ranks).isSorted();
+            });
+        }
+    }
+
+    @DisplayName("컵을 삭제할 때")
+    @Nested
+    class Delete {
+
+        private final Member member = MemberFixtureBuilder.builder().build();
+        private final Cup firstCup = CupFixtureBuilder
+                .withMember(member)
+                .cupRank(new CupRank(1))
+                .build();
+        private final Cup secondCup = CupFixtureBuilder
+                .withMember(member)
+                .cupRank(new CupRank(2))
+                .build();
+        private final Cup thirdCup = CupFixtureBuilder
+                .withMember(member)
+                .cupRank(new CupRank(3))
+                .build();
+
+        @DisplayName("우선순위가 더 낮은 컵들의 우선순위가 한 단계씩 승격된다.")
+        @Test
+        void success_withLowerPriorityCups() {
+            // given
+            when(cupRepository.findByIdAndMemberId(firstCup.getId(), member.getId())).thenReturn(Optional.of(firstCup));
+            when(cupRepository.findAllByMemberId(member.getId())).thenReturn(List.of(secondCup, thirdCup));
+
+            // when
+            cupService.delete(firstCup.getId(), member.getId());
+
+            // then
+            assertSoftly(softly -> {
+                softly.assertThat(secondCup.getCupRank()).isEqualTo(new CupRank(1));
+                softly.assertThat(thirdCup.getCupRank()).isEqualTo(new CupRank(2));
+            });
+        }
+
+        @DisplayName("우선순위가 더 낮은 컵이 없는 경우, 그 어떤 컵의 우선순위도 승격되지 않는다.")
+        @Test
+        void success_withoutLowerPriorityCups() {
+            // given
+            when(cupRepository.findByIdAndMemberId(thirdCup.getId(), member.getId())).thenReturn(Optional.of(thirdCup));
+            when(cupRepository.findAllByMemberId(member.getId())).thenReturn(List.of(firstCup, secondCup));
+
+            // when
+            cupService.delete(thirdCup.getId(), member.getId());
+
+            // then
+            assertSoftly(softly -> {
+                softly.assertThat(firstCup.getCupRank()).isEqualTo(new CupRank(1));
+                softly.assertThat(secondCup.getCupRank()).isEqualTo(new CupRank(2));
             });
         }
     }
