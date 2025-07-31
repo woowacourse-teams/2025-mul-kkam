7cc10213dc2c3d935b9a2b71085911432439f4c2
Jin409 <sualng123@gmail.com>
2025-07-31 21:48:47 +0900

    feat: OpenWeather 과 통신할 수 있는 클라이언트 추가

---
+++ backend/src/main/java/backend/mulkkam/common/infrastructure/WeatherClient.java
@@ -0,0 +1,37 @@
+package backend.mulkkam.common.infrastructure;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestClient;
+
+public class WeatherClient {
+
+    private static final String URL = "https://api.openweathermap.org";
+
+    @Value("${open-weather.api.key}")
+    private String apiKey;
+
+    private final RestClient restClient;
+
+    public WeatherClient() {
+        this.restClient = RestClient.builder()
+                .baseUrl(URL)
+                .defaultHeader(
+                        HttpHeaders.CONTENT_TYPE,
+                        MediaType.APPLICATION_JSON_VALUE
+                )
+                .build();
+    }
+
+    public String getCurrentWeather(String city) {
+        return restClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/data/2.5/forecast")
+                        .queryParam("id", city)
+                        .queryParam("appid", apiKey)
+                        .build())
+                .retrieve()
+                .body(String.class);
+    }
+}
