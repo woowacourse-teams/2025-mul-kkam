77746dad43fd517aca3dd54b8308b4be17afbaa9
hwannow <vnfdlvgid061@naver.com>
2025-07-31 19:04:05 +0900

    feat: 카카오 로그인 기능 추가

--- android/app/src/main/java/com/mulkkam/ui/onboarding/LoginActivity.kt
+++ android/app/src/main/java/com/mulkkam/ui/onboarding/LoginActivity.kt
@@ -1,11 +1,44 @@
 package com.mulkkam.ui.onboarding
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 import com.mulkkam.databinding.ActivityLoginBinding
 import com.mulkkam.ui.binding.BindingActivity
 
 class LoginActivity : BindingActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.clKakaoLogin.setOnClickListener {
+            val kakaoCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                if (error != null) {
+                    Log.e("kakao login", "카카오계정으로 로그인 실패", error)
+                } else if (token != null) {
+                    Toast.makeText(this, "카카오 로그인 성공 !", Toast.LENGTH_SHORT).show()
+                    Log.d("hwannow_log", token.accessToken)
+                }
+            }
+
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                    if (error != null) {
+                        Log.e("kakao login", "카카오톡으로 로그인 실패", error)
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+                        UserApiClient.instance.loginWithKakaoAccount(this, callback = kakaoCallback)
+                    } else if (token != null) {
+                        Toast.makeText(this, "카카오 로그인 성공 !", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = kakaoCallback)
+            }
+        }
     }
 }
