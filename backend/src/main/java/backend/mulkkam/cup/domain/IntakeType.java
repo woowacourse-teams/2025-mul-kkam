722d9feb3345512fe1748d20ec560379df032ebe
Jin409 <sualng123@gmail.com>
2025-07-31 16:27:06 +0900

    feat: 음용 타입 추가

--- backend/src/main/java/backend/mulkkam/cup/domain/IntakeType.java
+++ backend/src/main/java/backend/mulkkam/cup/domain/IntakeType.java
@@ -1,7 +1,19 @@
 package backend.mulkkam.cup.domain;
 
+import backend.mulkkam.common.exception.CommonException;
+import java.util.Arrays;
+
+import static backend.mulkkam.common.exception.errorCode.NotFoundErrorCode.NOT_FOUND_INTAKE_TYPE;
+
 public enum IntakeType {
     WATER(),
     COFFEE(),
     ;
+
+    public static IntakeType findByName(String name) {
+        return Arrays.stream(values())
+                .filter(intakeType -> intakeType.name().equalsIgnoreCase(name))
+                .findFirst()
+                .orElseThrow(() -> new CommonException(NOT_FOUND_INTAKE_TYPE));
+    }
 }
