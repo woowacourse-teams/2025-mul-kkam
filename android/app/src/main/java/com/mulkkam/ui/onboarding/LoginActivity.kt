fe6054ccc1d2d48822d42744d3ea7ca607cf28ec
hwannow <vnfdlvgid061@naver.com>
2025-07-31 16:31:32 +0900

    feat: 로그인 화면 UI 구현

---
+++ android/app/src/main/java/com/mulkkam/ui/onboarding/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.mulkkam.ui.onboarding
+
+import android.os.Bundle
+import com.mulkkam.databinding.ActivityLoginBinding
+import com.mulkkam.ui.binding.BindingActivity
+
+class LoginActivity : BindingActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
