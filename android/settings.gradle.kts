843fb9d32aa0b99cf4bb8b2ea7d81b8db702edec
hwannow <vnfdlvgid061@naver.com>
2025-08-01 18:57:03 +0900

    refactor: maven url 설정 방식 변경

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,7 +18,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://jitpack.io")
-        maven { url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/") }
+        maven("https://devrepo.kakao.com/nexus/content/groups/public/")
     }
 }
 
