54c5550601cabb7a063d4cea47ba2976ba4282bc
Jin409 <sualng123@gmail.com>
2025-08-01 11:24:59 +0900

    fix: member 엔티티 수정에 따른 data.sql 수정

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,4 +1,2 @@
-INSERT INTO MEMBER (target_amount, nickname, gender, weight)
-VALUES (1800, '칼리', 'FEMALE', '55.8');
-
-
+INSERT INTO MEMBER (target_amount, nickname, gender, weight, oauth_id)
+VALUES (1800, '칼리', 'FEMALE', '55.8', 'hello');
