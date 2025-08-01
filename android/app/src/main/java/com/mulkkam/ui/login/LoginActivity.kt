370c99774321256c49a050faca31d6a49595293c
hwannow <vnfdlvgid061@naver.com>
2025-08-01 15:06:17 +0900

    feat: 카카오 로그인 성공 시 스낵바 표시

--- android/app/src/main/java/com/mulkkam/ui/login/LoginActivity.kt
+++ android/app/src/main/java/com/mulkkam/ui/login/LoginActivity.kt
@@ -2,12 +2,13 @@ package com.mulkkam.ui.login
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.viewModels
+import com.google.android.material.snackbar.Snackbar
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
+import com.mulkkam.R
 import com.mulkkam.databinding.ActivityLoginBinding
 import com.mulkkam.ui.binding.BindingActivity
 
@@ -16,39 +17,52 @@ class LoginActivity : BindingActivity<ActivityLoginBinding>(ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.clKakaoLogin.setOnClickListener {
-            val kakaoCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-                if (error != null) {
-                    Log.e("kakao login", "카카오계정으로 로그인 실패", error)
-                } else if (token != null) {
-                    viewModel.login(token.accessToken)
-//                    Toast.makeText(this, "카카오 로그인 성공 !", Toast.LENGTH_SHORT).show()
-//                    Log.d("hwannow_log", token.toString())
-                }
-            }
+        initClickListeners()
+    }
 
-            if (UserApiClient.Companion.instance.isKakaoTalkLoginAvailable(this)) {
-                UserApiClient.Companion.instance.loginWithKakaoTalk(this) { token, error ->
-                    if (error != null) {
-                        Log.e("kakao login", "카카오톡으로 로그인 실패", error)
-                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                            return@loginWithKakaoTalk
-                        }
-                        UserApiClient.Companion.instance.loginWithKakaoAccount(
-                            this,
-                            callback = kakaoCallback,
-                        )
-                    } else if (token != null) {
-                        Toast.makeText(this, "카카오 로그인 성공 !", Toast.LENGTH_SHORT).show()
-                        finish()
-                    }
-                }
-            } else {
-                UserApiClient.Companion.instance.loginWithKakaoAccount(
-                    this,
-                    callback = kakaoCallback,
-                )
+    private fun initClickListeners() {
+        binding.clKakaoLogin.setOnClickListener {
+            loginWithKakao()
+        }
+    }
+
+    private fun loginWithKakao() {
+        val kakaoCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                Log.e("[Login Error]", "카카오 계정으로 로그인 실패", error)
+            } else if (token != null) {
+                showSnackBar(R.string.login_kakao_success)
+                viewModel.loginWithKakao(token.accessToken)
             }
         }
+
+        if (UserApiClient.Companion.instance.isKakaoTalkLoginAvailable(this)) {
+            UserApiClient.Companion.instance.loginWithKakaoTalk(this) { token, error ->
+                if (error != null) {
+                    Log.e("[Login Error]", "카카오톡으로 로그인 실패", error)
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+                    UserApiClient.Companion.instance.loginWithKakaoAccount(
+                        this,
+                        callback = kakaoCallback,
+                    )
+                } else if (token != null) {
+                    showSnackBar(R.string.login_kakao_success)
+                    viewModel.loginWithKakao(token.accessToken)
+                }
+            }
+        } else {
+            UserApiClient.Companion.instance.loginWithKakaoAccount(
+                this,
+                callback = kakaoCallback,
+            )
+        }
+    }
+
+    private fun showSnackBar(message: Int) {
+        Snackbar
+            .make(binding.root, message, Snackbar.LENGTH_SHORT)
+            .show()
     }
 }
