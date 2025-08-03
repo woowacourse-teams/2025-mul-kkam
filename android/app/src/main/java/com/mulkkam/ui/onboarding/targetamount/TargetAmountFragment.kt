f358a7f658f121437fe86a4735f747a7798379a4
hwannow <vnfdlvgid061@naver.com>
2025-08-03 22:56:36 +0900

    feat: 목표 섭취량 입력 화면 디테일 구현

--- android/app/src/main/java/com/mulkkam/ui/onboarding/targetamount/TargetAmountFragment.kt
+++ android/app/src/main/java/com/mulkkam/ui/onboarding/targetamount/TargetAmountFragment.kt
@@ -1,7 +1,12 @@
 package com.mulkkam.ui.onboarding.targetamount
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.TextAppearanceSpan
 import android.view.View
+import androidx.annotation.StyleRes
+import com.mulkkam.R
 import com.mulkkam.databinding.FragmentPhysicalInfoBinding.inflate
 import com.mulkkam.databinding.FragmentTargetAmountBinding
 import com.mulkkam.ui.binding.BindingFragment
@@ -15,5 +20,35 @@ class TargetAmountFragment :
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+        initAppearance()
+    }
+
+    private fun initAppearance() {
+        binding.tvViewLabel.text =
+            getAppearanceSpannable(
+                R.style.title1,
+                getString(R.string.target_amount_input_hint),
+                getString(R.string.target_amount_input_hint_highlight),
+            )
+    }
+
+    private fun getAppearanceSpannable(
+        @StyleRes typographyResId: Int,
+        fullText: String,
+        vararg highlightedText: String,
+    ): SpannableString {
+        val spannable = SpannableString(fullText)
+
+        highlightedText.forEach { target ->
+            var startIndex = fullText.indexOf(target)
+            spannable.setSpan(
+                TextAppearanceSpan(context, typographyResId),
+                startIndex,
+                startIndex + target.length,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
+            )
+        }
+
+        return spannable
     }
 }
