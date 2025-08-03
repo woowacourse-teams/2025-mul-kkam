ff3c1effea9c6200f9daa62b5611109f07919be0
Jin409 <sualng123@gmail.com>
2025-08-03 22:18:01 +0900

    feat: 요청 날짜 검증에 대한 테스트 추가

--- backend/src/main/java/backend/mulkkam/intake/service/WeatherService.java
+++ backend/src/main/java/backend/mulkkam/intake/service/WeatherService.java
@@ -1,32 +1,56 @@
 package backend.mulkkam.intake.service;
 
+import backend.mulkkam.common.exception.CommonException;
 import backend.mulkkam.intake.dto.OpenWeatherResponse;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static backend.mulkkam.common.exception.errorCode.BadRequestErrorCode.INVALID_FORECAST_TARGET_DATE;
 
 @RequiredArgsConstructor
 @Service
 public class WeatherService {
 
     private static final String SEOUL_CITY_CODE = "1835847";
+    private static final int AVAILABLE_DATE_RANGE_FOR_FORECAST = 5;
 
     private final WeatherClient weatherClient;
 
     public double getAverageTemperatureForDate(LocalDate targetDate) {
+        validateTargetDate(targetDate);
+
         OpenWeatherResponse weatherOfFourDays = weatherClient.getFourDayWeatherForecast(SEOUL_CITY_CODE);
         double averageTemperatureForDate = computeAverageTemperatureForDate(weatherOfFourDays, targetDate);
 
         return convertFromKelvinToCelsius(averageTemperatureForDate);
     }
 
-    private double computeAverageTemperatureForDate(OpenWeatherResponse response, LocalDate targetDate) {
+    private void validateTargetDate(LocalDate targetDate) {
+        LocalDate now = LocalDate.now(ZoneId.of("Asia/Seoul"));
+
+        if (!targetDate.isAfter(now)) {
+            throw new CommonException(INVALID_FORECAST_TARGET_DATE);
+        }
+
+        if (targetDate.isAfter(now.plusDays(AVAILABLE_DATE_RANGE_FOR_FORECAST))) {
+            throw new CommonException(INVALID_FORECAST_TARGET_DATE);
+        }
+
+    }
+
+    private double computeAverageTemperatureForDate(
+            OpenWeatherResponse response,
+            LocalDate targetDate
+    ) {
+        int offsetSeconds = response.city().timezone();
+        Duration offset = Duration.ofSeconds(offsetSeconds);
+
         return response.forecastEntries().stream()
                 .filter(entry -> entry.dtTxt()
-                        .atZone(ZoneOffset.UTC)
-                        .withZoneSameInstant(ZoneId.of("Asia/Seoul"))
+                        .plusSeconds(offset.getSeconds())
                         .toLocalDate()
                         .equals(targetDate))
                 .mapToDouble(entry -> entry.info().temp())
