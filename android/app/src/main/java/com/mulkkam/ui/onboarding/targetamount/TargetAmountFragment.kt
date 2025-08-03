e3208560925e0abe471aa67ff01e68dbd2bf3d0b
hwannow <vnfdlvgid061@naver.com>
2025-08-03 16:33:40 +0900

    feat: 목표 섭취량 화면 UI 구현

---
+++ android/app/src/main/java/com/mulkkam/ui/onboarding/targetamount/TargetAmountFragment.kt
@@ -0,0 +1,19 @@
+package com.mulkkam.ui.onboarding.targetamount
+
+import android.os.Bundle
+import android.view.View
+import com.mulkkam.databinding.FragmentPhysicalInfoBinding.inflate
+import com.mulkkam.databinding.FragmentTargetAmountBinding
+import com.mulkkam.ui.binding.BindingFragment
+
+class TargetAmountFragment :
+    BindingFragment<FragmentTargetAmountBinding>(
+        FragmentTargetAmountBinding::inflate,
+    ) {
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
