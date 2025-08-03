cebcabd511e747dacfabd644f6c8af51c13063df
hwannow <vnfdlvgid061@naver.com>
2025-08-03 22:06:58 +0900

    feat: 신체 정보 입력 화면 디테일 구현

--- android/app/src/main/java/com/mulkkam/ui/onboarding/physicalinfo/PhysicalInfoFragment.kt
+++ android/app/src/main/java/com/mulkkam/ui/onboarding/physicalinfo/PhysicalInfoFragment.kt
@@ -1,8 +1,13 @@
 package com.mulkkam.ui.onboarding.physicalinfo
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.TextAppearanceSpan
 import android.view.View
+import androidx.annotation.StyleRes
 import androidx.fragment.app.activityViewModels
+import com.mulkkam.R
 import com.mulkkam.databinding.FragmentPhysicalInfoBinding
 import com.mulkkam.ui.binding.BindingFragment
 import com.mulkkam.ui.onboarding.OnboardingViewModel
@@ -18,7 +23,37 @@ class PhysicalInfoFragment :
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+        initHighlight()
         initClickListeners()
+    }
+
+    private fun initHighlight() {
+        binding.tvViewLabel.text =
+            getAppearanceSpannable(
+                R.style.title1,
+                getString(R.string.physical_info_input_hint),
+                getString(R.string.physical_info_input_hint_highlight),
+            )
+    }
+
+    private fun getAppearanceSpannable(
+        @StyleRes typographyResId: Int,
+        fullText: String,
+        vararg highlightedText: String,
+    ): SpannableString {
+        val spannable = SpannableString(fullText)
+
+        highlightedText.forEach { target ->
+            var startIndex = fullText.indexOf(target)
+            spannable.setSpan(
+                TextAppearanceSpan(context, typographyResId),
+                startIndex,
+                startIndex + target.length,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
+            )
+        }
+
+        return spannable
     }
 
     private fun initClickListeners() {
