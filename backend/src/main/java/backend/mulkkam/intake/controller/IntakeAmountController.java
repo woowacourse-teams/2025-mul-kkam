4d7054bc29d3dbe90d8dd2f103ee2862fe49c2fa
2jin2.1031 <2jin2.1031@gmail.com>
2025-08-04 21:56:01 +0900

    feat: 신체 속성으로 추천 음용량 조회 기능 구현

--- backend/src/main/java/backend/mulkkam/intake/controller/IntakeAmountController.java
+++ backend/src/main/java/backend/mulkkam/intake/controller/IntakeAmountController.java
@@ -3,7 +3,9 @@ package backend.mulkkam.intake.controller;
 import backend.mulkkam.intake.dto.IntakeRecommendedAmountResponse;
 import backend.mulkkam.intake.dto.IntakeTargetAmountModifyRequest;
 import backend.mulkkam.intake.dto.IntakeTargetAmountResponse;
+import backend.mulkkam.intake.dto.PhysicalAttributesRequest;
 import backend.mulkkam.intake.service.IntakeAmountService;
+import backend.mulkkam.intake.dto.RecommendedIntakeAmountResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +25,14 @@ public class IntakeAmountController {
     public ResponseEntity<IntakeRecommendedAmountResponse> getRecommended() {
         IntakeRecommendedAmountResponse intakeRecommendedAmountResponse = intakeAmountService.getRecommended(1L);
         return ResponseEntity.ok(intakeRecommendedAmountResponse);
+    }
+
+    @GetMapping("/target/recommended")
+    public ResponseEntity<RecommendedIntakeAmountResponse> getRecommendedTargetAmount(
+            @RequestBody PhysicalAttributesRequest physicalAttributesRequest
+    ) {
+        RecommendedIntakeAmountResponse recommendedIntakeAmountResponse = intakeAmountService.getRecommendedTargetAmount(physicalAttributesRequest);
+        return ResponseEntity.ok().body(recommendedIntakeAmountResponse);
     }
 
     @PatchMapping("/target")
