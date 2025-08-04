3a689a9dc3919c447f6905586b0fdc5da79029c0
2jin2.1031 <2jin2.1031@gmail.com>
2025-08-04 21:56:17 +0900

    test: 신체 속성으로 추천 음용량 조회 기능 테스트 추가

--- backend/src/test/java/backend/mulkkam/intake/service/IntakeAmountServiceUnitTest.java
+++ backend/src/test/java/backend/mulkkam/intake/service/IntakeAmountServiceUnitTest.java
@@ -1,22 +1,5 @@
 package backend.mulkkam.intake.service;
 
-import backend.mulkkam.common.exception.CommonException;
-import backend.mulkkam.common.exception.errorCode.NotFoundErrorCode;
-import backend.mulkkam.intake.domain.vo.Amount;
-import backend.mulkkam.intake.dto.IntakeRecommendedAmountResponse;
-import backend.mulkkam.intake.dto.IntakeTargetAmountModifyRequest;
-import backend.mulkkam.member.domain.Member;
-import backend.mulkkam.member.repository.MemberRepository;
-import backend.mulkkam.support.MemberFixtureBuilder;
-import java.util.Optional;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -25,6 +8,27 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+
+import backend.mulkkam.common.exception.CommonException;
+import backend.mulkkam.common.exception.errorCode.NotFoundErrorCode;
+import backend.mulkkam.intake.domain.vo.Amount;
+import backend.mulkkam.intake.dto.IntakeRecommendedAmountResponse;
+import backend.mulkkam.intake.dto.IntakeTargetAmountModifyRequest;
+import backend.mulkkam.intake.dto.PhysicalAttributesRequest;
+import backend.mulkkam.intake.dto.RecommendedIntakeAmountResponse;
+import backend.mulkkam.member.domain.Member;
+import backend.mulkkam.member.domain.vo.Gender;
+import backend.mulkkam.member.repository.MemberRepository;
+import backend.mulkkam.support.MemberFixtureBuilder;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class IntakeAmountServiceUnitTest {
@@ -99,6 +103,12 @@ public class IntakeAmountServiceUnitTest {
     class GetRecommended {
 
         public static final long MEMBER_ID = 1L;
+        private IntakeAmountService intakeAmountService;
+
+        @BeforeEach
+        void setUp() {
+            intakeAmountService = new IntakeAmountService(memberRepository, new SimpleWeightBasedCalculator());
+        }
 
         @DisplayName("멤버의 신체 정보에 따라 추천 음용량이 계산된다")
         @Test
@@ -138,6 +148,46 @@ public class IntakeAmountServiceUnitTest {
 
             // then
             assertThat(intakeRecommendedAmountResponse.amount()).isEqualTo(1_800);
+        }
+    }
+
+    @DisplayName("사용자의 신체적 속성으로 추천 음용량을 조회하려고 할 때")
+    @Nested
+    class GetRecommendedTargetAmount {
+
+        private IntakeAmountService intakeAmountService;
+
+        @BeforeEach
+        void setUp() {
+            intakeAmountService = new IntakeAmountService(memberRepository, new SimpleWeightBasedCalculator());
+        }
+
+        @DisplayName("멤버의 신체 정보에 따라 추천 음용량이 계산된다")
+        @Test
+        void success_physicalAttributes() {
+            // given
+            PhysicalAttributesRequest physicalAttributesRequest = new PhysicalAttributesRequest(Gender.FEMALE, 60.0);
+
+            // when
+            RecommendedIntakeAmountResponse recommendedTargetAmount = intakeAmountService.getRecommendedTargetAmount(
+                    physicalAttributesRequest);
+
+            // then
+            assertThat(recommendedTargetAmount.amount()).isEqualTo(1_800);
+        }
+
+        @DisplayName("멤버 신체 정보가 없을 경우 기본 값들로 계산된다")
+        @Test
+        void success_physicalAttributesIsNotExisted() {
+            // given
+            PhysicalAttributesRequest physicalAttributesRequest = new PhysicalAttributesRequest(null, null);
+
+            // when
+            RecommendedIntakeAmountResponse recommendedTargetAmount = intakeAmountService.getRecommendedTargetAmount(
+                    physicalAttributesRequest);
+
+            // then
+            assertThat(recommendedTargetAmount.amount()).isEqualTo(1_800);
         }
     }
 }
