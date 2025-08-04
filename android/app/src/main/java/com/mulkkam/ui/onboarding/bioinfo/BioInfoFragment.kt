1497dab5f0912b2d7220c161f1d187de28114d88
hwannow <vnfdlvgid061@naver.com>
2025-08-04 17:48:08 +0900

    refactor: spannable 생성 함수를 확장 함수로 변경

--- android/app/src/main/java/com/mulkkam/ui/onboarding/bioinfo/BioInfoFragment.kt
+++ android/app/src/main/java/com/mulkkam/ui/onboarding/bioinfo/BioInfoFragment.kt
@@ -1,17 +1,14 @@
 package com.mulkkam.ui.onboarding.bioinfo
 
 import android.os.Bundle
-import android.text.SpannableString
-import android.text.Spanned
-import android.text.style.TextAppearanceSpan
 import android.view.View
-import androidx.annotation.StyleRes
 import androidx.fragment.app.activityViewModels
 import com.mulkkam.R
 import com.mulkkam.databinding.FragmentBioInfoBinding
 import com.mulkkam.ui.binding.BindingFragment
 import com.mulkkam.ui.onboarding.OnboardingViewModel
 import com.mulkkam.ui.onboarding.bioinfo.dialog.WeightFragment
+import com.mulkkam.ui.util.getAppearanceSpannable
 
 class BioInfoFragment :
     BindingFragment<FragmentBioInfoBinding>(
@@ -30,33 +27,11 @@ class BioInfoFragment :
 
     private fun initTextAppearance() {
         binding.tvViewLabel.text =
-            getAppearanceSpannable(
+            getString(R.string.bio_info_input_hint).getAppearanceSpannable(
+                requireContext(),
                 R.style.title1,
-                getString(R.string.bio_info_input_hint),
                 getString(R.string.bio_info_input_hint_highlight),
             )
-    }
-
-    private fun getAppearanceSpannable(
-        @StyleRes typographyResId: Int,
-        fullText: String,
-        vararg highlightedText: String,
-    ): SpannableString {
-        val spannable = SpannableString(fullText)
-
-        highlightedText.forEach { target ->
-            var startIndex = fullText.indexOf(target)
-            if (startIndex != -1) {
-                spannable.setSpan(
-                    TextAppearanceSpan(context, typographyResId),
-                    startIndex,
-                    startIndex + target.length,
-                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
-                )
-            }
-        }
-
-        return spannable
     }
 
     private fun initClickListeners() {
--- android/app/src/main/java/com/mulkkam/ui/onboarding/targetamount/TargetAmountFragment.kt
+++ android/app/src/main/java/com/mulkkam/ui/onboarding/targetamount/TargetAmountFragment.kt
@@ -1,15 +1,12 @@
 package com.mulkkam.ui.onboarding.targetamount
 
 import android.os.Bundle
-import android.text.SpannableString
-import android.text.Spanned
-import android.text.style.TextAppearanceSpan
 import android.view.View
-import androidx.annotation.StyleRes
 import com.mulkkam.R
 import com.mulkkam.databinding.FragmentTargetAmountBinding
 import com.mulkkam.ui.binding.BindingFragment
 import com.mulkkam.ui.onboarding.dialog.CompleteDialogFragment
+import com.mulkkam.ui.util.getAppearanceSpannable
 
 class TargetAmountFragment :
     BindingFragment<FragmentTargetAmountBinding>(
@@ -26,33 +23,11 @@ class TargetAmountFragment :
 
     private fun initTextAppearance() {
         binding.tvViewLabel.text =
-            getAppearanceSpannable(
+            getString(R.string.target_amount_input_hint).getAppearanceSpannable(
+                requireContext(),
                 R.style.title1,
-                getString(R.string.target_amount_input_hint),
                 getString(R.string.target_amount_input_hint_highlight),
             )
-    }
-
-    private fun getAppearanceSpannable(
-        @StyleRes typographyResId: Int,
-        fullText: String,
-        vararg highlightedText: String,
-    ): SpannableString {
-        val spannable = SpannableString(fullText)
-
-        highlightedText.forEach { target ->
-            var startIndex = fullText.indexOf(target)
-            if (startIndex != -1) {
-                spannable.setSpan(
-                    TextAppearanceSpan(context, typographyResId),
-                    startIndex,
-                    startIndex + target.length,
-                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
-                )
-            }
-        }
-
-        return spannable
     }
 
     private fun initClickListeners() {
