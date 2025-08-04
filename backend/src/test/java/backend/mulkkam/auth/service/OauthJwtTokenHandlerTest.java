6479bdbffa9f5eb260fd50c9744ea22d62aaff12
minSsan <msp1125@hanyang.ac.kr>
2025-08-04 19:24:45 +0900

    test(OauthJwtTokenHandler): jwt 토큰 생성 객체 테스트 추가

---
+++ backend/src/test/java/backend/mulkkam/auth/service/OauthJwtTokenHandlerTest.java
@@ -0,0 +1,41 @@
+package backend.mulkkam.auth.service;
+
+import backend.mulkkam.auth.domain.OauthAccount;
+import backend.mulkkam.auth.domain.OauthProvider;
+import backend.mulkkam.auth.repository.OauthAccountRepository;
+import backend.mulkkam.support.ServiceIntegrationTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OauthJwtTokenHandlerTest extends ServiceIntegrationTest {
+
+    @Autowired
+    private OauthJwtTokenHandler oauthJwtTokenHandler;
+
+    @Autowired
+    private OauthAccountRepository oauthAccountRepository;
+
+    @DisplayName("토큰 생성을 요청하면")
+    @Nested
+    class CreateToken {
+
+        @DisplayName("생성된 토큰에서 account id 값을 subject로 추출할 수 있다.")
+        @Test
+        void success_withAccount() {
+            // given
+            OauthAccount oauthAccount = new OauthAccount("testId", OauthProvider.KAKAO);
+            oauthAccountRepository.save(oauthAccount);
+
+            // when
+            String token = oauthJwtTokenHandler.createToken(oauthAccount);
+            Long actual = oauthJwtTokenHandler.getSubject(token);
+
+            // then
+            assertThat(actual).isEqualTo(oauthAccount.getId());
+        }
+    }
+}
