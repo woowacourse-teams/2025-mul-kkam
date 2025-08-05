5cbb505216b102c8c2278acaa6e6f7b6c8a4fe01
hwannow <vnfdlvgid061@naver.com>
2025-08-05 17:07:20 +0900

    refactor: 불필요한 HttpLoggingInterceptor 제거

--- android/app/src/main/java/com/mulkkam/di/NetworkInjection.kt
+++ android/app/src/main/java/com/mulkkam/di/NetworkInjection.kt
@@ -10,24 +10,10 @@ import com.mulkkam.data.remote.service.IntakeService
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
 object NetworkInjection {
     private val contentType = "application/json".toMediaType()
-
-    private val httpLoggingInterceptor: HttpLoggingInterceptor by lazy {
-        HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY
-        }
-    }
-
-    private val okHttpClient: OkHttpClient by lazy {
-        OkHttpClient
-            .Builder()
-            .addInterceptor(httpLoggingInterceptor)
-            .build()
-    }
 
     private val interceptorClient =
         OkHttpClient()
@@ -39,7 +25,6 @@ object NetworkInjection {
         Retrofit
             .Builder()
             .baseUrl(BuildConfig.BASE_URL)
-            .client(okHttpClient)
             .client(interceptorClient)
             .addCallAdapterFactory(MulKkamCallAdapterFactory())
             .addConverterFactory(Json.asConverterFactory(contentType))
