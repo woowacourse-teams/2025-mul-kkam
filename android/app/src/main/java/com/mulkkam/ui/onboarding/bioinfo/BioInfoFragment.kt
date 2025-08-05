4a16ce29bf32858afe890a19bb7ef38345bfb950
hwannow <vnfdlvgid061@naver.com>
2025-08-05 19:59:58 +0900

    feat: 신체 정보 입력 화면 UI 및 로직 구현

--- android/app/src/main/java/com/mulkkam/ui/onboarding/bioinfo/BioInfoFragment.kt
+++ android/app/src/main/java/com/mulkkam/ui/onboarding/bioinfo/BioInfoFragment.kt
@@ -1,12 +1,17 @@
 package com.mulkkam.ui.onboarding.bioinfo
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.activityViewModels
 import com.mulkkam.R
 import com.mulkkam.databinding.FragmentBioInfoBinding
 import com.mulkkam.ui.binding.BindingFragment
 import com.mulkkam.ui.onboarding.OnboardingViewModel
+import com.mulkkam.ui.onboarding.bioinfo.Gender.FEMALE
+import com.mulkkam.ui.onboarding.bioinfo.Gender.MALE
 import com.mulkkam.ui.onboarding.bioinfo.dialog.WeightFragment
 import com.mulkkam.ui.util.getAppearanceSpannable
 
@@ -18,6 +23,7 @@ class BioInfoFragment :
         WeightFragment()
     }
     private val parentViewModel: OnboardingViewModel by activityViewModels()
+    private val viewModel: BioInfoViewModel by activityViewModels()
 
     override fun onViewCreated(
         view: View,
@@ -26,6 +32,7 @@ class BioInfoFragment :
         super.onViewCreated(view, savedInstanceState)
         initTextAppearance()
         initClickListeners()
+        initObservers()
     }
 
     private fun initTextAppearance() {
@@ -44,6 +51,69 @@ class BioInfoFragment :
 
         binding.tvWeight.setOnClickListener {
             weightFragment.show(parentFragmentManager, null)
+        }
+
+        binding.tvGenderMale.setOnClickListener {
+            viewModel.updateGender(MALE)
+        }
+
+        binding.tvGenderFemale.setOnClickListener {
+            viewModel.updateGender(FEMALE)
+        }
+    }
+
+    private fun initObservers() {
+        viewModel.weight.observe(viewLifecycleOwner) { weight ->
+            binding.tvWeight.text = getString(R.string.bio_info_weight_format, weight)
+        }
+
+        viewModel.gender.observe(viewLifecycleOwner) { selectedGender ->
+            changeGender(selectedGender)
+        }
+
+        viewModel.canNext.observe(viewLifecycleOwner) { enabled ->
+            updateNextButtonState(enabled)
+        }
+    }
+
+    private fun changeGender(selectedGender: Gender) {
+        if (selectedGender == MALE) {
+            selectGender(binding.tvGenderMale)
+            deselectGender(binding.tvGenderFemale)
+        } else {
+            selectGender(binding.tvGenderFemale)
+            deselectGender(binding.tvGenderMale)
+        }
+    }
+
+    private fun selectGender(gender: TextView) {
+        with(gender) {
+            isSelected = true
+            setTextColor(getColor(requireContext(), R.color.white))
+            backgroundTintList =
+                ColorStateList.valueOf(getColor(requireContext(), R.color.primary_100))
+        }
+    }
+
+    private fun deselectGender(gender: TextView) {
+        with(gender) {
+            isSelected = false
+            setTextColor(getColor(requireContext(), R.color.gray_400))
+            backgroundTintList =
+                ColorStateList.valueOf(
+                    getColor(
+                        requireContext(),
+                        R.color.gray_200,
+                    ),
+                )
+        }
+    }
+
+    private fun updateNextButtonState(enabled: Boolean) {
+        binding.tvNext.isEnabled = enabled
+        if (enabled) {
+            binding.tvNext.backgroundTintList =
+                ColorStateList.valueOf(getColor(requireContext(), R.color.primary_200))
         }
     }
 }
