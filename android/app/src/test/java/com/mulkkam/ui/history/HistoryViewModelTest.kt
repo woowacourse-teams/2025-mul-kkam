0b9bcea8f3addf726458de833f03e6f6c0289314
hwannow <vnfdlvgid061@naver.com>
2025-08-06 10:59:58 +0900

    test: 기록 화면 UI 테스트 코드 제거

--- android/app/src/test/java/com/mulkkam/ui/history/HistoryViewModelTest.kt
+++ android/app/src/test/java/com/mulkkam/ui/history/HistoryViewModelTest.kt
@@ -1,8 +1,10 @@
 package com.mulkkam.ui.history
 
-import com.mulkkam.data.repository.IntakeRepository
 import com.mulkkam.di.RepositoryInjection
+import com.mulkkam.domain.IntakeHistorySummaries
 import com.mulkkam.domain.IntakeHistorySummary
+import com.mulkkam.domain.MulKkamResult
+import com.mulkkam.domain.repository.IntakeRepository
 import com.mulkkam.ui.fixture.FULL_INTAKE_HISTORY
 import com.mulkkam.ui.util.CoroutinesTestExtension
 import com.mulkkam.ui.util.InstantTaskExecutorExtension
@@ -38,30 +40,27 @@ class HistoryViewModelTest {
     }
 
     @Test
-    @DisplayName("오늘을 기준으로 주간 음용 데이터를 가져와 저장한다")
-    fun loadIntakeHistories() {
-        // given
-        val mockedHistories = listOf(FULL_INTAKE_HISTORY)
-        coEvery {
-            fakeIntakeRepository.getIntakeHistory(any(), any())
-        } returns mockedHistories
-
-        // when
-        historyViewModel.loadIntakeHistories()
-        val actual = historyViewModel.weeklyIntakeHistories.getOrAwaitValue()
-
-        // then
-        assertThat(actual).containsAll(mockedHistories)
-    }
-
-    @Test
-    @DisplayName("기록이 없는 날짜는 빈 기록을 생성한다")
+    @DisplayName("주간 음용량을 조회한다")
     fun loadIntakeHistories_noHistory() {
         // given
-        coEvery { fakeIntakeRepository.getIntakeHistory(any(), any()) } returns emptyList()
+        coEvery {
+            fakeIntakeRepository.getIntakeHistory(
+                any(),
+                any(),
+            )
+        } returns MulKkamResult(data = IntakeHistorySummaries(emptyList()))
         val monday = LocalDate.now().with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
         val expected =
-            List(7) { IntakeHistorySummary.EMPTY_DAILY_WATER_INTAKE.copy(date = monday.plusDays(it.toLong())) }
+            IntakeHistorySummaries(
+                List(7) {
+                    IntakeHistorySummary.EMPTY_DAILY_WATER_INTAKE.copy(
+                        date =
+                            monday.plusDays(
+                                it.toLong(),
+                            ),
+                    )
+                },
+            )
 
         // when
         historyViewModel.loadIntakeHistories()
