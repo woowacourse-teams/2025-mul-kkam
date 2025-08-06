62f4dd3f161fea8bfd9b091ff437099d0356dc35
junseo511 <prime54@g.skku.edu>
2025-08-06 20:07:32 +0900

    refactor: 나의 컵 설정 아이템 UI 변경

--- android/app/src/main/java/com/mulkkam/ui/settingcups/adapter/CupViewHolder.kt
+++ android/app/src/main/java/com/mulkkam/ui/settingcups/adapter/CupViewHolder.kt
@@ -1,7 +1,6 @@
 package com.mulkkam.ui.settingcups.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import com.mulkkam.R
 import com.mulkkam.databinding.ItemSettingCupsCupBinding
@@ -16,32 +15,22 @@ class CupViewHolder(
     override fun bind(item: SettingCupsItem.CupItem) {
         super.bind(item)
         showCupInfo(item)
-        handleRepresentativeCup(item)
         initClickListeners(item)
     }
 
     private fun showCupInfo(item: SettingCupsItem.CupItem) =
         with(binding) {
-            tvSettingWaterCupName.text = item.value.nickname
-            tvSettingWaterCupIncrement.text =
+            tvNickname.text = item.value.nickname
+            tvIncrement.text =
                 root.context.getString(
                     R.string.setting_cups_increment,
                     item.value.amount,
                 )
         }
 
-    private fun handleRepresentativeCup(item: SettingCupsItem.CupItem) =
-        with(binding) {
-            if (item.value.isRepresentative) {
-                tvSettingWaterCupTagRepresentative.visibility = View.VISIBLE
-            }
-        }
-
     private fun initClickListeners(item: SettingCupsItem.CupItem) =
-        with(binding) {
-            tvSettingWaterCupEdit.setOnClickListener {
-                handler.onEditClick(item.value)
-            }
+        binding.ivEdit.setOnClickListener {
+            handler.onEditClick(item.value)
         }
 
     interface Handler {
