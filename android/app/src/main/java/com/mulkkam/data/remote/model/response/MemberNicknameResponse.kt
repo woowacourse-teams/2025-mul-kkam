b759cbe542d8feb73ff2d8d39cac57d3667990ce
hwannow <vnfdlvgid061@naver.com>
2025-08-06 15:02:54 +0900

    style: 린트 적용

--- android/app/src/main/java/com/mulkkam/data/remote/model/response/MemberNicknameResponse.kt
+++ android/app/src/main/java/com/mulkkam/data/remote/model/response/MemberNicknameResponse.kt
@@ -1,11 +1,10 @@
 package com.mulkkam.data.remote.model.response
 
-
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class MemberNicknameResponse(
     @SerialName("memberNickname")
-    val memberNickname: String
+    val memberNickname: String,
 )
