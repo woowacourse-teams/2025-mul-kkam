433a6e7145a556dae13a49876280cdff24e53b66
hwannow <vnfdlvgid061@naver.com>
2025-08-06 15:44:50 +0900

    feat: 신체 정보 설정 화면 추가

---
+++ android/app/src/main/java/com/mulkkam/ui/settingbioinfo/SettingBioInfoActivity.kt
@@ -0,0 +1,14 @@
+package com.mulkkam.ui.settingbioinfo
+
+import android.os.Bundle
+import com.mulkkam.databinding.ActivitySettingBioInfoBinding
+import com.mulkkam.ui.binding.BindingActivity
+
+class SettingBioInfoActivity :
+    BindingActivity<ActivitySettingBioInfoBinding>(
+        ActivitySettingBioInfoBinding::inflate,
+    ) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
