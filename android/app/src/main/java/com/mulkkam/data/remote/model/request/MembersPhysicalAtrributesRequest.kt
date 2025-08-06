1285712c50cdb8abb2ef32cedd9de2983c0f3907
hwannow <vnfdlvgid061@naver.com>
2025-08-06 17:51:41 +0900

    style: 린트 적용

--- android/app/src/main/java/com/mulkkam/data/remote/model/request/MembersPhysicalAtrributesRequest.kt
+++ android/app/src/main/java/com/mulkkam/data/remote/model/request/MembersPhysicalAtrributesRequest.kt
@@ -1,6 +1,5 @@
 package com.mulkkam.data.remote.model.request
 
-
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -9,5 +8,5 @@ data class MembersPhysicalAtrributesRequest(
     @SerialName("gender")
     val gender: String,
     @SerialName("weight")
-    val weight: Double
+    val weight: Double,
 )
