d547d575e87f43559bfbbbbac5cd15176c94ce44
minSsan <msp1125@hanyang.ac.kr>
2025-08-06 23:33:56 +0900

    style(OauthJwtTokenHandler): 컨벤션에 따라 파라미터 final 키워드 삭제

--- backend/src/main/java/backend/mulkkam/auth/infrastructure/OauthJwtTokenHandler.java
+++ backend/src/main/java/backend/mulkkam/auth/infrastructure/OauthJwtTokenHandler.java
@@ -55,7 +55,7 @@ public class OauthJwtTokenHandler {
         }
     }
 
-    private Claims getClaims(final String token) {
+    private Claims getClaims(String token) {
         try {
             return parser.parseSignedClaims(token).getPayload();
         } catch (JwtException | IllegalArgumentException e) {
