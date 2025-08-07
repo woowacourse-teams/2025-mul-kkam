5627efb84a9e212f2ad793b1b2d85694e5a0fe06
CheChe903 <cheche903@naver.com>
2025-08-07 21:06:19 +0900

    feat: API 응답 속도 로깅 구현

--- backend/src/main/java/backend/mulkkam/common/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/backend/mulkkam/common/filter/JwtAuthenticationFilter.java
@@ -5,22 +5,22 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private static final List<HttpEndpoint> EXCLUDE_ENDPOINTS = List.of(
-        HttpEndpoint.of("/auth", HttpMethod.POST),
-        HttpEndpoint.of("/swagger-ui", HttpMethod.GET),
-        HttpEndpoint.of("/v3/api-docs", HttpMethod.GET)
+            HttpEndpoint.of("/auth", HttpMethod.POST),
+            HttpEndpoint.of("/swagger-ui", HttpMethod.GET),
+            HttpEndpoint.of("/v3/api-docs", HttpMethod.GET),
+            HttpEndpoint.of("/nickname/validation", HttpMethod.GET)
     );
 
     private final AuthenticationHeaderHandler authenticationHeaderHandler;
