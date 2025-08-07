c62cc99eb014ba3bc27d477883112cf738196215
hwannow <vnfdlvgid061@naver.com>
2025-08-07 22:54:32 +0900

    refactor: 불필요한 상수 제거

--- android/app/src/main/java/com/mulkkam/ui/home/HomeFragment.kt
+++ android/app/src/main/java/com/mulkkam/ui/home/HomeFragment.kt
@@ -178,8 +178,6 @@ class HomeFragment :
 
     companion object {
         private const val PROGRESS_BAR_RADIUS: Float = 12f
-        private const val MANUAL_DRINK_IMAGE: String =
-            "https://github-production-user-asset-6210df.s3.amazonaws.com/127238018/474919237-4e25a9f8-ab08-46e4-bd01-578d2de907df.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20250806%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250806T085526Z&X-Amz-Expires=300&X-Amz-Signature=2c41117c496fdf0a94dd9062232cc396e7e44f58048958a92185c836d1caf5d4&X-Amz-SignedHeaders=host"
         private const val ALARM_COUNT_MIN: Int = 0
     }
 }
