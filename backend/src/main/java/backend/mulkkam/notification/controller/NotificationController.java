aa7aa7823e718dc4705adb8f556b6de61adcf526
kali <111180367+2Jin1031@users.noreply.github.com>
2025-08-07 09:29:19 +0900

    Update backend/src/main/java/backend/mulkkam/notification/controller/NotificationController.java
    Co-authored-by: Seunghee Jin <77621712+Jin409@users.noreply.github.com>

--- backend/src/main/java/backend/mulkkam/notification/controller/NotificationController.java
+++ backend/src/main/java/backend/mulkkam/notification/controller/NotificationController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("notifications")
+@RequestMapping("/notifications")
 public class NotificationController {
 
     private final NotificationService notificationService;
