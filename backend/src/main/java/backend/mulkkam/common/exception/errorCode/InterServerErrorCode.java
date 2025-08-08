99337f954a457ebd62cc99a351a32ce790396d80
Jin409 <sualng123@gmail.com>
2025-08-08 11:27:38 +0900

    feat: InterServerErrorCode 추가

---
+++ backend/src/main/java/backend/mulkkam/common/exception/errorCode/InterServerErrorCode.java
@@ -0,0 +1,15 @@
+package backend.mulkkam.common.exception.errorCode;
+
+import org.springframework.http.HttpStatus;
+
+public enum InterServerErrorCode implements ErrorCode {
+
+    INTER_SERVER_ERROR_CODE,
+    ;
+
+
+    @Override
+    public HttpStatus getStatus() {
+        return HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+}
