a6bde790a3117b13cf9525e70ef7ae36e91e47cd
junseo511 <prime54@g.skku.edu>
2025-08-10 03:07:27 +0900

    feat: 키보드 올라올 때 저장 버튼 함께 이동
    - 키보드가 올라올 때 저장 버튼이 가려지는 현상 수정
    - 안드로이드 11 이상에서 제공하는 WindowInsetsAnimationCompat.Callback을 사용하여 키보드 애니메이션과 동기화

---
+++ android/app/src/main/java/com/mulkkam/util/extensions/ViewExtensions.kt
@@ -0,0 +1,46 @@
+package com.mulkkam.util.extensions
+
+import android.view.View
+import androidx.core.graphics.Insets
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsAnimationCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.math.max
+
+fun View.applyImeMargin(extraBottomSpace: Int = 0) {
+    var startTranslationY = translationY
+    var endTranslationY = translationY
+
+    ViewCompat.setOnApplyWindowInsetsListener(this) { _, insets ->
+        val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        val ime = insets.getInsets(WindowInsetsCompat.Type.ime())
+        val overlap = max(0, ime.bottom - systemBars.bottom) + extraBottomSpace
+        endTranslationY = -overlap.toFloat()
+        if (!isImeAnimating(insets)) translationY = endTranslationY
+        insets
+    }
+
+    ViewCompat.setWindowInsetsAnimationCallback(
+        this,
+        object : WindowInsetsAnimationCompat.Callback(DISPATCH_MODE_CONTINUE_ON_SUBTREE) {
+            override fun onPrepare(animation: WindowInsetsAnimationCompat) {
+                startTranslationY = translationY
+            }
+
+            override fun onProgress(
+                insets: WindowInsetsCompat,
+                running: MutableList<WindowInsetsAnimationCompat>,
+            ): WindowInsetsCompat {
+                val imeAnim = running.firstOrNull { it.typeMask and WindowInsetsCompat.Type.ime() != 0 } ?: return insets
+                val fraction = imeAnim.interpolatedFraction.coerceIn(0f, 1f)
+                translationY = startTranslationY + (endTranslationY - startTranslationY) * fraction
+                return insets
+            }
+        },
+    )
+}
+
+private fun isImeAnimating(insets: WindowInsetsCompat): Boolean {
+    val ime = insets.getInsets(WindowInsetsCompat.Type.ime())
+    return ime != Insets.NONE
+}
