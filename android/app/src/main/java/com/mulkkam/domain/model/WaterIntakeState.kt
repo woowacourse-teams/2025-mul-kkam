7dc6d11c8b0b9a4f1272f47efdabb1af35b7f35c
hwannow <vnfdlvgid061@naver.com>
2025-08-09 18:55:32 +0900

    feat: 물 섭취 상태 모델 구현

---
+++ android/app/src/main/java/com/mulkkam/domain/model/WaterIntakeState.kt
@@ -0,0 +1,19 @@
+package com.mulkkam.domain.model
+
+sealed interface WaterIntakeState {
+    sealed interface Past : WaterIntakeState {
+        data object NoRecord : Past
+
+        data object Partial : Past
+
+        data object Full : Past
+    }
+
+    sealed interface Present : WaterIntakeState {
+        data object NotFull : Present
+
+        data object Full : Present
+    }
+
+    data object Future : WaterIntakeState
+}
