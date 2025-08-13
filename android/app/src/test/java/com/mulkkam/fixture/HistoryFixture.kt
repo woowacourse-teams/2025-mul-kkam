4874a78df523a0d1d1fd41a43e54ec4e69f4fdc8
hwannow <vnfdlvgid061@naver.com>
2025-08-13 13:01:35 +0900

    test: IntakeHistorySummary 테스트 코드 작성

--- android/app/src/test/java/com/mulkkam/fixture/HistoryFixture.kt
+++ android/app/src/test/java/com/mulkkam/fixture/HistoryFixture.kt
@@ -36,16 +36,53 @@ val FULL_INTAKE_HISTORY =
                     intakeAmount = 300,
                 ),
                 IntakeHistory(
-                    id = 1,
+                    id = 2,
                     dateTime = LocalTime.of(11, 0),
                     intakeAmount = 400,
                 ),
                 IntakeHistory(
-                    id = 1,
+                    id = 3,
                     dateTime = LocalTime.of(12, 0),
                     intakeAmount = 500,
                 ),
             ),
+    )
+
+val HALF_INTAKE_HISTORY =
+    IntakeHistorySummary(
+        date = LocalDate.now(),
+        totalIntakeAmount = 600,
+        targetAmount = 1200,
+        achievementRate = 50f,
+        intakeHistories =
+            listOf(
+                IntakeHistory(
+                    id = 1,
+                    dateTime = LocalTime.of(10, 0),
+                    intakeAmount = 300,
+                ),
+                IntakeHistory(
+                    id = 2,
+                    dateTime = LocalTime.of(11, 0),
+                    intakeAmount = 300,
+                ),
+            ),
+    )
+
+val ZERO_INTAKE_HISTORY =
+    IntakeHistorySummary(
+        date = LocalDate.now(),
+        totalIntakeAmount = 0,
+        targetAmount = 1200,
+        achievementRate = 0f,
+        intakeHistories = emptyList(),
+    )
+
+val SAMPLE_INTAKE_HISTORY =
+    IntakeHistory(
+        id = 1,
+        dateTime = LocalTime.of(10, 0),
+        intakeAmount = 300,
     )
 
 private const val WEEK_LENGTH: Int = 7
