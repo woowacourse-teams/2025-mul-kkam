e07f8f0c9f1007d7aa58fd10ec1f75d3b10f407b
minSsan <msp1125@hanyang.ac.kr>
2025-08-13 22:14:06 +0900

    docs(AuthController): 로그아웃 API 명세서 작성

--- backend/src/main/java/backend/mulkkam/auth/controller/AuthController.java
+++ backend/src/main/java/backend/mulkkam/auth/controller/AuthController.java
@@ -1,22 +1,25 @@
 package backend.mulkkam.auth.controller;
 
+import backend.mulkkam.auth.domain.OauthAccount;
 import backend.mulkkam.auth.dto.KakaoSigninRequest;
 import backend.mulkkam.auth.dto.OauthLoginResponse;
 import backend.mulkkam.auth.service.KakaoAuthService;
 import backend.mulkkam.common.exception.FailureBody;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "로그인 및 회원가입", description = "사용자 로그인 / 회원가입 관련 API")
+@Tag(name = "계정 관리", description = "사용자 계정 로그인 / 로그아웃 / 회원가입 / 회원탈퇴 관련 API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
@@ -35,5 +38,13 @@ public class AuthController {
             KakaoSigninRequest kakaoSigninRequest
     ) {
         return kakaoAuthService.signIn(kakaoSigninRequest);
+    }
+
+    @Operation(summary = "로그아웃", description = "사용자를 로그아웃 상태로 기록합니다. 성공 응답 수신시 액세스 토큰과 리프레시 토큰을 삭제해주셔야 합니다.")
+    @ApiResponse(responseCode = "204", description = "성공 응답")
+    @ApiResponse(responseCode = "401", description = "인증 실패")
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@Parameter(hidden = true) OauthAccount oauthAccount) {
+        return ResponseEntity.noContent().build();
     }
 }
