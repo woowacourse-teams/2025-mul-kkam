2319678860e2e65ae17ccf756e25a8cf15447c9a
minSsan <msp1125@hanyang.ac.kr>
2025-08-13 10:54:12 +0900

    fix: migration sql fk 연관관계 추가

--- backend/src/main/resources/db/migration/V11__add_account_refresh_token.sql
+++ backend/src/main/resources/db/migration/V11__add_account_refresh_token.sql
@@ -1,5 +1,8 @@
 CREATE TABLE account_refresh_token(
     id              BIGINT AUTO_INCREMENT PRIMARY KEY,
     account_id      BIGINT  NOT NULL,
-    refresh_token   VARCHAR(255) NOT NULL
+    refresh_token   VARCHAR(255) NOT NULL,
+
+    CONSTRAINT fk_oauth_account_id
+        FOREIGN KEY (account_id) REFERENCES oauth_account (id)
 );
