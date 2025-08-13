67c7e350ad7a4c67a01a09406a9579dddfbe94e7
hwannow <vnfdlvgid061@naver.com>
2025-08-13 14:19:23 +0900

    test: Cups 테스트 코드 추가

---
+++ android/app/src/test/java/com/mulkkam/fixture/CupFixture.kt
@@ -0,0 +1,34 @@
+package com.mulkkam.fixture
+
+import com.mulkkam.domain.model.cups.Cup
+import com.mulkkam.domain.model.intake.IntakeType
+
+val HWANNOW_CUP =
+    Cup(
+        id = 1,
+        nickname = "hwannow",
+        amount = 300,
+        rank = 1,
+        intakeType = IntakeType.WATER,
+        emoji = "",
+    )
+
+val CHECHE_CUP =
+    Cup(
+        id = 2,
+        nickname = "cheche",
+        amount = 200,
+        rank = 2,
+        intakeType = IntakeType.WATER,
+        emoji = "",
+    )
+
+val KALI_CUP =
+    Cup(
+        id = 3,
+        nickname = "kali",
+        amount = 500,
+        rank = 3,
+        intakeType = IntakeType.WATER,
+        emoji = "",
+    )
