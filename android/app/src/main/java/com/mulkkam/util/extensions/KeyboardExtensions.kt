f5b6981820377a44e93f4557015247e4d2d239e1
devfeijoa <devfeijoa@gmail.com>
2025-08-14 00:17:54 +0900

    feat: 키보드 관련 익스텐션 함수 추가
    키보드를 숨기는 `hideKeyboard` 함수와
    EditText에서 IME_ACTION_DONE 이벤트 발생 시 키보드를 숨기고 포커스를 해제하는 `setOnImeActionDoneListener` 함수를 추가

---
+++ android/app/src/main/java/com/mulkkam/util/extensions/KeyboardExtensions.kt
@@ -0,0 +1,24 @@
+package com.mulkkam.util.extensions
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+
+fun Context.hideKeyboard(view: View) {
+    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+    imm?.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun EditText.setOnImeActionDoneListener(context: Context) {
+    setOnEditorActionListener { view, actionId, _ ->
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            context.hideKeyboard(view)
+            view.clearFocus()
+            true
+        } else {
+            false
+        }
+    }
+}
