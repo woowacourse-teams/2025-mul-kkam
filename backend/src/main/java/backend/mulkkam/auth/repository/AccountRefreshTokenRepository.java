8f8efaacdb307de609d502a5ec0da0e37068a8fb
minSsan <msp1125@hanyang.ac.kr>
2025-08-13 15:03:19 +0900

    feat: 리프레시 토큰 매핑 테이블 레파지토리 추가

---
+++ backend/src/main/java/backend/mulkkam/auth/repository/AccountRefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package backend.mulkkam.auth.repository;
+
+import backend.mulkkam.auth.domain.AccountRefreshToken;
+import backend.mulkkam.auth.domain.OauthAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRefreshTokenRepository extends JpaRepository<AccountRefreshToken, Long> {
+
+    Optional<AccountRefreshToken> findByAccount(OauthAccount account);
+}
