f41a5d2b647d464fe880519c1d5dd9169df5e5a8
Jin409 <sualng123@gmail.com>
2025-08-14 16:23:23 +0900

    test: 컨트롤러 테스트 추가

--- backend/src/test/java/backend/mulkkam/version/controller/AppMinimumVersionControllerTest.java
+++ backend/src/test/java/backend/mulkkam/version/controller/AppMinimumVersionControllerTest.java
@@ -1,11 +1,57 @@
 package backend.mulkkam.version.controller;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import backend.mulkkam.support.AppMinimumVersionFixtureBuilder;
+import backend.mulkkam.support.ControllerTest;
+import backend.mulkkam.version.domain.AppMinimumVersion;
+import backend.mulkkam.version.dto.AppMinimumVersionResponse;
+import backend.mulkkam.version.repository.AppMinimumVersionRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-class AppMinimumVersionControllerTest {
+class AppMinimumVersionControllerTest extends ControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private AppMinimumVersionRepository appMinimumVersionRepository;
+
+    @DisplayName("앱의 최소 버전을 확인할 때")
+    @Nested
+    class Get {
+
+        @DisplayName("버전 정보가 존재하는 경우 정상적으로 반환된다")
+        @Test
+        void success_alreadySavedVersion() throws Exception {
+            // given
+            String minimumVersion = "1.0.0";
+            AppMinimumVersion appMinimumVersion = AppMinimumVersionFixtureBuilder
+                    .builder()
+                    .minimumVersion(minimumVersion)
+                    .build();
+            appMinimumVersionRepository.save(appMinimumVersion);
+
+            // when
+            String response = mockMvc.perform(get("/versions"))
+                    .andExpect(status().isOk())
+                    .andReturn().getResponse().getContentAsString();
+
+            AppMinimumVersionResponse actual = objectMapper.readValue(response,
+                    AppMinimumVersionResponse.class);
+
+            // then
+            assertThat(actual.minimumVersion()).isEqualTo(minimumVersion);
+        }
+    }
 }
