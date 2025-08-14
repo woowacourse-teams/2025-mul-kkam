033516c07fe07be0fa88a3b321458f02b7fdc4b2
Jin409 <sualng123@gmail.com>
2025-08-14 16:07:41 +0900

    fix: GetMapping 으로 수정

--- backend/src/main/java/backend/mulkkam/version/controller/AppMinimumVersionController.java
+++ backend/src/main/java/backend/mulkkam/version/controller/AppMinimumVersionController.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +21,7 @@ public class AppMinimumVersionController {
 
     @Operation(summary = "앱 버전 확인", description = "최소한으로 등록되어 있어야 하는 앱의 버전을 확인합니다.")
     @ApiResponse(responseCode = "200", description = "조회 성공")
-    @PostMapping
+    @GetMapping
     public ResponseEntity<AppMinimumVersionResponse> read() {
         AppMinimumVersionResponse appMinimumVersionResponse = appMinimumVersionService.read();
         return ResponseEntity.ok().body(appMinimumVersionResponse);
