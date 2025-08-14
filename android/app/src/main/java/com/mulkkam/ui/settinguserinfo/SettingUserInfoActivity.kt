7217068d66b543930d6e2ef396f60816e2456a2d
hwannow <vnfdlvgid061@naver.com>
2025-08-14 15:34:19 +0900

    feat: 회원 정보 설정 화면 구현

---
+++ android/app/src/main/java/com/mulkkam/ui/settinguserinfo/SettingUserInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.mulkkam.ui.settinguserinfo
+
+import android.os.Bundle
+import com.mulkkam.databinding.ActivitySettingTargetAmountBinding.inflate
+import com.mulkkam.databinding.ActivitySettingUserInfoBinding
+import com.mulkkam.ui.util.binding.BindingActivity
+
+class SettingUserInfoActivity : BindingActivity<ActivitySettingUserInfoBinding>(ActivitySettingUserInfoBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
