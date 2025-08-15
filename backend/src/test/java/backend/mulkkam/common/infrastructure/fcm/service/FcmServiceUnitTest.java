d9d6af6cd9ee4fcf05c6628dd657fb98aa0f6bb9
2jin2.1031 <2jin2.1031@gmail.com>
2025-08-15 17:05:57 +0900

    test: fcm topic 테스트 추가

--- backend/src/test/java/backend/mulkkam/common/infrastructure/fcm/service/FcmServiceUnitTest.java
+++ backend/src/test/java/backend/mulkkam/common/infrastructure/fcm/service/FcmServiceUnitTest.java
@@ -1,6 +1,7 @@
 package backend.mulkkam.common.infrastructure.fcm.service;
 
 
+import static backend.mulkkam.common.exception.errorCode.FirebaseErrorCode.SENDER_ID_MISMATCH;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.mockito.ArgumentMatchers.any;
@@ -9,9 +10,10 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import backend.mulkkam.common.exception.AlarmException;
+import backend.mulkkam.common.exception.CommonException;
 import backend.mulkkam.common.infrastructure.fcm.domain.Action;
 import backend.mulkkam.common.infrastructure.fcm.dto.request.SendMessageByFcmTokenRequest;
+import backend.mulkkam.common.infrastructure.fcm.dto.request.SendMessageByFcmTopicRequest;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
@@ -73,7 +75,6 @@ class FcmServiceUnitTest {
             });
         }
 
-
         @DisplayName("실패 시 예외가 발생한다")
         @Test
         void sendMessageByToken_wrapsFcmException() throws Exception {
@@ -82,12 +83,71 @@ class FcmServiceUnitTest {
             when(firebaseMessagingException.getMessagingErrorCode()).thenReturn(MessagingErrorCode.SENDER_ID_MISMATCH);
             when(firebaseMessaging.send(any(Message.class))).thenThrow(firebaseMessagingException);
 
-            SendMessageByFcmTokenRequest sendMessageByFcmTokenRequest = new SendMessageByFcmTokenRequest("title", "body", "token-123", Action.GO_NOTIFICATION);
+            SendMessageByFcmTokenRequest sendMessageByFcmTokenRequest = new SendMessageByFcmTokenRequest("title",
+                    "body", "token-123", Action.GO_NOTIFICATION);
 
             // when & then
             assertThatThrownBy(
                     () -> fcmService.sendMessageByToken(sendMessageByFcmTokenRequest)
-            ).isInstanceOf(AlarmException.class);
+            ).isInstanceOf(CommonException.class)
+                    .hasMessage(SENDER_ID_MISMATCH.name());
+
+            verify(firebaseMessaging, times(1)).send(any(Message.class));
+        }
+    }
+
+    @DisplayName("토픽 형식으로 알림을 보낼 때")
+    @Nested
+    class SendMessageByTopic {
+
+        @DisplayName("성공 시 예외가 터지지 않는다")
+        @Test
+        void success_validInput() throws FirebaseMessagingException {
+            // given
+            when(firebaseMessaging.send(any(Message.class))).thenReturn("msgId");
+
+            SendMessageByFcmTopicRequest sendMessageByFcmTopicRequest = new SendMessageByFcmTopicRequest("title",
+                    "body", "exampleTopic", Action.GO_NOTIFICATION);
+
+            // when & then
+            Assertions.assertThatCode(
+                    () -> fcmService.sendMessageByTopic(sendMessageByFcmTopicRequest)
+            ).doesNotThrowAnyException();
+
+            ArgumentCaptor<Message> messageArgumentCaptor = ArgumentCaptor.forClass(Message.class);
+            verify(firebaseMessaging, times(1)).send(messageArgumentCaptor.capture());
+
+            Message msg = messageArgumentCaptor.getValue();
+
+            String token = invoke(msg, "getToken");
+            Map<String, String> data = invoke(msg, "getData");
+            String topic = invoke(msg, "getTopic");
+            String condition = invoke(msg, "getCondition");
+
+            assertSoftly(softly -> {
+                softly.assertThat(topic).isEqualTo("exampleTopic");
+                softly.assertThat(token).isNull();
+                softly.assertThat(data.get("action")).isEqualTo(Action.GO_NOTIFICATION.name());
+                softly.assertThat(condition).isNull();
+            });
+        }
+
+        @DisplayName("실패 시 예외가 발생한다")
+        @Test
+        void error() throws FirebaseMessagingException {
+            // given
+            FirebaseMessagingException firebaseMessagingException = mock(FirebaseMessagingException.class);
+            when(firebaseMessagingException.getMessagingErrorCode()).thenReturn(MessagingErrorCode.SENDER_ID_MISMATCH);
+            when(firebaseMessaging.send(any(Message.class))).thenThrow(firebaseMessagingException);
+
+            SendMessageByFcmTopicRequest sendMessageByFcmTopicRequest = new SendMessageByFcmTopicRequest("title",
+                    "body", "exampleTopic", Action.GO_NOTIFICATION);
+
+            // when & then
+            assertThatThrownBy(
+                    () -> fcmService.sendMessageByTopic(sendMessageByFcmTopicRequest)
+            ).isInstanceOf(CommonException.class)
+                    .hasMessage(SENDER_ID_MISMATCH.name());
 
             verify(firebaseMessaging, times(1)).send(any(Message.class));
         }
