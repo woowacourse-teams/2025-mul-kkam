8eb6205cb4765be85ad1c03bac948b73d165f13c
CheChe903 <cheche903@naver.com>
2025-08-16 19:45:24 +0900

    docs: 스웨거 응답 구조 변경

--- backend/src/main/java/backend/mulkkam/member/dto/response/MemberResponse.java
+++ backend/src/main/java/backend/mulkkam/member/dto/response/MemberResponse.java
@@ -9,13 +9,13 @@ public record MemberResponse(
         @Schema(description = "회원 ID", example = "1")
         Long id,
 
-        @Schema(description = "닉네임", example = "밍곰", nullable = true)
+        @Schema(description = "닉네임", example = "밍곰")
         String nickname,
 
         @Schema(description = "체중 (kg)", example = "50.0", nullable = true)
         Double weight,
 
-        @Schema(description = "성별", example = "FEMALE", implementation = Gender.class)
+        @Schema(description = "성별", example = "FEMALE", implementation = Gender.class, nullable = true)
         Gender gender,
 
         @Schema(description = "목표 음용량 (ml)", example = "2000", minimum = "1")
