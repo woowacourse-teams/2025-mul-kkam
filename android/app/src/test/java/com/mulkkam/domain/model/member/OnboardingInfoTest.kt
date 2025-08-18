08f1c147cfcf63a806aa1ba54d66e331abe0ce34
hwannow <vnfdlvgid061@naver.com>
2025-08-19 08:49:13 +0900

    test: 온보딩 정보 테스트 코드 네이밍 수정

--- android/app/src/test/java/com/mulkkam/domain/model/member/OnboardingInfoTest.kt
+++ android/app/src/test/java/com/mulkkam/domain/model/member/OnboardingInfoTest.kt
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test
 
 class OnboardingInfoTest {
     @Test
-    fun `몸무게와 성별 정보가 있으면 신체 정보가 있다`() {
+    fun `몸무게와 성별 정보가 있으면 신체 정보가 있다고 판별한다`() {
         // given
         val mockedOnboardingInfo = OnboardingInfo(weight = BioWeight(60), gender = Gender.MALE)
 
@@ -20,7 +20,7 @@ class OnboardingInfoTest {
     }
 
     @Test
-    fun `몸무게와 성별 정보가 없으면 신체 정보가 없다`() {
+    fun `몸무게와 성별 정보가 없으면 신체 정보가 없다고 판별한다`() {
         // given
         val mockedOnboardingInfo = OnboardingInfo()
 
