ac234de88dc54a9df96992f3e72f37f2c34ee97b
CheChe903 <cheche903@naver.com>
2025-08-19 22:53:27 +0900

    feat: 데이터소스 설정 DEV 프로파일로 설정

--- backend/src/main/java/backend/mulkkam/common/config/datasource/DataSourceConfig.java
+++ backend/src/main/java/backend/mulkkam/common/config/datasource/DataSourceConfig.java
@@ -9,7 +9,9 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
+@Profile("dev")
 @Configuration
 public class DataSourceConfig {
 
