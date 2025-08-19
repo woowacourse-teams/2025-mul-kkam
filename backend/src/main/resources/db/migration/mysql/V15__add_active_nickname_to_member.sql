6087651b38527d3b115c395b46d9886252fd81a4
2jin2.1031 <2jin2.1031@gmail.com>
2025-08-19 22:11:54 +0900

    chore: member nickname 필드 제거 후 다시 생성

--- backend/src/main/resources/db/migration/mysql/V15__add_active_nickname_to_member.sql
+++ backend/src/main/resources/db/migration/mysql/V15__add_active_nickname_to_member.sql
@@ -1,5 +1,9 @@
 -- ALLOW_DROP
 -- reason: rename 과정에서 기존 제약/컬럼 drop 필요 (PR-1234)
-ALTER TABLE member DROP INDEX nickname;
+ALTER TABLE member
+    DROP COLUMN nickname;
+ALTER TABLE member
+    ADD COLUMN nickname VARCHAR(10) NOT NULL;
+
 ALTER TABLE member
     ADD COLUMN active_nickname VARCHAR(50) NULL UNIQUE;
