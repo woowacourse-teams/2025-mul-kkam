44d88516ca8565b3c4d2b78f93af19f56bd69168
2jin2.1031 <2jin2.1031@gmail.com>
2025-08-19 21:58:33 +0900

    chore: 유니크키 drop 스크립트를 mysql 문법으로 수정

--- backend/src/main/resources/db/migration/mysql/V15__add_active_nickname_to_member.sql
+++ backend/src/main/resources/db/migration/mysql/V15__add_active_nickname_to_member.sql
@@ -1,5 +1,5 @@
 -- ALLOW_DROP
 -- reason: rename 과정에서 기존 제약/컬럼 drop 필요 (PR-1234)
-ALTER TABLE member DROP INDEX CONSTRAINT_87;
+DROP INDEX CONSTRAINT_87 ON member;
 ALTER TABLE member
     ADD COLUMN active_nickname VARCHAR(50) NULL UNIQUE;
