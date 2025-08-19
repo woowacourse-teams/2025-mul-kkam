d92c43d316f28d9f5f1a2d19a8e050c2009c0c25
2jin2.1031 <2jin2.1031@gmail.com>
2025-08-19 20:12:24 +0900

    tmp: pr base branch syn 테스트

--- backend/src/main/java/backend/mulkkam/MulKkamApplication.java
+++ backend/src/main/java/backend/mulkkam/MulKkamApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MulKkamApplication {
     public static void main(String[] args) {
         SpringApplication.run(MulKkamApplication.class, args);
-
     }
 }
