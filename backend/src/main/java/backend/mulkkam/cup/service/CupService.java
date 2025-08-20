b20a4c709b965ac6cf1699399c3a96b95b355509
2jin2.1031 <2jin2.1031@gmail.com>
2025-08-20 20:41:20 +0900

    refactor: 명확한 필드명으로 수정(emoji -> emojiId)

--- backend/src/main/java/backend/mulkkam/cup/service/CupService.java
+++ backend/src/main/java/backend/mulkkam/cup/service/CupService.java
@@ -62,7 +62,7 @@ public class CupService {
     ) {
         Member member = getMember(memberDetails.id());
         IntakeType intakeType = IntakeType.findByName(createCupRequest.intakeType());
-        CupEmoji cupEmoji = getCupEmoji(createCupRequest.emoji());
+        CupEmoji cupEmoji = getCupEmoji(createCupRequest.emojiId());
         Cup cup = createCupRequest.toCup(member, calculateNextCupRank(member), intakeType, cupEmoji);
 
         Cup createdCup = cupRepository.save(cup);
