3e289baad33efdee9ff2661c9a05e6247edc02c6
2jin2.1031 <2jin2.1031@gmail.com>
2025-08-20 19:59:52 +0900

    refactor: stream 사용

--- backend/src/main/java/backend/mulkkam/cup/service/CupEmojiService.java
+++ backend/src/main/java/backend/mulkkam/cup/service/CupEmojiService.java
@@ -4,8 +4,8 @@ import backend.mulkkam.cup.domain.CupEmoji;
 import backend.mulkkam.cup.dto.CupEmojiResponse;
 import backend.mulkkam.cup.dto.CupEmojisResponse;
 import backend.mulkkam.cup.repository.CupEmojiRepository;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -22,11 +22,7 @@ public class CupEmojiService {
     }
 
     private static List<CupEmojiResponse> toCupEmojiResponse(List<CupEmoji> cupEmojis) {
-        List<CupEmojiResponse> cupEmojiResponses = new ArrayList<>();
-        for (CupEmoji cupEmoji : cupEmojis) {
-            CupEmojiResponse cupEmojiResponse = new CupEmojiResponse(cupEmoji);
-            cupEmojiResponses.add(cupEmojiResponse);
-        }
-        return cupEmojiResponses;
+        return cupEmojis.stream().map(CupEmojiResponse::new)
+                .collect(Collectors.toList());
     }
 }
