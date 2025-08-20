b18bfdf0058e7e36a7976ae840bb549c91af2c05
2jin2.1031 <2jin2.1031@gmail.com>
2025-08-20 16:35:47 +0900

    fix: IntakeHistoryDetail과 Cup과의 연관관계 제거

--- backend/src/main/resources/db/migration/common/V19__add_cup_id_to_intake_history_detail.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE intake_history_detail
-    ADD COLUMN cup_id BIGINT NOT NULL;;
-
-ALTER TABLE intake_history_detail
-    ADD CONSTRAINT fk_intake_history_detail_cup
-        FOREIGN KEY (cup_id) REFERENCES cup(id);
