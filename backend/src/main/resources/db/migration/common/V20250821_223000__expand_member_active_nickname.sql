7bf24e2c217c1f795b30a873d0e8050355e38f64
Jin409 <sualng123@gmail.com>
2025-08-22 00:54:41 +0900

    chore: unique_nickname 정의 변경

--- backend/src/main/resources/db/migration/common/V20250821_223000__expand_member_active_nickname.sql
+++ backend/src/main/resources/db/migration/common/V20250821_223000__expand_member_active_nickname.sql
@@ -2,9 +2,6 @@
 -- reason: nickname 컬럼에 걸린 고유 인덱스명을 동적으로 조회해 드롭 (679)
 
 ALTER TABLE member
-    ADD COLUMN active_nickname VARCHAR(10)
-        GENERATED ALWAYS AS (
-            CASE WHEN deleted_at IS NULL THEN nickname ELSE NULL END
-            );
+    ADD COLUMN active_nickname VARCHAR(10);
 
 CREATE UNIQUE INDEX uq_member_active_nickname ON member (active_nickname);
