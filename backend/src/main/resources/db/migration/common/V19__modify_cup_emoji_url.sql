c26793734377ddb778e617d2d52eeb8d95be0c83
2jin2.1031 <2jin2.1031@gmail.com>
2025-08-21 13:04:55 +0900

    refactor: 불필요한 공백 제거

--- backend/src/main/resources/db/migration/common/V19__modify_cup_emoji_url.sql
+++ backend/src/main/resources/db/migration/common/V19__modify_cup_emoji_url.sql
@@ -1,11 +1,7 @@
 DELETE
 FROM cup_emoji;
 
-
-
 INSERT INTO cup_emoji (url, created_at, deleted_at)
 VALUES ('https://github.com/user-attachments/assets/df68b91b-772c-4feb-bc2a-59955fe74c57', '2025-08-01 10:10:11', NULL),
        ('https://github.com/user-attachments/assets/5344a5e5-95ab-4925-9749-d2b9334162e8', '2025-08-01 10:10:11', NULL),
        ('https://github.com/user-attachments/assets/ce9ccbdf-e17f-410c-ac16-626c919b535b', '2025-08-01 10:10:12', NULL);
-
-
