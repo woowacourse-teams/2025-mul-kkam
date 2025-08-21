d3615582dc66c926409fab5ab7ec6006b8303eda
hwannow <vnfdlvgid061@naver.com>
2025-08-21 20:39:48 +0900

    feat: 섭취 종류에 따라 태그 색상 변경

--- android/app/src/main/java/com/mulkkam/ui/settingcups/adapter/CupViewHolder.kt
+++ android/app/src/main/java/com/mulkkam/ui/settingcups/adapter/CupViewHolder.kt
@@ -1,8 +1,10 @@
 package com.mulkkam.ui.settingcups.adapter
 
+import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.ViewGroup
+import androidx.core.graphics.toColorInt
 import androidx.recyclerview.widget.RecyclerView
 import com.mulkkam.R
 import com.mulkkam.databinding.ItemSettingCupsCupBinding
@@ -28,7 +30,8 @@ class CupViewHolder(
     private fun showCupInfo(item: SettingCupsItem.CupItem) =
         with(binding) {
             tvNickname.text = item.value.name
-            tvIncrement.text = root.context.getString(R.string.setting_cups_increment, item.value.amount)
+            tvIncrement.text =
+                root.context.getString(R.string.setting_cups_increment, item.value.amount)
             tvSettingWaterCupTagType.text =
                 when (item.value.intakeType) {
                     IntakeType.WATER -> root.context.getString(R.string.setting_cups_intake_type_water)
@@ -39,6 +42,17 @@ class CupViewHolder(
                 url = item.value.emoji,
                 shape = ImageShape.Rounded(12),
             )
+
+            val intakeTypeColor =
+                ColorStateList.valueOf(
+                    item.value.intakeType
+                        .toColorHex()
+                        .toColorInt(),
+                )
+
+            tvSettingWaterCupTagType.backgroundTintList = intakeTypeColor
+
+            tvIncrement.setTextColor(intakeTypeColor)
         }
 
     private fun initClickListeners(item: SettingCupsItem.CupItem) =
