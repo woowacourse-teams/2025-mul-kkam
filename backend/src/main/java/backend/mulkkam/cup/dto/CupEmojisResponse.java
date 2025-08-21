e6bd1b855e0adfc278660cc51e4a9faa8682cdaf
2jin2.1031 <2jin2.1031@gmail.com>
2025-08-21 13:08:24 +0900

    refactor: 컵 이모지 조회 swagger 설정 추가

--- backend/src/main/java/backend/mulkkam/cup/dto/CupEmojisResponse.java
+++ backend/src/main/java/backend/mulkkam/cup/dto/CupEmojisResponse.java
@@ -1,8 +1,12 @@
 package backend.mulkkam.cup.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
+@Schema(description = "컵 이모지 리스트 응답")
 public record CupEmojisResponse(
+
+        @Schema(description = "컵 이모지 목록")
         List<CupEmojiResponse> cups
 ) {
 }
