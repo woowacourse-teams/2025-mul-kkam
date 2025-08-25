6f18202a438d65939d32cf66c34029512b59737d
2jin2.1031 <2jin2.1031@gmail.com>
2025-08-26 01:05:48 +0900

    chore: 없는 필드 설정 제거 및 title 컬럼 제거

---
+++ backend/src/main/resources/db/migration/common/V20250826_004900_modify_notification.sql
@@ -0,0 +1,5 @@
+-- ALLOW DROP
+-- reason: title 필드를 content로 수정했는데 컬럼 삭제를 안함
+ALTER TABLE notification
+    DROP COLUMN title;
+
--- backend/src/main/resources/db/migration/h2/V20250821_214001__modify_column_name_of_notifcation.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE notification
-    ALTER COLUMN recommended_target_amount INT NULL;
