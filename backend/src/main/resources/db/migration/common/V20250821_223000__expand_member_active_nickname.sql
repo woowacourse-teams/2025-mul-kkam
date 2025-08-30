105c06f5a905781333a923bdf52436dc8a7d47ef
minSsan <msp1125@hanyang.ac.kr>
2025-08-30 15:41:41 +0900

    feat: member - active nickname 결정권을 어플리케이션 코드에게 위임

--- backend/src/main/resources/db/migration/common/V20250821_223000__expand_member_active_nickname.sql
+++ backend/src/main/resources/db/migration/common/V20250821_223000__expand_member_active_nickname.sql
@@ -1,7 +1,4 @@
 ALTER TABLE member
-    ADD COLUMN active_nickname VARCHAR(10)
-        GENERATED ALWAYS AS (
-            CASE WHEN deleted_at IS NULL THEN nickname ELSE NULL END
-        );
+    ADD COLUMN active_nickname VARCHAR(10) NULL;
 
 CREATE UNIQUE INDEX uq_member_active_nickname ON member (active_nickname);
