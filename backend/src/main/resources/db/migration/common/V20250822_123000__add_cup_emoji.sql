416673a51a58713bf9649b8a3c4dfc844dcb9bda
minSsan <msp1125@hanyang.ac.kr>
2025-09-01 00:31:22 +0900

    feat: cup_emoji 생성 마이그레이션 파일 - 기본 데이터 insert 스크립트 삭제

--- backend/src/main/resources/db/migration/common/V20250822_123000__add_cup_emoji.sql
+++ backend/src/main/resources/db/migration/common/V20250822_123000__add_cup_emoji.sql
@@ -5,7 +5,3 @@ CREATE TABLE cup_emoji
     created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP    NULL
 );
-
-INSERT INTO cup_emoji (url, created_at, deleted_at)
-VALUES ('https://github.com/user-attachments/assets/783767ab-ee37-4079-8e38-e08884a8de1c', '2025-08-01 10:10:10', NULL),
-       ('https://github.com/user-attachments/assets/393fc8f9-bc46-4856-bfbe-889efc97151e', '2025-08-01 10:10:11', NULL);
