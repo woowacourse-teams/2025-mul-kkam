1c45be0bab09124cc6604457c35fb96a617d32ec
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-05 00:06:34 +0900

    refactor: swagger 설정 제거

--- backend/src/main/java/backend/mulkkam/cup/dto/CreateCup.java
+++ backend/src/main/java/backend/mulkkam/cup/dto/CreateCup.java
@@ -7,22 +7,13 @@ import backend.mulkkam.cup.domain.vo.CupAmount;
 import backend.mulkkam.cup.domain.vo.CupNickname;
 import backend.mulkkam.cup.domain.vo.CupRank;
 import backend.mulkkam.member.domain.Member;
-import io.swagger.v3.oas.annotations.media.Schema;
 
 
 public record CreateCup(
-        @Schema(description = "컵 이름", example = "스타벅스 텀블러")
         CupNickname cupNickname,
-
-        @Schema(description = "컵 용량", example = "500")
         CupAmount cupAmount,
-
         CupRank cupRank,
-
-        @Schema(description = "음료 종류", implementation = IntakeType.class)
         IntakeType intakeType,
-
-        @Schema(description = "컵 이모지 id", example = "1")
         CupEmoji cupEmoji
 ) {
     public Cup toCup(
