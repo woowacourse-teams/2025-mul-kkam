6f80d3c00e5679cc40ed1df1e9146eff02b828e3
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-05 01:16:59 +0900

    feat: 온보딩 생성 요청 유효성 검증 추가

--- backend/src/main/java/backend/mulkkam/member/dto/CreateMemberRequest.java
+++ backend/src/main/java/backend/mulkkam/member/dto/CreateMemberRequest.java
@@ -7,6 +7,7 @@ import backend.mulkkam.member.domain.vo.MemberNickname;
 import backend.mulkkam.member.domain.vo.PhysicalAttributes;
 import backend.mulkkam.member.domain.vo.TargetAmount;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
 import java.util.List;
 
 @Schema(description = "온보딩 정보 생성 요청")
@@ -31,7 +32,7 @@ public record CreateMemberRequest(
         boolean isNightNotificationAgreed,
 
         @Schema(description = "컵 생성 데이터 리스트")
-        List<CreateCupRequest> createCupRequests
+        List<@Valid CreateCupRequest> createCupRequests
 ) {
     public Member toMember() {
         return new Member(
