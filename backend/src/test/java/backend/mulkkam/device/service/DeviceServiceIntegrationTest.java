aa3b9f2ac3f6a306b0574848e19e6c3dedeeb84a
Jin409 <sualng123@gmail.com>
2025-09-05 14:08:59 +0900

    test: 권한 검사 서비스 테스트 추가

--- backend/src/test/java/backend/mulkkam/device/service/DeviceServiceIntegrationTest.java
+++ backend/src/test/java/backend/mulkkam/device/service/DeviceServiceIntegrationTest.java
@@ -1,11 +1,16 @@
 package backend.mulkkam.device.service;
 
+import static backend.mulkkam.common.exception.errorCode.ForbiddenErrorCode.NOT_PERMITTED_FOR_DEVICE;
+import static backend.mulkkam.common.exception.errorCode.NotFoundErrorCode.NOT_FOUND_DEVICE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import backend.mulkkam.common.dto.MemberDetails;
+import backend.mulkkam.common.exception.CommonException;
 import backend.mulkkam.device.domain.Device;
 import backend.mulkkam.device.repository.DeviceRepository;
 import backend.mulkkam.member.domain.Member;
+import backend.mulkkam.member.domain.vo.MemberNickname;
 import backend.mulkkam.member.repository.MemberRepository;
 import backend.mulkkam.support.fixture.DeviceFixtureBuilder;
 import backend.mulkkam.support.fixture.MemberFixtureBuilder;
@@ -53,7 +58,55 @@ class DeviceServiceIntegrationTest extends ServiceIntegrationTest {
             deviceService.deleteFcmToken(deviceId, memberDetails);
 
             // then
-            assertThat(device.getToken()).isNull();
+            Device updatedDevice = deviceRepository.findById(device.getId()).orElse(null);
+            assertThat(updatedDevice.getToken()).isNull();
+        }
+
+        @DisplayName("Device Id 가 존재하지 않는 경우 예외가 발생한다")
+        @Test
+        void error_whenDeviceIdIsNotExisted() {
+            // given
+            Member member = MemberFixtureBuilder
+                    .builder()
+                    .build();
+            memberRepository.save(member);
+
+            MemberDetails memberDetails = new MemberDetails(member);
+
+            // when & then
+            assertThatThrownBy(() -> deviceService.deleteFcmToken("invalidId", memberDetails))
+                    .isInstanceOf(CommonException.class)
+                    .hasMessageContaining(NOT_FOUND_DEVICE.name());
+        }
+
+        @DisplayName("Device Id 가 존재하지 않는 경우 예외가 발생한다")
+        @Test
+        void error_whenDeviceIdIsNotOwnedByMember() {
+            // given
+            Member member = MemberFixtureBuilder
+                    .builder()
+                    .build();
+            memberRepository.save(member);
+
+            String deviceId = "deviceId";
+            Device device = DeviceFixtureBuilder
+                    .withMember(member)
+                    .deviceId(deviceId)
+                    .build();
+            deviceRepository.save(device);
+
+            Member anotherMember = MemberFixtureBuilder
+                    .builder()
+                    .memberNickname(new MemberNickname("칼리"))
+                    .build();
+            memberRepository.save(anotherMember);
+
+            MemberDetails memberDetails = new MemberDetails(anotherMember);
+
+            // when & then
+            assertThatThrownBy(() -> deviceService.deleteFcmToken(deviceId, memberDetails))
+                    .isInstanceOf(CommonException.class)
+                    .hasMessageContaining(NOT_PERMITTED_FOR_DEVICE.name());
         }
     }
 }
