02a3750d927a2abbd438691d1e9f130bf99650fd
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-08 05:03:31 +0900

    refactor: CreateCupWithoutRankRequest intakeType 필드 자료형 수정(String -> IntakeType)

--- backend/src/main/java/backend/mulkkam/cup/dto/request/CreateCupWithoutRankRequest.java
+++ backend/src/main/java/backend/mulkkam/cup/dto/request/CreateCupWithoutRankRequest.java
@@ -17,7 +17,7 @@ public record CreateCupWithoutRankRequest(
         Integer cupAmount,
 
         @Schema(description = "음료 종류", implementation = IntakeType.class)
-        String intakeType,
+        IntakeType intakeType,
 
         @Schema(description = "컵 이모지 id", example = "1")
         Long cupEmojiId
@@ -30,7 +30,7 @@ public record CreateCupWithoutRankRequest(
                 new CupNickname(cupNickname),
                 new CupAmount(cupAmount),
                 cupRank,
-                IntakeType.findByName(intakeType),
+                intakeType,
                 cupEmoji
         );
     }
