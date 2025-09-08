7cb724c4a7707870cb07a5dcac4fae85190490f5
CheChe903 <cheche903@naver.com>
2025-09-08 18:49:19 +0900

    chore: 디바이스 uuid 255자로 설정

--- backend/src/main/resources/db/migration/common/V20250907_2148__add_device_uuid_to_account_refresh_token.sql
+++ backend/src/main/resources/db/migration/common/V20250907_2148__add_device_uuid_to_account_refresh_token.sql
@@ -2,14 +2,14 @@ ALTER TABLE device
     MODIFY COLUMN `device_id` VARCHAR(50) NULL;
 
 ALTER TABLE device
-    ADD COLUMN `device_uuid` VARCHAR(50);
+    ADD COLUMN `device_uuid` VARCHAR(255);
 
 UPDATE device
 SET device_uuid = device_id
 WHERE device_uuid IS NULL;
 
 ALTER TABLE
-    device MODIFY COLUMN device_uuid VARCHAR(50) NOT NULL;
+    device MODIFY COLUMN device_uuid VARCHAR(255) NOT NULL;
 
 ALTER TABLE account_refresh_token
-    ADD COLUMN device_uuid VARCHAR(50);
+    ADD COLUMN device_uuid VARCHAR(255);
