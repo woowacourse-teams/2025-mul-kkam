2b0002225eb6bc21bffdc27562de23089cb5f877
junseo511 <prime54@g.skku.edu>
2025-09-09 11:10:44 +0900

    refactor: HistoryFragment의 HistoryAdapter `setSingleClickListener`를 `onItemClickListener`로 변경

--- android/app/src/main/java/com/mulkkam/ui/history/HistoryFragment.kt
+++ android/app/src/main/java/com/mulkkam/ui/history/HistoryFragment.kt
@@ -78,7 +78,7 @@ class HistoryFragment :
             adapter = historyAdapter
             layoutManager = LinearLayoutManager(requireContext())
         }
-        historyAdapter.setSingleClickListener =
+        historyAdapter.onItemClickListener =
             HistoryViewHolder.Handler { history ->
                 if (viewModel.waterIntakeState.value !is WaterIntakeState.Present) {
                     CustomSnackBar
