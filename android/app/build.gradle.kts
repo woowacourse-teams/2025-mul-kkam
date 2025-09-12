de82af7ad92c0725dec507bc1feba52c77ecc417
hwannow <vnfdlvgid061@naver.com>
2025-09-12 18:45:20 +0900

    chore: Jetpack Compose 라이브러리 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -93,6 +93,14 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.fragment.ktx)
 
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+
     // 네트워크 통신 관련 라이브러리
     implementation(libs.okhttp)
     implementation(libs.okhttp.logging)
