506145dac42e145829ad3129b18b6c816d15170e
minSsan <msp1125@hanyang.ac.kr>
2025-09-12 15:08:58 +0900

    feat: 마이그레이션 파일 및 데이터 설정 파일 추가

--- backend/src/main/resources/db/migration/common/V20250911_215000__add_emoji_code_to_cup_emoji.sql
+++ backend/src/main/resources/db/migration/common/V20250911_215000__add_emoji_code_to_cup_emoji.sql
@@ -1,22 +1,2 @@
 ALTER TABLE cup_emoji
     ADD COLUMN `code` VARCHAR(64) NULL;
-
--- default coffee
-INSERT INTO cup_emoji (url, code, created_at, deleted_at)
-SELECT 'https://github.com/user-attachments/assets/c045e7ff-96b4-4c14-83bc-706112c1e871',
-       'default.coffee',
-       CURRENT_TIMESTAMP,
-       NULL
-WHERE NOT EXISTS (SELECT 1
-                  FROM cup_emoji
-                  WHERE url = 'https://github.com/user-attachments/assets/c045e7ff-96b4-4c14-83bc-706112c1e871');
-
--- default water
-INSERT INTO cup_emoji (url, code, created_at, deleted_at)
-SELECT 'https://github.com/user-attachments/assets/e096b351-c8c1-494e-abad-5c9881af7ef5',
-       'default.water',
-       CURRENT_TIMESTAMP,
-       NULL
-WHERE NOT EXISTS (SELECT 1
-                  FROM cup_emoji
-                  WHERE url = 'https://github.com/user-attachments/assets/e096b351-c8c1-494e-abad-5c9881af7ef5');
