41c36ef44e9073c85480f95238ccc98b8875fa03
hwannow <vnfdlvgid061@naver.com>
2025-09-13 16:25:37 +0900

    feat: 디자인 시스템 색상 및 타이포그래피 추가

--- android/app/src/main/java/com/mulkkam/ui/designsystem/Theme.kt
+++ android/app/src/main/java/com/mulkkam/ui/designsystem/Theme.kt
@@ -12,16 +12,16 @@ import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme =
     darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80,
+        primary = Primary100,
+        secondary = Primary100,
+        tertiary = Primary100,
     )
 
 private val LightColorScheme =
     lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40,
+        primary = Primary100,
+        secondary = Primary100,
+        tertiary = Primary100,
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
--- android/app/src/main/java/com/mulkkam/ui/designsystem/Typography.kt
+++ android/app/src/main/java/com/mulkkam/ui/designsystem/Typography.kt
@@ -2,35 +2,90 @@ package com.mulkkam.ui.designsystem
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.mulkkam.R
 
-// Set of Material typography styles to start with
-val Typography =
-    Typography(
-        bodyLarge =
-            TextStyle(
-                fontFamily = FontFamily.Default,
-                fontWeight = FontWeight.Normal,
-                fontSize = 16.sp,
-                lineHeight = 24.sp,
-                letterSpacing = 0.5.sp,
-            ),
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+val PretendardBold = FontFamily(Font(R.font.pretendard_bold))
+val PretendardSemiBold = FontFamily(Font(R.font.pretendard_semi_bold))
+val PretendardMedium = FontFamily(Font(R.font.pretendard_medium))
+val PretendardRegular = FontFamily(Font(R.font.pretendard_regular))
+
+val Headline1 =
+    TextStyle(
+        fontFamily = PretendardBold,
         fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        lineHeight = 30.sp,
     )
-     */
+
+val Title1 =
+    TextStyle(
+        fontFamily = PretendardBold,
+        fontSize = 18.sp,
+        lineHeight = 25.sp,
+    )
+
+val Title2 =
+    TextStyle(
+        fontFamily = PretendardSemiBold,
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
+    )
+
+val Title3 =
+    TextStyle(
+        fontFamily = PretendardMedium,
+        fontSize = 14.sp,
+        lineHeight = 14.sp,
+    )
+
+val Body1 =
+    TextStyle(
+        fontFamily = PretendardRegular,
+        fontSize = 17.sp,
+        lineHeight = 23.sp,
+    )
+
+val Body2 =
+    TextStyle(
+        fontFamily = PretendardRegular,
+        fontSize = 15.sp,
+        lineHeight = 22.sp,
+    )
+
+val Body3 =
+    TextStyle(
+        fontFamily = PretendardRegular,
+        fontSize = 14.sp,
+        lineHeight = 21.sp,
+    )
+
+val Body4 =
+    TextStyle(
+        fontFamily = PretendardMedium,
+        fontSize = 13.sp,
+        lineHeight = 18.sp,
+    )
+
+val Body5 =
+    TextStyle(
+        fontFamily = PretendardRegular,
+        fontSize = 12.sp,
+        lineHeight = 17.sp,
+    )
+
+val Label1 =
+    TextStyle(
+        fontFamily = PretendardMedium,
+        fontSize = 13.sp,
+        lineHeight = 18.sp,
+    )
+
+val Label2 =
+    TextStyle(
+        fontFamily = PretendardMedium,
+        fontSize = 11.sp,
+        lineHeight = 11.sp,
     )
