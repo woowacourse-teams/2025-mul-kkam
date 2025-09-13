fc65e0450cae3aed3be2fd9338e70889b5d40ac3
hwannow <vnfdlvgid061@naver.com>
2025-09-14 01:39:20 +0900

    feat: 공백의 개쩌는 타이포그래피 추가

--- android/app/src/main/java/com/mulkkam/ui/designsystem/Typography.kt
+++ android/app/src/main/java/com/mulkkam/ui/designsystem/Typography.kt
@@ -3,7 +3,10 @@ package com.mulkkam.ui.designsystem
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.mulkkam.R
 
 val PretendardBold = FontFamily(Font(R.font.pretendard_bold))
@@ -11,79 +14,42 @@ val PretendardSemiBold = FontFamily(Font(R.font.pretendard_semi_bold))
 val PretendardMedium = FontFamily(Font(R.font.pretendard_medium))
 val PretendardRegular = FontFamily(Font(R.font.pretendard_regular))
 
-val Headline1 =
-    TextStyle(
-        fontFamily = PretendardBold,
-        fontSize = 22.sp,
-        lineHeight = 30.sp,
-    )
+data class Typography(
+    val headline1: TextStyle,
+    val title1: TextStyle,
+    val title2: TextStyle,
+    val title3: TextStyle,
+    val body1: TextStyle,
+    val body2: TextStyle,
+    val body3: TextStyle,
+    val body4: TextStyle,
+    val body5: TextStyle,
+    val label1: TextStyle,
+    val label2: TextStyle,
+)
 
-val Title1 =
-    TextStyle(
-        fontFamily = PretendardBold,
-        fontSize = 18.sp,
-        lineHeight = 25.sp,
-    )
+internal fun Typography(density: Density): Typography {
+    val textStyle =
+        { fontFamily: FontFamily, fontWeight: FontWeight, fontSizeDp: Dp, lineHeightDp: Dp ->
+            TextStyle(
+                lineHeight = with(density) { lineHeightDp.toSp() },
+                fontFamily = fontFamily,
+                fontWeight = fontWeight,
+                fontSize = with(density) { fontSizeDp.toSp() },
+            )
+        }
 
-val Title2 =
-    TextStyle(
-        fontFamily = PretendardSemiBold,
-        fontSize = 16.sp,
-        lineHeight = 22.sp,
+    return Typography(
+        headline1 = textStyle(PretendardBold, FontWeight.Bold, 22.dp, 30.dp),
+        title1 = textStyle(PretendardBold, FontWeight.Bold, 18.dp, 25.dp),
+        title2 = textStyle(PretendardSemiBold, FontWeight.SemiBold, 16.dp, 22.dp),
+        title3 = textStyle(PretendardMedium, FontWeight.Medium, 14.dp, 14.dp),
+        body1 = textStyle(PretendardRegular, FontWeight.Normal, 17.dp, 23.dp),
+        body2 = textStyle(PretendardRegular, FontWeight.Normal, 15.dp, 22.dp),
+        body3 = textStyle(PretendardRegular, FontWeight.Normal, 14.dp, 21.dp),
+        body4 = textStyle(PretendardMedium, FontWeight.Medium, 13.dp, 18.dp),
+        body5 = textStyle(PretendardRegular, FontWeight.Normal, 12.dp, 17.dp),
+        label1 = textStyle(PretendardMedium, FontWeight.Medium, 13.dp, 18.dp),
+        label2 = textStyle(PretendardMedium, FontWeight.Normal, 10.dp, 10.dp),
     )
-
-val Title3 =
-    TextStyle(
-        fontFamily = PretendardMedium,
-        fontSize = 14.sp,
-        lineHeight = 14.sp,
-    )
-
-val Body1 =
-    TextStyle(
-        fontFamily = PretendardRegular,
-        fontSize = 17.sp,
-        lineHeight = 23.sp,
-    )
-
-val Body2 =
-    TextStyle(
-        fontFamily = PretendardRegular,
-        fontSize = 15.sp,
-        lineHeight = 22.sp,
-    )
-
-val Body3 =
-    TextStyle(
-        fontFamily = PretendardRegular,
-        fontSize = 14.sp,
-        lineHeight = 21.sp,
-    )
-
-val Body4 =
-    TextStyle(
-        fontFamily = PretendardMedium,
-        fontSize = 13.sp,
-        lineHeight = 18.sp,
-    )
-
-val Body5 =
-    TextStyle(
-        fontFamily = PretendardRegular,
-        fontSize = 12.sp,
-        lineHeight = 17.sp,
-    )
-
-val Label1 =
-    TextStyle(
-        fontFamily = PretendardMedium,
-        fontSize = 13.sp,
-        lineHeight = 18.sp,
-    )
-
-val Label2 =
-    TextStyle(
-        fontFamily = PretendardMedium,
-        fontSize = 11.sp,
-        lineHeight = 11.sp,
-    )
+}
