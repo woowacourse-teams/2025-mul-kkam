e4ec1e8d6872caf36668cfc5e911f07975ba5817
hwannow <vnfdlvgid061@naver.com>
2025-09-14 01:42:58 +0900

    feat: 공백의 개쩌는 테마 적용

--- android/app/src/main/java/com/mulkkam/ui/designsystem/Theme.kt
+++ android/app/src/main/java/com/mulkkam/ui/designsystem/Theme.kt
@@ -8,42 +8,35 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 
-private val DarkColorScheme =
-    darkColorScheme(
-        primary = Primary100,
-        secondary = Primary100,
-        tertiary = Primary100,
-    )
+object MulKkamTheme {
+    val typography: Typography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalTypography.current
+}
 
-private val LightColorScheme =
-    lightColorScheme(
-        primary = Primary100,
-        secondary = Primary100,
-        tertiary = Primary100,
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-     */
-    )
+private val LocalTypography =
+    staticCompositionLocalOf<Typography> { error("No Typography provided") }
 
 @Composable
-fun AndroidpaymentsTheme(
+fun MulkkamTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit,
 ) {
+    val density = LocalDensity.current
+    val context = LocalContext.current
+    val typography = remember { Typography(density) }
     val colorScheme =
         when {
             dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-                val context = LocalContext.current
                 if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
             }
 
@@ -51,9 +44,24 @@ fun AndroidpaymentsTheme(
             else -> LightColorScheme
         }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content,
-    )
+    CompositionLocalProvider(LocalTypography provides typography) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            content = content,
+        )
+    }
 }
+
+private val DarkColorScheme =
+    darkColorScheme(
+        primary = Primary100,
+        secondary = Secondary100,
+        tertiary = Gray100,
+    )
+
+private val LightColorScheme =
+    lightColorScheme(
+        primary = Primary100,
+        secondary = Secondary100,
+        tertiary = Gray100,
+    )
