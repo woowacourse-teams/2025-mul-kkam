17dd7d5f8792d5a6617c696832a479c048da258e
hwannow <vnfdlvgid061@naver.com>
2025-09-13 16:39:56 +0900

    feat: .editorconfig 파일 추가

--- android/app/src/main/java/com/mulkkam/ui/designsystem/Typography.kt
+++ android/app/src/main/java/com/mulkkam/ui/designsystem/Typography.kt
@@ -1,10 +1,8 @@
 package com.mulkkam.ui.designsystem
 
-import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.mulkkam.R
 
