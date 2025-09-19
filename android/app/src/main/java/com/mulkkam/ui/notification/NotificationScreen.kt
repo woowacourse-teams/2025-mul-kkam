7f0e236b6f6dd600ab616e6dccfb7daae92eb3c5
hwannow <vnfdlvgid061@naver.com>
2025-09-19 10:00:36 +0900

    feat: 알림 화면 비었을 때 빈 화면 UI 추가

--- android/app/src/main/java/com/mulkkam/ui/notification/NotificationScreen.kt
+++ android/app/src/main/java/com/mulkkam/ui/notification/NotificationScreen.kt
@@ -1,14 +1,28 @@
 package com.mulkkam.ui.notification
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.mulkkam.R
+import com.mulkkam.ui.designsystem.Gray400
+import com.mulkkam.ui.designsystem.MulKkamTheme
 import com.mulkkam.ui.designsystem.MulkkamTheme
 import com.mulkkam.ui.designsystem.White
 import com.mulkkam.ui.model.MulKkamUiState.Idle.toSuccessDataOrNull
@@ -28,6 +42,28 @@ fun NotificationScreen(
         topBar = { NotificationTopAppBar(onBackClick) },
         containerColor = White,
     ) { innerPadding ->
+        if (notifications.value.toSuccessDataOrNull()?.isEmpty() == true) {
+            Column(
+                modifier =
+                    Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.img_crying_character),
+                    modifier = Modifier.size(200.dp),
+                    contentDescription = stringResource(R.string.notification_empty_description),
+                )
+                Spacer(modifier = Modifier.padding(vertical = 20.dp))
+                Text(
+                    text = stringResource(R.string.notification_empty),
+                    style = MulKkamTheme.typography.body2,
+                    color = Gray400,
+                )
+            }
+        }
         LazyColumn(
             modifier = Modifier.padding(innerPadding),
         ) {
