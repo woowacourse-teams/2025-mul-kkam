7b51bc756c2dfb9b3b06eae06d2a8b9f5c6704f1
hwannow <vnfdlvgid061@naver.com>
2025-09-21 16:07:58 +0900

    style: 린트 적용

--- android/app/src/main/java/com/mulkkam/ui/notification/EmptyNotificationScreen.kt
+++ android/app/src/main/java/com/mulkkam/ui/notification/EmptyNotificationScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
--- android/app/src/main/java/com/mulkkam/ui/notification/NotificationScreen.kt
+++ android/app/src/main/java/com/mulkkam/ui/notification/NotificationScreen.kt
@@ -1,26 +1,14 @@
 package com.mulkkam.ui.notification
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.mulkkam.R
-import com.mulkkam.ui.designsystem.Gray400
-import com.mulkkam.ui.designsystem.MulKkamTheme
 import com.mulkkam.ui.designsystem.MulkkamTheme
 import com.mulkkam.ui.designsystem.White
 import com.mulkkam.ui.model.MulKkamUiState.Idle.toSuccessDataOrNull
