9c217fd4979f900139262cec6be565ade4b0035b
junseo511 <prime54@g.skku.edu>
2025-09-22 17:36:38 +0900

    refactor: `HealthConnectClient` 초기화 시 Health Connect 사용 가능 여부 확인

--- android/app/src/main/java/com/mulkkam/di/HealthConnectInjection.kt
+++ android/app/src/main/java/com/mulkkam/di/HealthConnectInjection.kt
@@ -2,16 +2,17 @@ package com.mulkkam.di
 
 import android.content.Context
 import androidx.health.connect.client.HealthConnectClient
+import com.mulkkam.ui.util.extensions.isHealthConnectAvailable
 
 object HealthConnectInjection {
     private var _healthConnectClient: HealthConnectClient? = null
     val healthConnectClient: HealthConnectClient
-        get() = _healthConnectClient ?: throw IllegalArgumentException()
+        get() = _healthConnectClient ?: throw IllegalStateException()
 
     const val PROVIDER_PACKAGE_NAME: String = "com.google.android.apps.healthdata"
 
     fun init(context: Context) {
-        if (_healthConnectClient == null) {
+        if (context.isHealthConnectAvailable() && _healthConnectClient == null) {
             _healthConnectClient = HealthConnectClient.getOrCreate(context)
         }
     }
