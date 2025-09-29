fcd565664f64bb14491b93663e25efbcbc1f51ec
devfeijoa <devfeijoa@gmail.com>
2025-09-30 00:43:39 +0900

    refactor: settingScreen 으로 점진적 리팩토링

---
+++ android/app/src/main/java/com/mulkkam/ui/setting/SettingFragment2.kt
@@ -0,0 +1,15 @@
+package com.mulkkam.ui.setting
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import com.mulkkam.ui.setting.adapter.SettingItem
+import com.mulkkam.ui.setting.model.SettingType
+
+@Composable
+fun SettingScreen(
+    items: List<SettingItem>,
+    onItemClick: (SettingType) -> Unit,
+) {
+    Column {
+    }
+}
