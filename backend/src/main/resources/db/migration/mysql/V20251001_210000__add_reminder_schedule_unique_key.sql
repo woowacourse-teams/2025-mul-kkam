a46f5d87d853cfb40b28ba2064de811bca282222
CheChe903 <cheche903@naver.com>
2025-10-02 13:19:52 +0900

    fix: sql 수정

--- backend/src/main/resources/db/migration/mysql/V20251001_210000__add_reminder_schedule_unique_key.sql
+++ backend/src/main/resources/db/migration/mysql/V20251001_210000__add_reminder_schedule_unique_key.sql
@@ -1,11 +1,12 @@
 -- ALLOW_DROP
 -- reason: reminder_schedule에는 soft delete 예정이므로 유니크키를 변경함. (886)
 
-ALTER TABLE reminder_schedule DROP INDEX uq_member_schedule;
+ALTER TABLE reminder_schedule
+DROP INDEX uq_member_schedule;
 
 ALTER TABLE reminder_schedule
     ADD COLUMN active_key BIGINT
         GENERATED ALWAYS AS (CASE WHEN deleted_at IS NULL THEN 0 ELSE id END) STORED;
 
 CREATE UNIQUE INDEX uq_member_schedule_active
-    ON reminder_schedule(member_id, schedule, active_key)
+    ON reminder_schedule(member_id, schedule, active_key);
