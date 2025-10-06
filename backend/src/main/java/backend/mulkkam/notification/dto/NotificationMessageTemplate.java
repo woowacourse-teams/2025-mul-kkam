a5aa407e618d770f2298853ca4aed6b636a03f2f
CheChe903 <cheche903@naver.com>
2025-10-06 13:59:14 +0900

    refactor: dto 위치 변경

--- backend/src/main/java/backend/mulkkam/notification/dto/NotificationMessageTemplate.java
+++ backend/src/main/java/backend/mulkkam/notification/dto/NotificationMessageTemplate.java
@@ -5,10 +5,7 @@ import backend.mulkkam.common.infrastructure.fcm.dto.request.SendMessageByFcmTop
 import backend.mulkkam.member.domain.Member;
 import backend.mulkkam.notification.domain.Notification;
 import backend.mulkkam.notification.domain.NotificationType;
-import backend.mulkkam.notification.dto.response.NotificationResponse;
-import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
-import java.util.List;
 
 public record NotificationMessageTemplate(
         String title,
@@ -33,15 +30,5 @@ public record NotificationMessageTemplate(
                 topic,
                 action
         );
-    }
-
-    @Schema(description = "알림 목록 조회 응답")
-    public static record ReadNotificationsResponse(
-            @Schema(description = "알림 목록")
-            List<NotificationResponse> readNotificationResponses,
-
-            @Schema(description = "다음 페이지 조회를 위한 커서 ID (다음 페이지가 없으면 null)", example = "80")
-            Long nextCursor
-    ) {
     }
 }
