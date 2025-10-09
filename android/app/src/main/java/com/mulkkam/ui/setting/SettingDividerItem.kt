7978378b3231a2312d6cb08d2f5e43b176d470ad
devfeijoa <devfeijoa@gmail.com>
2025-10-10 04:02:48 +0900

    refactor: 색상 변경

--- android/app/src/main/java/com/mulkkam/ui/setting/SettingDividerItem.kt
+++ android/app/src/main/java/com/mulkkam/ui/setting/SettingDividerItem.kt
@@ -5,13 +5,13 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.mulkkam.ui.designsystem.Gray100
+import com.mulkkam.ui.designsystem.Gray50
 
 @Composable
 fun SettingDividerItem() {
     HorizontalDivider(
         modifier = Modifier.fillMaxWidth(),
         thickness = 8.dp,
-        color = Gray100,
+        color = Gray50,
     )
 }
