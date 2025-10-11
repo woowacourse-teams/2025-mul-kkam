ff4bcaa210cc132105ed141a1cf871049ade1b59
2jin2.1031 <2jin2.1031@gmail.com>
2025-10-11 20:42:55 +0900

    feat: 불변 켤렉션을 반환하도록 수정

--- backend/src/main/java/backend/mulkkam/intake/domain/IntakeHistory.java
+++ backend/src/main/java/backend/mulkkam/intake/domain/IntakeHistory.java
@@ -16,6 +16,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import lombok.Getter;
@@ -77,6 +78,10 @@ public class IntakeHistory extends BaseEntity {
 
     public void addTargetAmount(int targetAmount) {
         this.targetAmount = new TargetAmount(this.targetAmount.value() + targetAmount);
+    }
+
+    public List<IntakeHistoryDetail> getIntakeHistoryDetails() {
+        return Collections.unmodifiableList(intakeHistoryDetails);
     }
 
     @Override
