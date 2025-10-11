7c827f2a3024dc93ce75aa1d99f5d8f1dd4c23cd
Jin409 <sualng123@gmail.com>
2025-10-11 15:51:59 +0900

    refactor: 불필요한 Member 호출 제거

--- backend/src/main/java/backend/mulkkam/friend/service/FriendService.java
+++ backend/src/main/java/backend/mulkkam/friend/service/FriendService.java
@@ -10,7 +10,6 @@ import backend.mulkkam.friend.domain.Friend;
 import backend.mulkkam.friend.domain.FriendRequest;
 import backend.mulkkam.friend.repository.FriendRepository;
 import backend.mulkkam.friend.repository.FriendRequestRepository;
-import backend.mulkkam.member.domain.Member;
 import backend.mulkkam.member.repository.MemberRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -30,10 +29,11 @@ public class FriendService {
             boolean isAccept,
             MemberDetails memberDetails
     ) {
-        Member member = getMember(memberDetails.id());
+        Long memberId = memberDetails.id();
+        validateMemberExistence(memberId);
         FriendRequest friendRequest = getFriendRequest(friendRequestId);
 
-        if (!friendRequest.validatePermission(member.getId())) {
+        if (!friendRequest.validatePermission(memberId)) {
             throw new CommonException(NOT_PERMITTED_FOR_PROCESS_FRIEND_REQUEST);
         }
 
@@ -44,13 +44,14 @@ public class FriendService {
         friendRequestRepository.delete(friendRequest);
     }
 
+    private void validateMemberExistence(Long memberId) {
+        if (!memberRepository.existsById(memberId)) {
+            throw new CommonException(NOT_FOUND_MEMBER);
+        }
+    }
+
     private FriendRequest getFriendRequest(Long friendRequestId) {
         return friendRequestRepository.findById(friendRequestId)
                 .orElseThrow(() -> new CommonException(NOT_FOUND_FRIEND_REQUEST));
-    }
-
-    private Member getMember(Long id) {
-        return memberRepository.findById(id)
-                .orElseThrow(() -> new CommonException(NOT_FOUND_MEMBER));
     }
 }
