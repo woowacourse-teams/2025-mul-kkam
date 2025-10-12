baa71daf8c93b9a77d47026f7587e847aa526e0a
minSsan <msp1125@hanyang.ac.kr>
2025-10-12 21:50:36 +0900

    feat: 친구 삭제 API 구현

--- backend/src/main/java/backend/mulkkam/friend/controller/FriendController.java
+++ backend/src/main/java/backend/mulkkam/friend/controller/FriendController.java
@@ -1,12 +1,32 @@
 package backend.mulkkam.friend.controller;
 
+import backend.mulkkam.common.dto.MemberDetails;
 import backend.mulkkam.friend.service.FriendService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "친구", description = "친구 API")
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/friends")
 public class FriendController {
 
     private final FriendService friendService;
+
+    @Operation(summary = "친구 삭제", description = "로그인 멤버와 지정 멤버 사이의 친구 관계를 삭제합니다.")
+    @DeleteMapping("/{id}")
+    public void deleteFriend(
+            @Parameter(description = "삭제할 친구의 id", required = true)
+            @PathVariable Long id,
+            @Parameter(hidden = true)
+            MemberDetails memberDetails
+    ) {
+        friendService.delete(id, memberDetails);
+    }
 }
