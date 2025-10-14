ac6d5f39dafe8566d1f83d785811b770e692f715
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2025-10-14 23:45:45 +0900

    [BE][test]: 임시로 비동기 처리하도록 수정 (#922)

--- backend/src/main/java/backend/mulkkam/common/infrastructure/fcm/service/FcmEventListener.java
+++ backend/src/main/java/backend/mulkkam/common/infrastructure/fcm/service/FcmEventListener.java
@@ -4,6 +4,7 @@ import backend.mulkkam.common.infrastructure.fcm.dto.request.SendMessageByFcmTok
 import backend.mulkkam.common.infrastructure.fcm.dto.request.SendMessageByFcmTopicRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
@@ -12,11 +13,15 @@ public class FcmEventListener {
 
     private final FcmClient fcmClient;
 
+    // TODO: 알림 성능 테스트 이후 삭제
+    @Async
     @EventListener
     public void onTopic(SendMessageByFcmTopicRequest sendMessageByFcmTopicRequest) {
         fcmClient.sendMessageByTopic(sendMessageByFcmTopicRequest);
     }
 
+    // TODO: 알림 성능 테스트 이후 삭제
+    @Async
     @EventListener
     public void onToken(SendMessageByFcmTokenRequest sendMessageByFcmTokenRequest) {
         fcmClient.sendMessageByToken(sendMessageByFcmTokenRequest);
