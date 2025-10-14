3f07675c2c95f4a7477cfb47811a735cf7a4b63b
Park Minseon <63039855+minSsan@users.noreply.github.com>
2025-10-14 21:26:51 +0900

    [BE][feat]: 친구 삭제 (#910)

--- backend/src/main/java/backend/mulkkam/friend/service/FriendService.java
+++ backend/src/main/java/backend/mulkkam/friend/service/FriendService.java
@@ -1,7 +1,9 @@
 package backend.mulkkam.friend.service;
 
+import backend.mulkkam.common.dto.MemberDetails;
 import backend.mulkkam.friend.repository.FriendRepository;
 import backend.mulkkam.friend.repository.FriendRequestRepository;
+import backend.mulkkam.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +13,10 @@ public class FriendService {
 
     private final FriendRepository friendRepository;
     private final FriendRequestRepository friendRequestRepository;
+    private final MemberRepository memberRepository;
+
+    public void delete(Long memberId, MemberDetails memberDetails) {
+        friendRepository.findByFriendIdAndMemberId(memberId, memberDetails.id())
+                .ifPresent(friendRepository::delete);
+    }
 }
