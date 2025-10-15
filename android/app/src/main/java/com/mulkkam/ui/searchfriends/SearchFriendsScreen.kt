ca736875548cff20868b69090f8fdb7b2b437155
hwannow <vnfdlvgid061@naver.com>
2025-10-15 15:03:40 +0900

    feat: 친구 검색 화면 구현

---
+++ android/app/src/main/java/com/mulkkam/ui/searchfriends/SearchFriendsScreen.kt
@@ -0,0 +1,118 @@
+package com.mulkkam.ui.searchfriends
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.mulkkam.R
+import com.mulkkam.ui.designsystem.Gray100
+import com.mulkkam.ui.designsystem.Gray300
+import com.mulkkam.ui.designsystem.Gray400
+import com.mulkkam.ui.designsystem.MulKkamTheme
+import com.mulkkam.ui.designsystem.MulkkamTheme
+import com.mulkkam.ui.designsystem.White
+import com.mulkkam.ui.model.MulKkamUiState.Idle.toSuccessDataOrNull
+import com.mulkkam.ui.searchfriends.component.SearchFriendsItem
+import com.mulkkam.ui.searchfriends.component.SearchFriendsTopAppBar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchFriendsScreen(
+    navigateToBack: () -> Unit,
+    viewModel: SearchFriendsViewModel = viewModel(),
+) {
+    var name by remember { mutableStateOf("") }
+    val searchFriendsUiState by viewModel.searchFriendsUiState.collectAsStateWithLifecycle()
+
+    Scaffold(
+        topBar = { SearchFriendsTopAppBar(navigateToBack) },
+        containerColor = White,
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding),
+        ) {
+            BasicTextField(
+                value = name,
+                onValueChange = { name = it },
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(24.dp)
+                        .height(44.dp)
+                        .border(width = 1.dp, color = Gray400, shape = RoundedCornerShape(8.dp)),
+                textStyle = MulKkamTheme.typography.body2.copy(color = Gray400),
+                decorationBox = { innerTextField ->
+                    Row(
+                        modifier =
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 10.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Icon(
+                            modifier = Modifier.padding(vertical = 4.dp, horizontal = 16.dp),
+                            painter = painterResource(R.drawable.ic_search_friends_search),
+                            contentDescription = null,
+                            tint = Gray300,
+                        )
+                        innerTextField()
+                    }
+                },
+            )
+
+            HorizontalDivider(
+                modifier = Modifier.fillMaxWidth(),
+                thickness = 1.dp,
+                color = Gray100,
+            )
+
+            LazyColumn {
+                val searchFriends = searchFriendsUiState.toSuccessDataOrNull() ?: return@LazyColumn
+
+                items(
+                    count = searchFriends.size,
+                    key = { index -> searchFriends[index].name },
+                ) { index ->
+                    SearchFriendsItem(
+                        name = searchFriends[index].name,
+                    )
+
+                    HorizontalDivider(
+                        modifier = Modifier.fillMaxWidth(),
+                        thickness = 1.dp,
+                        color = Gray100,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SearchFriendsScreenPreview() {
+    MulkkamTheme {
+        SearchFriendsScreen(navigateToBack = {})
+    }
+}
---
+++ android/app/src/main/java/com/mulkkam/ui/searchfriends/SearchFriendsViewModel.kt
@@ -0,0 +1,26 @@
+package com.mulkkam.ui.searchfriends
+
+import androidx.lifecycle.ViewModel
+import com.mulkkam.ui.model.MulKkamUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class SearchFriendsViewModel : ViewModel() {
+    private val _searchFriendsUiState: MutableStateFlow<MulKkamUiState<List<Friend>>> =
+        MutableStateFlow(
+            MulKkamUiState.Success(
+                listOf(
+                    Friend("돈가스먹는환노", false),
+                    Friend("돈가스먹는공백", true),
+                    Friend("돈가스싫은이든", false),
+                ),
+            ),
+        )
+    val searchFriendsUiState: StateFlow<MulKkamUiState<List<Friend>>> = _searchFriendsUiState.asStateFlow()
+}
+
+data class Friend(
+    val name: String,
+    val isRequested: Boolean,
+)
