063ff72d54ad3f55bddad83695a71e9d4026d5ee
hwannow <vnfdlvgid061@naver.com>
2025-10-15 15:00:00 +0900

    feat: 친구 찾기 화면 TopAppBar 구현

---
+++ android/app/src/main/java/com/mulkkam/ui/searchfriends/component/SearchFriendsTopAppBar.kt
@@ -0,0 +1,69 @@
+package com.mulkkam.ui.searchfriends.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mulkkam.R
+import com.mulkkam.ui.designsystem.Gray100
+import com.mulkkam.ui.designsystem.Gray400
+import com.mulkkam.ui.designsystem.MulKkamTheme
+import com.mulkkam.ui.designsystem.MulkkamTheme
+import com.mulkkam.ui.designsystem.White
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchFriendsTopAppBar(onBackClick: () -> Unit) {
+    Column {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = stringResource(R.string.search_friends_top_app_bar_title),
+                    style = MulKkamTheme.typography.title2,
+                )
+            },
+            navigationIcon = {
+                IconButton(
+                    onClick = onBackClick,
+                    modifier = Modifier.size(48.dp),
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_common_prev),
+                        contentDescription = stringResource(R.string.notification_app_bar_navigation_icon_description),
+                    )
+                }
+            },
+            colors =
+                TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = White,
+                    titleContentColor = Gray400,
+                    navigationIconContentColor = Gray400,
+                ),
+        )
+        HorizontalDivider(
+            modifier = Modifier.fillMaxWidth(),
+            thickness = 1.dp,
+            color = Gray100,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SearchFriendsTopAppBarPreview() {
+    MulkkamTheme {
+        SearchFriendsTopAppBar(onBackClick = {})
+    }
+}
