67d3ba685609942017d962ab116b197516c1abad
Jin409 <sualng123@gmail.com>
2025-10-16 23:13:25 +0900

    test: 친구 목록 조회에 대한 테스트 추가

--- backend/src/test/java/backend/mulkkam/friend/service/FriendRelationServiceIntegrationTest.java
+++ backend/src/test/java/backend/mulkkam/friend/service/FriendRelationServiceIntegrationTest.java
@@ -15,12 +15,15 @@ import backend.mulkkam.common.dto.MemberDetails;
 import backend.mulkkam.common.exception.CommonException;
 import backend.mulkkam.friend.domain.FriendRelation;
 import backend.mulkkam.friend.domain.FriendRelationStatus;
+import backend.mulkkam.friend.dto.FriendRelationResponse;
+import backend.mulkkam.friend.dto.FriendRelationResponse.MemberInfo;
 import backend.mulkkam.friend.repository.FriendRelationRepository;
 import backend.mulkkam.member.domain.Member;
 import backend.mulkkam.member.domain.vo.MemberNickname;
 import backend.mulkkam.member.repository.MemberRepository;
 import backend.mulkkam.support.fixture.member.MemberFixtureBuilder;
 import backend.mulkkam.support.service.ServiceIntegrationTest;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -279,6 +282,93 @@ class FriendRelationServiceIntegrationTest extends ServiceIntegrationTest {
                     new MemberDetails(addressee.getId())))
                     .isInstanceOf(CommonException.class)
                     .hasMessage(NOT_FOUND_FRIEND_RELATION.name());
+        }
+    }
+
+    @DisplayName("친구 목록을 조회할 때")
+    @Nested
+    class Read {
+
+        @DisplayName("친구 관계의 수락자나 요청자가 조회 시도자인 경우만 반환한다")
+        @Test
+        void success_onlyRequesterOrAddressee() {
+            // given
+            List<Long> idOfMemberInRelation = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                Member member = MemberFixtureBuilder.builder().memberNickname(new MemberNickname("히로" + i)).build();
+                memberRepository.save(member);
+
+                // 3의 배수인 경우에는 member 를 요청자로 저장
+                if (i % 3 == 0) {
+                    FriendRelation friendRelation = new FriendRelation(member.getId(), requester.getId(),
+                            FriendRelationStatus.ACCEPTED);
+                    friendRelationRepository.save(friendRelation);
+                    idOfMemberInRelation.add(member.getId());
+                    continue;
+                }
+
+                // 짝수인 경우에는 member 를 수락자로 저장
+                if (i % 2 == 0) {
+                    FriendRelation friendRelation = new FriendRelation(requester.getId(), member.getId(),
+                            FriendRelationStatus.ACCEPTED);
+                    friendRelationRepository.save(friendRelation);
+                    idOfMemberInRelation.add(member.getId());
+                }
+            }
+
+            // when
+            FriendRelationResponse friendRelationResponse = friendRelationService.read(null, 10,
+                    new MemberDetails(requester.getId()));
+
+            // then
+            List<Long> memberIdsOfResult = friendRelationResponse.memberInfo().stream()
+                    .map(MemberInfo::memberId)
+                    .toList();
+
+            assertSoftly(softly -> {
+                softly.assertThat(memberIdsOfResult).containsExactlyInAnyOrderElementsOf(idOfMemberInRelation);
+            });
+        }
+
+        @DisplayName("상태가 ACCEPTED 인 경우에만 반환한다")
+        @Test
+        void success_onlyStatusISAccepted() {
+            // given
+            List<Long> memberIdsOfAcceptedRelations = new ArrayList<>();
+
+            for (int i = 0; i < 10; i++) {
+                Member member = MemberFixtureBuilder.builder().memberNickname(new MemberNickname("히로" + i)).build();
+                memberRepository.save(member);
+
+                // 3의 배수인 경우에는 신청된 관계로 저장
+                if (i % 3 == 0) {
+                    FriendRelation friendRelation = new FriendRelation(member.getId(), requester.getId(),
+                            FriendRelationStatus.REQUESTED);
+                    friendRelationRepository.save(friendRelation);
+                    continue;
+                }
+
+                // 2의 배수인 경우에는 수락된 관계로 저장
+                if (i % 2 == 0) {
+                    FriendRelation friendRelation = new FriendRelation(requester.getId(), member.getId(),
+                            FriendRelationStatus.ACCEPTED);
+                    friendRelationRepository.save(friendRelation);
+                    memberIdsOfAcceptedRelations.add(member.getId());
+                }
+            }
+
+            // when
+            FriendRelationResponse friendRelationResponse = friendRelationService.read(null, 10,
+                    new MemberDetails(requester.getId()));
+
+            // then
+            List<Long> memberIdsOfResult = friendRelationResponse.memberInfo().stream()
+                    .map(MemberInfo::memberId)
+                    .toList();
+
+            assertSoftly(softly -> {
+                softly.assertThat(memberIdsOfResult).containsExactlyInAnyOrderElementsOf(memberIdsOfAcceptedRelations);
+            });
         }
     }
 }
