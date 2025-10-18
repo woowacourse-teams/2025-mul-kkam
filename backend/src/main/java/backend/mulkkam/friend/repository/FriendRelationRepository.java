9a4f33d20ad529cdfc30c354c50eb64fb202fb4b
Jin409 <sualng123@gmail.com>
2025-10-18 20:15:25 +0900

    feat: 내가 보낸 친구 신청 목록 조회 기능 구현

--- backend/src/main/java/backend/mulkkam/friend/repository/FriendRelationRepository.java
+++ backend/src/main/java/backend/mulkkam/friend/repository/FriendRelationRepository.java
@@ -3,8 +3,10 @@ package backend.mulkkam.friend.repository;
 import backend.mulkkam.friend.domain.FriendRelation;
 import backend.mulkkam.friend.dto.response.FriendRelationRequestResponse;
 import backend.mulkkam.friend.repository.dto.MemberInfoOfFriendRelation;
+import backend.mulkkam.friend.repository.dto.SentFriendRelationSummary;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -69,6 +71,21 @@ public interface FriendRelationRepository extends JpaRepository<FriendRelation, 
                 ORDER BY fr.id DESC
             """)
     List<MemberInfoOfFriendRelation> findByMemberId(
+            @Param("memberId") Long memberId,
+            @Param("lastId") Long lastId,
+            Pageable pageable
+    );
+
+    @Query("""
+                SELECT new backend.mulkkam.friend.repository.dto.SentFriendRelationSummary(fr.id, m.memberNickname.value)
+                FROM FriendRelation fr
+                JOIN Member m ON fr.requesterId = m.id
+                WHERE fr.requesterId = :memberId
+                  AND (fr.friendRelationStatus = "REQUESTED")
+                  AND (:lastId IS NULL OR fr.id < :lastId)
+                ORDER BY fr.id DESC
+            """)
+    List<SentFriendRelationSummary> findSentFriendRelationsAfterId(
             @Param("memberId") Long memberId,
             @Param("lastId") Long lastId,
             Pageable pageable
