a640c6326e907916ae6533d5bd0e895100016e1c
Jin409 <sualng123@gmail.com>
2025-10-19 16:30:17 +0900

    test: executor 풀 사이즈 튜닝

--- backend/src/main/java/backend/mulkkam/common/config/AsyncConfig.java
+++ backend/src/main/java/backend/mulkkam/common/config/AsyncConfig.java
@@ -1,10 +1,24 @@
 package backend.mulkkam.common.config;
 
+import java.util.concurrent.ThreadPoolExecutor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 // TODO: 알림 성능 테스트 이후 삭제
 @Configuration
 @EnableAsync
 public class AsyncConfig {
+    @Bean("fcmExecutor")
+    public ThreadPoolTaskExecutor fcmExecutor() {
+        ThreadPoolTaskExecutor ex = new ThreadPoolTaskExecutor();
+        ex.setCorePoolSize(2);
+        ex.setMaxPoolSize(4);
+        ex.setQueueCapacity(100);
+        ex.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        ex.setThreadNamePrefix("fcm-async-");
+        ex.initialize();
+        return ex;
+    }
 }
