e305829d773b8f6e721748eecccaa59514497199
hwannow <vnfdlvgid061@naver.com>
2025-10-19 13:19:22 +0900

    refactor: `noRippleClickable` 확장 함수를 사용하여 클릭 이벤트 처리 리팩터링

--- android/app/src/main/java/com/mulkkam/ui/pendingfriends/component/AcceptFriendButton.kt
+++ android/app/src/main/java/com/mulkkam/ui/pendingfriends/component/AcceptFriendButton.kt
@@ -1,15 +1,12 @@
 package com.mulkkam.ui.pendingfriends.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,6 +18,7 @@ import com.mulkkam.R
 import com.mulkkam.ui.designsystem.MulkkamTheme
 import com.mulkkam.ui.designsystem.Primary200
 import com.mulkkam.ui.designsystem.Primary50
+import com.mulkkam.ui.util.extensions.noRippleClickable
 
 @Composable
 fun AcceptFriendButton(onClick: () -> Unit) {
@@ -28,10 +26,7 @@ fun AcceptFriendButton(onClick: () -> Unit) {
         modifier =
             Modifier
                 .size(48.dp)
-                .clickable(
-                    interactionSource = remember { MutableInteractionSource() },
-                    indication = null,
-                ) { onClick }
+                .noRippleClickable(onClick = onClick)
                 .padding(6.dp)
                 .clip(RoundedCornerShape(12.dp))
                 .background(Primary50),
--- android/app/src/main/java/com/mulkkam/ui/pendingfriends/component/RejectFriendButton.kt
+++ android/app/src/main/java/com/mulkkam/ui/pendingfriends/component/RejectFriendButton.kt
@@ -1,15 +1,12 @@
 package com.mulkkam.ui.pendingfriends.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,6 +18,7 @@ import com.mulkkam.R
 import com.mulkkam.ui.designsystem.MulkkamTheme
 import com.mulkkam.ui.designsystem.Secondary100
 import com.mulkkam.ui.designsystem.Secondary200
+import com.mulkkam.ui.util.extensions.noRippleClickable
 
 @Composable
 fun RejectFriendButton(onClick: () -> Unit) {
@@ -28,10 +26,7 @@ fun RejectFriendButton(onClick: () -> Unit) {
         modifier =
             Modifier
                 .size(48.dp)
-                .clickable(
-                    interactionSource = remember { MutableInteractionSource() },
-                    indication = null,
-                ) { onClick }
+                .noRippleClickable(onClick = onClick)
                 .padding(6.dp)
                 .clip(RoundedCornerShape(12.dp))
                 .background(Secondary100),
