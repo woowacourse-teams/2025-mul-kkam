37c9e71ea4cb00801c34ac199e1fb9dc1cf61a30
Jin409 <sualng123@gmail.com>
2025-10-19 22:48:39 +0900

    chore: 설정값 정리

--- backend/src/main/java/backend/mulkkam/common/config/RabbitMQConfig.java
+++ backend/src/main/java/backend/mulkkam/common/config/RabbitMQConfig.java
@@ -12,7 +12,9 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("worker")
 @Configuration
 public class RabbitMQConfig {
 
