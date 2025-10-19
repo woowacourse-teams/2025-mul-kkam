144f7b29dde2f2fa74066129d60c1873f6770328
Eunyeong Jang <devfeijoa@gmail.com>
2025-10-20 00:44:41 +0900

    [AN][refactor]: 설정 화면 컴포즈 마이그레이션 (#902)
    Co-authored-by: hwannow <vnfdlvgid061@naver.com>

---
+++ android/app/src/main/java/com/mulkkam/ui/setting/component/SettingTitleItem.kt
@@ -0,0 +1,49 @@
+package com.mulkkam.ui.setting.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mulkkam.ui.designsystem.Gray50
+import com.mulkkam.ui.designsystem.MulKkamTheme
+import com.mulkkam.ui.designsystem.MulkkamTheme
+import com.mulkkam.ui.setting.SettingItem
+
+@Composable
+fun SettingTitleItem(item: SettingItem.TitleItem) {
+    Column(
+        modifier =
+            Modifier
+                .fillMaxWidth(),
+    ) {
+        Text(
+            text = item.title,
+            style = MulKkamTheme.typography.label1,
+            color = Color.Black,
+            modifier = Modifier.padding(start = 24.dp, top = 12.dp, bottom = 8.dp),
+        )
+        HorizontalDivider(
+            color = Gray50,
+            thickness = 1.dp,
+            modifier =
+                Modifier
+                    .fillMaxWidth(),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSettingTitleItem() {
+    MulkkamTheme {
+        SettingTitleItem(
+            item = SettingItem.TitleItem("계정 정보 설정"),
+        )
+    }
+}
