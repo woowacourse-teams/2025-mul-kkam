909369068d37d99694ac595b1ca08db44eb72bdb
Jin409 <sualng123@gmail.com>
2025-10-19 22:46:42 +0900

    style: 불필요한 import 문 제거

--- backend/src/main/java/backend/mulkkam/common/config/RabbitMQConfig.java
+++ backend/src/main/java/backend/mulkkam/common/config/RabbitMQConfig.java
@@ -1,20 +1,17 @@
 package backend.mulkkam.common.config;
 
+
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-
-import java.util.concurrent.Executor;
 
 @Configuration
 public class RabbitMQConfig {
