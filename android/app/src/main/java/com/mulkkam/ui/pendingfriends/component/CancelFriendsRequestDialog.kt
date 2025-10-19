e3dab94568f3be5f3601a28a42a54978e3991f66
hwannow <vnfdlvgid061@naver.com>
2025-10-19 16:36:22 +0900

    feat: 친구 요청 취소 다이얼로그 아이콘 크기 수정

--- android/app/src/main/java/com/mulkkam/ui/pendingfriends/component/CancelFriendsRequestDialog.kt
+++ android/app/src/main/java/com/mulkkam/ui/pendingfriends/component/CancelFriendsRequestDialog.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
@@ -67,6 +68,7 @@ fun CancelFriendsRequestDialog(
                     painter = painterResource(R.drawable.ic_alert_circle),
                     contentDescription = null,
                     tint = Secondary200,
+                    modifier = Modifier.size(48.dp)
                 )
                 Spacer(modifier = Modifier.height(12.dp))
                 Text(
