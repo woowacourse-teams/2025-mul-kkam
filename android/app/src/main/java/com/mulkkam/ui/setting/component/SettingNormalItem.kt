8bd3d38e5da192102d64f9ba014aa676c32e72ef
devfeijoa <devfeijoa@gmail.com>
2025-10-19 20:10:42 +0900

    refactor: Image를 Icon 컴포저블로 변경

--- android/app/src/main/java/com/mulkkam/ui/setting/component/SettingNormalItem.kt
+++ android/app/src/main/java/com/mulkkam/ui/setting/component/SettingNormalItem.kt
@@ -1,12 +1,12 @@
 package com.mulkkam.ui.setting.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -41,7 +41,7 @@ fun SettingNormalItem(
             modifier = Modifier.padding(start = 24.dp, top = 14.dp, bottom = 14.dp),
         )
 
-        Image(
+        Icon(
             painter = painterResource(id = R.drawable.ic_setting_next),
             contentDescription = null,
             modifier =
