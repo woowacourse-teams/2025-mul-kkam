352eb0e2016e950d4faa92d993c3cddbde03e0fe
minSsan <msp1125@hanyang.ac.kr>
2025-10-22 20:52:34 +0900

    fix: `@RequestBody` 파라미터 누락

--- backend/src/main/java/backend/mulkkam/friend/controller/FriendController.java
+++ backend/src/main/java/backend/mulkkam/friend/controller/FriendController.java
@@ -18,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -65,7 +66,7 @@ public class FriendController {
     @PostMapping("/reminder")
     public void createReminder(
             @Parameter(description = "물풍선 보내기 요청 body")
-            @Valid CreateFriendReminderRequest request,
+            @RequestBody @Valid CreateFriendReminderRequest request,
             @Parameter(hidden = true) MemberDetails memberDetails
     ) {
         friendReminderHistoryService.createAndSendReminder(request, memberDetails);
