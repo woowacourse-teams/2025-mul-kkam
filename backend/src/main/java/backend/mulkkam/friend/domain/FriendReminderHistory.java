3e8ecb0a1a6b5445589e72255c5c00c18a834997
Park Minseon <63039855+minSsan@users.noreply.github.com>
2025-10-22 16:39:35 +0900

    [BE][feat]: 소셜 친구 알림(물풍선) 기능 추가 (#984)

---
+++ backend/src/main/java/backend/mulkkam/friend/domain/FriendReminderHistory.java
@@ -0,0 +1,53 @@
+package backend.mulkkam.friend.domain;
+
+import backend.mulkkam.common.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@Table(uniqueConstraints = @UniqueConstraint(
+        name = "uk_sender_recipient_date",
+        columnNames = {"sender_id", "recipient_id", "quota_date"}))
+@Entity
+public class FriendReminderHistory extends BaseEntity {
+
+    private static final short INIT_REMAINING_VALUE = 10;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="sender_id", nullable=false)
+    private Long senderId;
+
+    @Column(name="recipient_id", nullable=false)
+    private Long recipientId;
+
+    @Column(name="quota_date", nullable=false)
+    private LocalDate quotaDate;
+
+    @Column(name="remaining", nullable=false)
+    private short remaining;
+
+    private FriendReminderHistory(Long id, Long senderId, Long recipientId, LocalDate quotaDate) {
+        this.id = id;
+        this.senderId = senderId;
+        this.recipientId = recipientId;
+        this.quotaDate = quotaDate;
+        this.remaining = INIT_REMAINING_VALUE;
+    }
+
+    public FriendReminderHistory(Long senderId, Long recipientId, LocalDate quotaDate) {
+        this(null, senderId, recipientId, quotaDate);
+    }
+}
