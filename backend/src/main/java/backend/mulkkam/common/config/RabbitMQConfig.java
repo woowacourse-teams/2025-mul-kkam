48951bb8ecff4e49967b634da5bff74236463b64
Jin409 <sualng123@gmail.com>
2025-10-23 13:49:22 +0900

    chore: rabbit 설정을 켜도록 수정

--- backend/src/main/java/backend/mulkkam/common/config/RabbitMQConfig.java
+++ backend/src/main/java/backend/mulkkam/common/config/RabbitMQConfig.java
@@ -11,7 +11,9 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 
+@EnableRabbit
 @Configuration
 public class RabbitMQConfig {
 
