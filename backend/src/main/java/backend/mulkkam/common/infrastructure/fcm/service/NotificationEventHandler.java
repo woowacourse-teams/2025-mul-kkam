a114521a9128ec82c5de4e598a78cf4294ad0844
Jin409 <sualng123@gmail.com>
2025-10-23 13:27:36 +0900

    chore: 전송 여부 확인을 위해 로깅 추가

--- backend/src/main/java/backend/mulkkam/common/infrastructure/fcm/service/NotificationEventHandler.java
+++ backend/src/main/java/backend/mulkkam/common/infrastructure/fcm/service/NotificationEventHandler.java
@@ -4,6 +4,7 @@ import backend.mulkkam.common.infrastructure.fcm.dto.request.SendMessageByFcmTok
 import backend.mulkkam.common.infrastructure.fcm.dto.request.SendMessageByFcmTokensRequest;
 import backend.mulkkam.common.infrastructure.fcm.dto.request.SendMessageByFcmTopicRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -12,22 +13,26 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Async("producerExecutor")
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class NotificationEventHandler {
 
     private final NotificationMQProducer producer;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onTopic(SendMessageByFcmTopicRequest e) {
+        log.info("onTopic");
         producer.publishTopic(e);
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onToken(SendMessageByFcmTokenRequest e) {
+        log.info("onToken");
         producer.publishToken(e);
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onTokens(SendMessageByFcmTokensRequest e) {
+        log.info("onTokens");
         producer.publishTokens(e);
     }
 }
--- backend/src/main/java/backend/mulkkam/common/infrastructure/fcm/service/NotificationMQProducer.java
+++ backend/src/main/java/backend/mulkkam/common/infrastructure/fcm/service/NotificationMQProducer.java
@@ -6,12 +6,14 @@ import backend.mulkkam.common.infrastructure.fcm.dto.request.SendMessageByFcmTop
 import com.google.common.collect.Lists;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class NotificationMQProducer {
 
     private final RabbitTemplate rabbitTemplate;
@@ -22,16 +24,19 @@ public class NotificationMQProducer {
     private static final int FCM_BATCH_SIZE = 500;
 
     public void publishToken(SendMessageByFcmTokenRequest req) {
+        log.info("Publishing token to fcm queue");
         rabbitTemplate.convertAndSend(exchange, "notify.token", req);
     }
 
     public void publishTokens(SendMessageByFcmTokensRequest req) {
+        log.info("Publishing token to fcm queue");
         for (List<String> part : Lists.partition(req.allTokens(), FCM_BATCH_SIZE)) {
             rabbitTemplate.convertAndSend(exchange, "notify.tokens", req.withTokens(part));
         }
     }
 
     public void publishTopic(SendMessageByFcmTopicRequest req) {
+        log.info("Publishing token to fcm queue");
         rabbitTemplate.convertAndSend(exchange, "notify.topic", req);
     }
 }
