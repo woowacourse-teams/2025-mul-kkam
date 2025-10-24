5fc0742e62e0525678a271df0ea810da1276a6e8
GongBaek <127238018+junseo511@users.noreply.github.com>
2025-10-24 11:32:25 +0900

    [AN][fix]: 위젯을 포함한 워크매니저 의존성 주입 오류 해결 (#1000)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
 }
 
