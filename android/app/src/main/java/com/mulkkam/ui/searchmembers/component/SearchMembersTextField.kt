754abbde9e889f647ceb34f171f4356a823e198b
hwannow <vnfdlvgid061@naver.com>
2025-10-27 12:22:39 +0900

    feat: 검색 시 키보드 숨기기 처리

--- android/app/src/main/java/com/mulkkam/ui/searchmembers/component/SearchMembersTextField.kt
+++ android/app/src/main/java/com/mulkkam/ui/searchmembers/component/SearchMembersTextField.kt
@@ -7,23 +7,30 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mulkkam.R
 import com.mulkkam.domain.model.members.Nickname
 import com.mulkkam.ui.designsystem.Gray300
 import com.mulkkam.ui.designsystem.Gray400
 import com.mulkkam.ui.designsystem.MulKkamTheme
+import com.mulkkam.ui.designsystem.MulkkamTheme
 
 @Composable
 fun SearchMembersTextField(
     name: String,
     onValueChanged: (String) -> Unit,
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
     BasicTextField(
         value = name,
         onValueChange = {
@@ -38,6 +45,11 @@ fun SearchMembersTextField(
                 .height(44.dp)
                 .border(width = 1.dp, color = Gray400, shape = RoundedCornerShape(8.dp)),
         textStyle = MulKkamTheme.typography.body2.copy(color = Gray400),
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+        keyboardActions =
+            KeyboardActions(onDone = {
+                keyboardController?.hide()
+            }),
         decorationBox = { innerTextField ->
             Row(
                 modifier =
@@ -56,4 +68,12 @@ fun SearchMembersTextField(
             }
         },
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SearchMembersTextFieldPreview() {
+    MulkkamTheme {
+        SearchMembersTextField(name = "", onValueChanged = {})
+    }
 }
