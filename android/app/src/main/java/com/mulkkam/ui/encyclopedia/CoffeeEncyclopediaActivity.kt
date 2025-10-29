aef9c530f2afe02b483d323fd4c26f4eee7e1a47
hwannow <vnfdlvgid061@naver.com>
2025-10-29 14:00:15 +0900

    refactor: 커피 백과사전 화면을 Compose로 마이그레이션

--- android/app/src/main/java/com/mulkkam/ui/encyclopedia/CoffeeEncyclopediaActivity.kt
+++ android/app/src/main/java/com/mulkkam/ui/encyclopedia/CoffeeEncyclopediaActivity.kt
@@ -3,19 +3,27 @@ package com.mulkkam.ui.encyclopedia
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import com.mulkkam.databinding.ActivityCoffeeEncyclopediaBinding
-import com.mulkkam.ui.util.binding.BindingActivity
-import com.mulkkam.ui.util.extensions.setSingleClickListener
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.mulkkam.ui.designsystem.MulkkamTheme
 
-class CoffeeEncyclopediaActivity :
-    BindingActivity<ActivityCoffeeEncyclopediaBinding>(
-        ActivityCoffeeEncyclopediaBinding::inflate,
-    ) {
+class CoffeeEncyclopediaActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding.ivBack.setSingleClickListener {
-            finish()
+        setContent {
+            MulkkamTheme {
+                CoffeeEncyclopediaScreen(
+                    navigateToBack = ::finish,
+                    navigateToSource = { uri ->
+                        val intent =
+                            Intent(
+                                Intent.ACTION_VIEW,
+                                uri,
+                            )
+                        startActivity(intent)
+                    },
+                )
+            }
         }
     }
 
