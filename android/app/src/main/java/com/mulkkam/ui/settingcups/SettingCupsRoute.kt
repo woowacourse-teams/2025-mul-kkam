502e8860300fb4e850fc8809aef7995004dbe482
junseo511 <prime54@g.skku.edu>
2025-11-23 16:56:56 +0900

    refactor: 불필요한 import 제거

--- android/app/src/main/java/com/mulkkam/ui/settingcups/SettingCupsRoute.kt
+++ android/app/src/main/java/com/mulkkam/ui/settingcups/SettingCupsRoute.kt
@@ -8,7 +8,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
