15638a3abae45dfe88a5efacb2d4cd8311188141
hwannow <vnfdlvgid061@naver.com>
2025-11-26 16:01:49 +0900

    refactor: SearchMembersTextField를 MulKkamTextField로 교체

--- android/app/src/main/java/com/mulkkam/ui/component/MulKkamTextField.kt
+++ android/app/src/main/java/com/mulkkam/ui/component/MulKkamTextField.kt
@@ -8,12 +8,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImagePainter.State.Empty.painter
@@ -36,6 +40,8 @@ fun MulKkamTextField(
     prefix: @Composable (Modifier) -> Unit = {},
     suffix: @Composable (Modifier) -> Unit = {},
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Box(
         modifier =
             modifier
@@ -73,6 +79,11 @@ fun MulKkamTextField(
                     suffix(Modifier.padding(start = 16.dp))
                 }
             },
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+            keyboardActions =
+                KeyboardActions(onDone = {
+                    keyboardController?.hide()
+                }),
         )
     }
 }
