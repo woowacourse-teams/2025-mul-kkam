011b5e55dd3325177e8b7884598126a3b91c04c6
hwannow <vnfdlvgid061@naver.com>
2025-12-01 01:47:56 +0900

    style: 린트 적용

--- android/app/src/main/java/com/mulkkam/ui/settingbioinfo/SettingBioInfoActivity.kt
+++ android/app/src/main/java/com/mulkkam/ui/settingbioinfo/SettingBioInfoActivity.kt
@@ -3,7 +3,6 @@ package com.mulkkam.ui.settingbioinfo
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
--- android/app/src/main/java/com/mulkkam/ui/settingbioinfo/SettingBioInfoViewModel.kt
+++ android/app/src/main/java/com/mulkkam/ui/settingbioinfo/SettingBioInfoViewModel.kt
@@ -1,8 +1,5 @@
 package com.mulkkam.ui.settingbioinfo
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mulkkam.domain.logger.Logger
