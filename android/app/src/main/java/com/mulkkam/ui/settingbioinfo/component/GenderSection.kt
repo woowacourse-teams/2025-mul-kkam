0a1db0b324a62c97b96afadb4bb4ae18f9380d34
hwannow <vnfdlvgid061@naver.com>
2025-12-01 01:24:37 +0900

    feat: 건강 정보 연동 섹션 추가

--- android/app/src/main/java/com/mulkkam/ui/settingbioinfo/component/GenderSection.kt
+++ android/app/src/main/java/com/mulkkam/ui/settingbioinfo/component/GenderSection.kt
@@ -1,35 +1,22 @@
 package com.mulkkam.ui.settingbioinfo.component
 
-import android.R.attr.text
-import android.R.attr.top
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mulkkam.R
 import com.mulkkam.domain.model.bio.Gender
 import com.mulkkam.ui.designsystem.Black
-import com.mulkkam.ui.designsystem.Gray400
 import com.mulkkam.ui.designsystem.MulKkamTheme
 import com.mulkkam.ui.designsystem.MulkkamTheme
-import com.mulkkam.ui.designsystem.Primary100
-import com.mulkkam.ui.designsystem.White
 
 @Composable
 fun GenderSection(
