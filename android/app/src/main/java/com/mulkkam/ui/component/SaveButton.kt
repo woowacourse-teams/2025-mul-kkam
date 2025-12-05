62163ed9c9f98def394538d213198e97c753165f
hwannow <106906887+hwannow@users.noreply.github.com>
2025-12-05 13:47:12 +0900

    [AN][refactor]: 닉네임 설정 컴포즈 마이그레이션 (#1034)

---
+++ android/app/src/main/java/com/mulkkam/ui/component/SaveButton.kt
@@ -0,0 +1,46 @@
+package com.mulkkam.ui.component
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.mulkkam.R
+import com.mulkkam.ui.designsystem.Gray200
+import com.mulkkam.ui.designsystem.MulKkamTheme
+import com.mulkkam.ui.designsystem.Primary200
+import com.mulkkam.ui.designsystem.White
+
+@Composable
+fun SaveButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+) {
+    Button(
+        onClick = { onClick() },
+        enabled = enabled,
+        colors =
+            ButtonColors(
+                containerColor = Primary200,
+                contentColor = White,
+                disabledContainerColor = Gray200,
+                disabledContentColor = White,
+            ),
+        shape = RoundedCornerShape(8.dp),
+        contentPadding = PaddingValues(0.dp),
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        Text(
+            text = stringResource(R.string.setting_save),
+            style = MulKkamTheme.typography.title2,
+            modifier = Modifier.padding(vertical = 14.dp),
+        )
+    }
+}
---
+++ android/app/src/main/java/com/mulkkam/ui/settingnickname/component/NicknameInputSection.kt
@@ -0,0 +1,60 @@
+package com.mulkkam.ui.settingnickname.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.mulkkam.R
+import com.mulkkam.domain.model.members.Nickname.Companion.NICKNAME_LENGTH_MAX
+import com.mulkkam.domain.model.result.MulKkamError
+import com.mulkkam.ui.component.MulKkamTextField
+import com.mulkkam.ui.component.MulKkamTextFieldState
+import com.mulkkam.ui.model.NicknameValidationUiState
+
+@Composable
+fun NicknameInputSection(
+    nickname: String,
+    nicknameValidationState: NicknameValidationUiState,
+    nicknameError: MulKkamError?,
+    onNicknameChange: (nickname: String) -> Unit,
+    onCheckDuplicate: () -> Unit,
+) {
+    Column {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.padding(top = 8.dp),
+        ) {
+            MulKkamTextField(
+                value = nickname,
+                onValueChanged = { onNicknameChange(it) },
+                state = getTextFieldState(nicknameValidationState),
+                modifier = Modifier.weight(1f),
+                placeHolder = stringResource(R.string.setting_nickname_hint_input_nickname),
+                maxLength = NICKNAME_LENGTH_MAX + 1,
+            )
+
+            DuplicateCheckButton(
+                enabled = nicknameValidationState == NicknameValidationUiState.PENDING_SERVER_VALIDATION,
+                onClick = onCheckDuplicate,
+            )
+        }
+
+        NicknameValidationMessage(
+            nicknameValidationState = nicknameValidationState,
+            nicknameError = nicknameError,
+        )
+    }
+}
+
+private fun getTextFieldState(state: NicknameValidationUiState): MulKkamTextFieldState =
+    when {
+        NicknameValidationUiState.isError(state) -> MulKkamTextFieldState.ERROR
+        state == NicknameValidationUiState.VALID -> MulKkamTextFieldState.VALID
+        else -> MulKkamTextFieldState.NORMAL
+    }
