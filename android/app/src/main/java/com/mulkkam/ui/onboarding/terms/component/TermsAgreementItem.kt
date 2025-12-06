44c6799e0867d63d77570abc1c77e9bff1a8df5c
hwannow <vnfdlvgid061@naver.com>
2025-12-06 16:22:38 +0900

    refactor: 약관 동의 체크박스 컴포저블 수정

--- android/app/src/main/java/com/mulkkam/ui/onboarding/terms/component/TermsAgreementItem.kt
+++ android/app/src/main/java/com/mulkkam/ui/onboarding/terms/component/TermsAgreementItem.kt
@@ -1,7 +1,7 @@
 package com.mulkkam.ui.onboarding.terms.component
 
+import android.R.attr.onClick
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -16,41 +16,57 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mulkkam.R
 import com.mulkkam.ui.designsystem.Gray100
+import com.mulkkam.ui.designsystem.Gray400
 import com.mulkkam.ui.designsystem.MulKkamTheme
 import com.mulkkam.ui.designsystem.MulkkamTheme
 import com.mulkkam.ui.designsystem.Primary200
+import com.mulkkam.ui.util.extensions.noRippleClickable
 
 @Composable
 fun TermsAgreementItem(
     termsLabel: String,
     isChecked: Boolean,
+    onClickCheck: () -> Unit,
+    onClickNext: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Row(
         modifier = modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        Icon(
-            modifier = Modifier.size(40.dp),
-            painter = painterResource(R.drawable.ic_terms_agreement_check),
-            contentDescription = null,
-            tint = getCheckIconTint(isChecked),
-        )
+        Row(
+            modifier =
+                Modifier
+                    .weight(1f)
+                    .noRippleClickable(onClick = onClickCheck),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
+                modifier =
+                    Modifier
+                        .size(40.dp)
+                        .padding(horizontal = 12.dp, vertical = 14.dp),
+                painter = painterResource(R.drawable.ic_terms_agreement_check),
+                contentDescription = null,
+                tint = getCheckIconTint(isChecked),
+            )
 
-        Text(
-            text = termsLabel,
-            style = MulKkamTheme.typography.body4,
-        )
-
-        Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = termsLabel,
+                style = MulKkamTheme.typography.body4,
+                color = Gray400,
+            )
+        }
 
         Icon(
             modifier =
                 Modifier
                     .size(40.dp)
+                    .noRippleClickable(onClick = onClickNext)
                     .padding(horizontal = 16.dp, vertical = 14.dp),
             painter = painterResource(R.drawable.ic_terms_agreement_next),
             contentDescription = null,
+            tint = Gray100,
         )
     }
 }
@@ -69,6 +85,8 @@ private fun TermsAgreementItemPreview_Checked() {
         TermsAgreementItem(
             termsLabel = "서비스 이용 약관 동의 (필수)",
             isChecked = true,
+            onClickCheck = {},
+            onClickNext = {},
         )
     }
 }
@@ -80,6 +98,8 @@ private fun TermsAgreementItemPreview_Unchecked() {
         TermsAgreementItem(
             termsLabel = "서비스 이용 약관 동의 (필수)",
             isChecked = false,
+            onClickCheck = {},
+            onClickNext = {},
         )
     }
 }
