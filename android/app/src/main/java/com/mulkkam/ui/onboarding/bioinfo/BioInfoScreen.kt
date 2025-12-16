246e1c6f126e832f4e32b2b1a10d7d33c651e3b1
hwannow <vnfdlvgid061@naver.com>
2025-12-17 00:23:00 +0900

    style: 린트 적용

--- android/app/src/main/java/com/mulkkam/ui/onboarding/bioinfo/BioInfoScreen.kt
+++ android/app/src/main/java/com/mulkkam/ui/onboarding/bioinfo/BioInfoScreen.kt
@@ -33,8 +33,8 @@ import com.mulkkam.ui.designsystem.White
 import com.mulkkam.ui.onboarding.component.NextButton
 import com.mulkkam.ui.onboarding.component.OnboardingTopAppBar
 import com.mulkkam.ui.settingbioinfo.component.GenderSection
-import com.mulkkam.ui.settingbioinfo.component.WeightSection
 import com.mulkkam.ui.settingbioinfo.component.SettingWeightBottomSheet
+import com.mulkkam.ui.settingbioinfo.component.WeightSection
 import com.mulkkam.ui.util.extensions.getStyledText
 
 @OptIn(ExperimentalMaterial3Api::class)
