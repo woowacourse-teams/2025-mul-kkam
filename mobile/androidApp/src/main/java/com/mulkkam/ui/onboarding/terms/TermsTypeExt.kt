acde9402f9b631000ff5b0fbb0cc581c20fc4095
junseo511 <prime54@g.skku.edu>
2025-12-29 19:57:56 +0900

    refactor: 일부 확장함수에 TODO 주석 추가

--- mobile/androidApp/src/main/java/com/mulkkam/ui/onboarding/terms/TermsTypeExt.kt
+++ mobile/androidApp/src/main/java/com/mulkkam/ui/onboarding/terms/TermsTypeExt.kt
@@ -3,11 +3,11 @@ package com.mulkkam.ui.onboarding.terms
 import com.mulkkam.R
 import com.mulkkam.ui.terms.model.TermsType
 
-fun TermsType.toResourceIds(): Pair<Int, Int> {
-    return when (this) {
+// TODO: 이것도 어떻게든 옮겨야함
+fun TermsType.toResourceIds(): Pair<Int, Int> =
+    when (this) {
         TermsType.SERVICE -> R.string.terms_agree_service to R.string.terms_service
         TermsType.PRIVACY -> R.string.terms_agree_privacy to R.string.terms_privacy
         TermsType.NIGHT_NOTIFICATION -> R.string.terms_agree_night_notification to R.string.terms_night_notification
         TermsType.MARKETING -> R.string.terms_agree_marketing to R.string.terms_marketing
     }
-}
--- mobile/androidApp/src/main/java/com/mulkkam/ui/settingaccountinfo/AccountInfoTypeExt.kt
+++ mobile/androidApp/src/main/java/com/mulkkam/ui/settingaccountinfo/AccountInfoTypeExt.kt
@@ -3,9 +3,9 @@ package com.mulkkam.ui.settingaccountinfo
 import com.mulkkam.R
 import com.mulkkam.ui.settingaccountinfo.model.AccountInfoType
 
-fun AccountInfoType.toStringResource(): Int {
-    return when (this) {
+// TODO: 이것도 어떻게든 옮겨야함
+fun AccountInfoType.toStringResource(): Int =
+    when (this) {
         AccountInfoType.LOGOUT -> R.string.setting_account_info_logout
         AccountInfoType.DELETE_ACCOUNT -> R.string.setting_account_info_delete_account
     }
-}
