3085ca6c0b84b6599eb90e8c2896f48f182e6e88
junseo511 <prime54@g.skku.edu>
2026-01-11 00:26:14 +0900

    refactor: Int.toCommaSeparated 음수 처리 로직 추가
    `toCommaSeparated` 확장 함수에서 음수일 경우 마이너스 부호가 유지되도록 로직을 수정합니다.

--- mobile/shared/src/commonMain/kotlin/com/mulkkam/ui/util/extensions/IntExtensions.kt
+++ mobile/shared/src/commonMain/kotlin/com/mulkkam/ui/util/extensions/IntExtensions.kt
@@ -1,9 +1,15 @@
 package com.mulkkam.ui.util.extensions
 
-fun Int.toCommaSeparated(): String =
-    this
-        .toString()
-        .reversed()
-        .chunked(3)
-        .joinToString(",")
-        .reversed()
+import kotlin.math.absoluteValue
+
+fun Int.toCommaSeparated(): String {
+    val isNegative: Boolean = this < 0
+    val formattedAbsoluteValue: String =
+        this.absoluteValue
+            .toString()
+            .reversed()
+            .chunked(3)
+            .joinToString(",")
+            .reversed()
+    return if (isNegative) "-$formattedAbsoluteValue" else formattedAbsoluteValue
+}
