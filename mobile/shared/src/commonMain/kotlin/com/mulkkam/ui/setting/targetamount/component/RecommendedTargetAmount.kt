ab33e6c7f228bc8985cab06fb62089bc4df59d99
hwannow <vnfdlvgid061@naver.com>
2026-01-12 00:15:31 +0900

    refactor: 목표 섭취량 설정 화면 컴포넌트 추가

---
+++ mobile/shared/src/commonMain/kotlin/com/mulkkam/ui/setting/targetamount/component/RecommendedTargetAmount.kt
@@ -0,0 +1,71 @@
+package com.mulkkam.ui.setting.targetamount.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.mulkkam.ui.component.ColoredText
+import com.mulkkam.ui.designsystem.Gray200
+import com.mulkkam.ui.designsystem.MulKkamTheme
+import com.mulkkam.ui.designsystem.Primary200
+import com.mulkkam.ui.util.extensions.toCommaSeparated
+import mulkkam.shared.generated.resources.Res
+import mulkkam.shared.generated.resources.target_amount_recommended_description
+import mulkkam.shared.generated.resources.target_amount_recommended_description_default
+import mulkkam.shared.generated.resources.target_amount_recommended_water_goal
+import mulkkam.shared.generated.resources.target_amount_recommended_water_goal_highlight
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RecommendedTargetAmount(
+    nickname: String,
+    recommended: Int,
+    modifier: Modifier = Modifier,
+    hasBioInfo: Boolean = true,
+) {
+    val recommendationDescription =
+        if (hasBioInfo) {
+            stringResource(resource = Res.string.target_amount_recommended_description)
+        } else {
+            stringResource(resource = Res.string.target_amount_recommended_description_default)
+        }
+
+    Column(modifier = modifier) {
+        ColoredText(
+            fullText =
+                stringResource(
+                    resource = Res.string.target_amount_recommended_water_goal,
+                    nickname,
+                    recommended.toCommaSeparated(),
+                ),
+            highlightedTexts =
+                listOf(
+                    nickname,
+                    stringResource(
+                        Res.string.target_amount_recommended_water_goal_highlight,
+                        recommended.toCommaSeparated(),
+                    ),
+                ),
+            highlightColor = Primary200,
+            style = MulKkamTheme.typography.body3,
+        )
+
+        Text(
+            text = recommendationDescription,
+            style = MulKkamTheme.typography.body5,
+            color = Gray200,
+            modifier = Modifier.padding(top = 24.dp),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun RecommendedTargetAmountPreview() {
+    MulKkamTheme {
+        RecommendedTargetAmount(nickname = "돈가스먹는환노", recommended = 1800)
+    }
+}
---
+++ mobile/shared/src/commonMain/kotlin/com/mulkkam/ui/setting/targetamount/component/TargetAmountInputSection.kt
@@ -0,0 +1,84 @@
+package com.mulkkam.ui.setting.targetamount.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import com.mulkkam.domain.model.result.MulKkamError
+import com.mulkkam.ui.component.MulKkamTextField
+import com.mulkkam.ui.component.MulKkamTextFieldState
+import com.mulkkam.ui.designsystem.Gray400
+import com.mulkkam.ui.designsystem.MulKkamTheme
+import com.mulkkam.ui.model.MulKkamUiState
+import mulkkam.shared.generated.resources.Res
+import mulkkam.shared.generated.resources.setting_target_amount_hint_input_goal
+import mulkkam.shared.generated.resources.setting_target_amount_unit_ml
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TargetAmountInputSection(
+    targetAmount: String,
+    targetAmountValidityUiState: MulKkamUiState<Unit>,
+    onTargetAmountChanged: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        MulKkamTextField(
+            value = targetAmount,
+            onValueChanged = { onTargetAmountChanged(it) },
+            placeHolder = stringResource(resource = Res.string.setting_target_amount_hint_input_goal),
+            suffix = {
+                Text(
+                    text = stringResource(resource = Res.string.setting_target_amount_unit_ml),
+                    style = MulKkamTheme.typography.title2,
+                    color = Gray400,
+                )
+            },
+            maxLength = 5,
+            state = getTextFieldState(targetAmountValidityUiState),
+            keyboardOptions =
+                KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                ),
+        )
+
+        if (targetAmountValidityUiState is MulKkamUiState.Failure) {
+            TargetAmountValidationMessage(targetAmountValidityUiState)
+        }
+    }
+}
+
+private fun getTextFieldState(targetAmountValidityUiState: MulKkamUiState<Unit>): MulKkamTextFieldState =
+    when {
+        targetAmountValidityUiState is MulKkamUiState.Failure -> MulKkamTextFieldState.ERROR
+        else -> MulKkamTextFieldState.NORMAL
+    }
+
+@Preview(showBackground = true)
+@Composable
+private fun TargetAmountInputSectionPreview() {
+    MulKkamTheme {
+        TargetAmountInputSection(
+            targetAmount = "1800",
+            onTargetAmountChanged = {},
+            targetAmountValidityUiState = MulKkamUiState.Success(Unit),
+        )
+    }
+}
+
+@Preview(showBackground = true, name = "목표량이 잘못 설정된 경우")
+@Composable
+private fun TargetAmountInputSectionPreview_Error() {
+    MulKkamTheme {
+        TargetAmountInputSection(
+            targetAmount = "1",
+            onTargetAmountChanged = {},
+            targetAmountValidityUiState = MulKkamUiState.Failure(MulKkamError.TargetAmountError.BelowMinimum),
+        )
+    }
+}
