d40771b0643b9ad7640fbed9c77e1379bd959dc2
junseo511 <prime54@g.skku.edu>
2026-01-11 17:40:28 +0900

    refactor: SettingScreen의 settingItems에 remember 적용
    recomposition 발생 시마다 리스트가 매번 재생성되지 않도록 `remember`를 사용하여 최적화하고, 리소스 문자열들을 변수로 추출하여 가독성을 개선합니다.

--- mobile/shared/src/commonMain/kotlin/com/mulkkam/ui/setting/setting/SettingScreen.kt
+++ mobile/shared/src/commonMain/kotlin/com/mulkkam/ui/setting/setting/SettingScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.mulkkam.ui.designsystem.Gray50
@@ -42,52 +43,54 @@ fun SettingScreen(
     padding: PaddingValues,
     navigateToSettingType: (type: SettingType) -> Unit,
 ) {
-    val settingItems =
+    val sectionAccount = stringResource(Res.string.setting_section_account)
+    val nicknameLabel = stringResource(Res.string.setting_nickname_edit_nickname_label)
+    val bodyInfoLabel = stringResource(Res.string.setting_item_body_info)
+    val accountInfoLabel = stringResource(Res.string.setting_item_account_info)
+    val sectionWater = stringResource(Res.string.setting_section_water)
+    val cupsLabel = stringResource(Res.string.setting_cups_toolbar_title)
+    val targetAmountLabel = stringResource(Res.string.setting_target_amount_toolbar_title)
+    val sectionNotification = stringResource(Res.string.setting_section_notification)
+    val pushNotificationLabel = stringResource(Res.string.setting_item_push_notification)
+    val reminderLabel = stringResource(Res.string.setting_reminder_toolbar_title)
+    val sectionSupport = stringResource(Res.string.setting_section_support)
+    val feedbackLabel = stringResource(Res.string.setting_item_feedback)
+    val termsLabel = stringResource(Res.string.setting_item_terms)
+
+    val settingItems = remember(
+        sectionAccount,
+        nicknameLabel,
+        bodyInfoLabel,
+        accountInfoLabel,
+        sectionWater,
+        cupsLabel,
+        targetAmountLabel,
+        sectionNotification,
+        pushNotificationLabel,
+        reminderLabel,
+        sectionSupport,
+        feedbackLabel,
+        termsLabel,
+    ) {
         listOf(
-            SettingItem.TitleItem(stringResource(Res.string.setting_section_account)),
-            SettingItem.NormalItem(
-                stringResource(Res.string.setting_nickname_edit_nickname_label),
-                SettingType.NICKNAME,
-            ),
-            SettingItem.NormalItem(
-                stringResource(Res.string.setting_item_body_info),
-                SettingType.BODY_INFO,
-            ),
-            SettingItem.NormalItem(
-                stringResource(Res.string.setting_item_account_info),
-                SettingType.ACCOUNT_INFO,
-            ),
+            SettingItem.TitleItem(sectionAccount),
+            SettingItem.NormalItem(nicknameLabel, SettingType.NICKNAME),
+            SettingItem.NormalItem(bodyInfoLabel, SettingType.BODY_INFO),
+            SettingItem.NormalItem(accountInfoLabel, SettingType.ACCOUNT_INFO),
             SettingItem.DividerItem,
-            SettingItem.TitleItem(stringResource(Res.string.setting_section_water)),
-            SettingItem.NormalItem(
-                stringResource(Res.string.setting_cups_toolbar_title),
-                SettingType.MY_CUP,
-            ),
-            SettingItem.NormalItem(
-                stringResource(Res.string.setting_target_amount_toolbar_title),
-                SettingType.GOAL,
-            ),
+            SettingItem.TitleItem(sectionWater),
+            SettingItem.NormalItem(cupsLabel, SettingType.MY_CUP),
+            SettingItem.NormalItem(targetAmountLabel, SettingType.GOAL),
             SettingItem.DividerItem,
-            SettingItem.TitleItem(stringResource(Res.string.setting_section_notification)),
-            SettingItem.NormalItem(
-                stringResource(Res.string.setting_item_push_notification),
-                SettingType.PUSH_NOTIFICATION,
-            ),
-            SettingItem.NormalItem(
-                stringResource(Res.string.setting_reminder_toolbar_title),
-                SettingType.REMINDER,
-            ),
+            SettingItem.TitleItem(sectionNotification),
+            SettingItem.NormalItem(pushNotificationLabel, SettingType.PUSH_NOTIFICATION),
+            SettingItem.NormalItem(reminderLabel, SettingType.REMINDER),
             SettingItem.DividerItem,
-            SettingItem.TitleItem(stringResource(Res.string.setting_section_support)),
-            SettingItem.NormalItem(
-                stringResource(Res.string.setting_item_feedback),
-                SettingType.FEEDBACK,
-            ),
-            SettingItem.NormalItem(
-                stringResource(Res.string.setting_item_terms),
-                SettingType.TERMS,
-            ),
+            SettingItem.TitleItem(sectionSupport),
+            SettingItem.NormalItem(feedbackLabel, SettingType.FEEDBACK),
+            SettingItem.NormalItem(termsLabel, SettingType.TERMS),
         )
+    }
 
     Column(
         modifier =
