6e726d5b2e958059c3e5275cb788109e9a48ed9d
hwannow <vnfdlvgid061@naver.com>
2026-01-15 20:28:34 +0900

    refactor: HealthKitManager 내 TODO 주석 추가

--- mobile/shared/src/iosMain/kotlin/com/mulkkam/ui/setting/bioinfo/HealthKitManager.kt
+++ mobile/shared/src/iosMain/kotlin/com/mulkkam/ui/setting/bioinfo/HealthKitManager.kt
@@ -2,6 +2,7 @@ package com.mulkkam.ui.setting.bioinfo
 
 import com.mulkkam.domain.model.bio.HealthManager
 
+// TODO: iOS HealthKit 구현 필요
 class HealthKitManager : HealthManager {
     override fun isAvailable(): Boolean = false
 
