4bf4ad0158437515fe2a15c2d8e06f14354e12a8
junseo511 <prime54@g.skku.edu>
2026-01-22 23:56:36 +0900

    feat: SettingCupBottomSheet 터치 시 포커스 해제 기능 추가
    바텀시트의 빈 영역을 클릭했을 때 키보드가 닫히도록 `FocusManager.clearFocus()`를 호출하는 `clickable` 수정을 추가합니다.

--- mobile/shared/src/commonMain/kotlin/com/mulkkam/ui/setting/cups/component/SettingCupBottomSheet.kt
+++ mobile/shared/src/commonMain/kotlin/com/mulkkam/ui/setting/cups/component/SettingCupBottomSheet.kt
@@ -1,5 +1,7 @@
 package com.mulkkam.ui.setting.cups.component
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,10 +18,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LifecycleOwner
@@ -78,6 +83,8 @@ fun SettingCupBottomSheet(
     modifier: Modifier = Modifier,
 ) {
     val lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
+    val focusManager: FocusManager = LocalFocusManager.current
+
     val cup: CupUiModel by viewModel.cup.collectAsStateWithLifecycle(initialValue = CupUiModel.Companion.EMPTY_CUP_UI_MODEL)
     val editType: SettingWaterCupEditType by viewModel.editType.collectAsStateWithLifecycle(initialValue = SettingWaterCupEditType.ADD)
     val cupNameValidity: MulKkamUiState<Unit> by viewModel.cupNameValidity.collectAsStateWithLifecycle(initialValue = MulKkamUiState.Idle)
@@ -133,7 +140,14 @@ fun SettingCupBottomSheet(
             modifier =
                 Modifier
                     .padding(horizontal = 24.dp)
-                    .padding(bottom = 24.dp),
+                    .padding(bottom = 24.dp)
+                    .padding(bottom = 24.dp)
+                    .clickable(
+                        indication = null,
+                        interactionSource = remember { MutableInteractionSource() },
+                    ) {
+                        focusManager.clearFocus()
+                    },
         ) {
             BottomSheetHeader(
                 title =
